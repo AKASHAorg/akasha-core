2fd6422645e0d67301e215fed4d2b592547f8b1c
Marius Darila <3396463+kenshyx@users.noreply.github.com>
2019-08-05 20:25:53 +0200

    Ui plugins integration (#31)
    * chore(): add plugin loader,  templates and a small documentation
    
    * chore(): add generator templates
    
    * refactor(ui-plugins): integrate into build env && playground
    
    * chore(): cleanup
    
    * chore(): add ignore rule

---
+++ playground/events-app/Readme.md
@@ -0,0 +1,5 @@
+## Usage
+
+- from the root of the project, run in a terminal `npm run watch-plugin-events`
+- in another terminal, from the current level run `npm start`
+- happy coding, every update on plugins ts files or the events app will trigger an update
---
+++ ui-plugins/events/src/components/App.tsx
@@ -0,0 +1,42 @@
+import { Component } from 'react';
+import React from 'react';
+
+export interface IProps {
+  removeMe: bigint;
+}
+
+/**
+ * This is the entry point of a plugin.
+ * Here you can add react-router, react-redux, etc..
+ *
+ * @todo Add more documentation for this component
+ *
+ * @warning :: Root component for a plugin should always extend React.Component
+ * @warning :: Always use default export
+ */
+
+export default class App extends Component<IProps> {
+  public state: { hasErrors: boolean };
+
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      hasErrors: false
+    };
+  }
+
+  public componentDidCatch(err: Error, info: React.ErrorInfo) {
+    this.setState({
+      hasErrors: true
+    });
+    // tslint:disable-next-line:no-console
+    console.error(err, info);
+  }
+
+  public render() {
+    if (this.state.hasErrors) {
+      return <div>Oh no, something went wrong in {'events-app'}</div>;
+    }
+    return <div>Events plugin loaded!</div>;
+  }
+}
