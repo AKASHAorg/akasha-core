08faeb884ea94df1dbc26fa7da5ca91e62f0dc3c
Marius Darila <marius.darila@gmail.com>
2019-09-17 18:32:04 +0200

    fix(db): remove remaping of settings

--- sdk-packages/db/src/settings.ts
+++ sdk-packages/db/src/settings.ts
@@ -3,7 +3,7 @@ import { DB_NAME, DB_PASSWORD, DB_SYNC_ENDPOINT } from './constants';
 
 const settings: IGeneralSettings = [
   [DB_SYNC_ENDPOINT, 'http://localhost:3000/db'],
-  [DB_NAME, 'akashadb.alpha0'],
+  [DB_NAME, 'akashadb_alpha0'],
   [DB_PASSWORD, 'SOMELONGPASSWORD123asdasdasda'],
 ];
 
