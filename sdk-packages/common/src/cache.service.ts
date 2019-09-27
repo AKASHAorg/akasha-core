42e76f176ac6aec8fa8f80c49fab0a58bf2eb664
Marius Darila <marius.darila@gmail.com>
2019-09-27 16:36:44 +0200

    refactor(services): add cache first strategy

--- sdk-packages/common/src/cache.service.ts
+++ sdk-packages/common/src/cache.service.ts
@@ -9,7 +9,7 @@ import Stash from '@akashaproject/sdk-runtime/lib/Stash';
 import { CACHE_SERVICE } from './constants';
 
 const service: AkashaService = () => {
-  const stash = new Stash({ max: 500, maxAge: 1000 * 60 * 60 });
+  const stash = new Stash({ max: 512, maxAge: 1000 * 60 * 60 });
   return registerServiceMethods({ stash: createServiceMethod(stash) });
 };
 
--- sdk-packages/common/src/web3.service.ts
+++ sdk-packages/common/src/web3.service.ts
@@ -7,25 +7,29 @@ import {
 } from '@akashaproject/sdk-core/lib/utils';
 import { ethers } from 'ethers';
 import { EthProviders, WEB3_SERVICE } from './constants';
+import commonServices, { CACHE_SERVICE } from './constants';
 import getProvider from './web3.methods/provider';
 
 const service: AkashaService = (invoke, log) => {
-  let web3Provider;
-
   const regen = async provider => {
+    const cacheService = invoke(commonServices[CACHE_SERVICE]);
     log.info('returning a new ethers provider instance');
     const { getSettings } = invoke(coreServices.SETTINGS_SERVICE);
-    web3Provider = await getProvider(getSettings, provider);
+    const web3Provider = await getProvider(getSettings, provider);
+    cacheService.stash().set(commonServices[WEB3_SERVICE], web3Provider);
     return web3Provider;
   };
 
   // to force regen() on the next web3 call
   const destroy = () => {
-    web3Provider = null;
+    const stash: any = invoke(commonServices[CACHE_SERVICE]).stash();
+    stash.remove(commonServices[WEB3_SERVICE]);
   };
 
   // fetch an existing instance or create web3Provider
   const web3 = async (provider: EthProviders) => {
+    const stash: any = invoke(commonServices[CACHE_SERVICE]).stash();
+    const web3Provider = stash.get(commonServices[WEB3_SERVICE]);
     if (!web3Provider) {
       return await regen(provider);
     }
@@ -33,7 +37,10 @@ const service: AkashaService = (invoke, log) => {
     return web3Provider;
   };
 
-  const getWeb3Instance = () => web3Provider;
+  const getWeb3Instance = () => {
+    const stash: any = invoke(commonServices[CACHE_SERVICE]).stash();
+    return stash.get(commonServices[WEB3_SERVICE]);
+  };
 
   // wrapper
   const wallet = createServiceMethod(ethers.Wallet);
