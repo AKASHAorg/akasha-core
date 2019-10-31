557a9f14f954c205a5caafa8195cf9974bee3cde
Marius Darila <marius.darila@gmail.com>
2019-10-31 15:48:37 +0100

    fix(): commons module extraction

--- examples/ui/feed-app/src/index.tsx
+++ examples/ui/feed-app/src/index.tsx
@@ -15,7 +15,7 @@ const app = new AppLoader({
 // here you can rename different modules before passing them to the plugins
 // it also enables dynamic build of dependencies
 // ex: [['commons', obj1],['entries', obj2]] becomes at plugin level {commons: obj1, entries: obj2}
-const commonModule = ['commons', sdk.modules.commons];
+const commonModule = ['commons', sdk.commons];
 const promises: void[] = [
   app.registerPlugin(FeedPlugin, null, [commonModule]),
   app.registerPlugin(ProfilePlugin, null, [commonModule]),
