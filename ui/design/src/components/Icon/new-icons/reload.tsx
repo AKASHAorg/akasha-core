bebd930c762eb26f8d0f6072b6c3cd4d9bf4518b
Vali Cotea <quininez@users.noreply.github.com>
2019-11-06 13:03:24 +0200

    chore(): cleanup (#207)
    * chore(): cleanup
    
    * chore(deps): bump @types/node from 12.12.3 to 12.12.6 (#208)
    
    Bumps [@types/node](https://github.com/DefinitelyTyped/DefinitelyTyped/tree/HEAD/types/node) from 12.12.3 to 12.12.6.
    - [Release notes](https://github.com/DefinitelyTyped/DefinitelyTyped/releases)
    - [Commits](https://github.com/DefinitelyTyped/DefinitelyTyped/commits/HEAD/types/node)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
    * chore(): cleanup
    
    * chore(): background for cards, menu textIcon style
    
    * chore(): cleanup tabs
    
    * chore(): make shadow themable
    
    * chore()

---
+++ ui/design/src/components/Icon/new-icons/reload.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Reload = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" {...props}>
+    <g fill="none" fillRule="evenodd" transform="translate(-856 -2714)">
+      <g stroke="#2E3747" strokeLinecap="round" transform="translate(858 2714)">
+        <polyline points="5.028 .728 8.268 2.631 6.339 5.828" />
+        <polyline points="10.942 19.323 7.734 17.365 9.717 14.2" />
+        <path d="M13.28 4.78947368C16.1962667 7.66631579 16.1962667 12.3336842 13.28 15.2105263 11.8218667 16.6484211 9.91146667 17.3684211 8.00106667 17.3684211M2.72 15.2105263C-.196266667 12.3336842-.196266667 7.66631579 2.72 4.78947368 4.17813333 3.35157895 6.08853333 2.63157895 8.00106667 2.63157895" />
+      </g>
+    </g>
+  </svg>
+);
+
+export default Reload;
--- ui/design/src/styles/colors-dark.ts
+++ ui/design/src/styles/colors-dark.ts
@@ -19,11 +19,13 @@ const colors: IColorsDark = {
   // define use cases for colors
   primaryText: '#132540', // darkBlue
   secondaryText: '#949EB3', // grey
+  background: '#FFF', // white
   lightBackground: '#EDF0F5', // lightGrey
   darkBackground: '#000C20',
   accent: '#4E71FF', // blue
   border: '#EDF0F5', // lightGrey
   secondary: '#000C20', // darkGrey
   secondaryOpacity: 'rgba(0, 12, 32, 0.5)', // darkgrey with 0.5 opacity
+  shadow: '0 8px 24px 0 rgba(83,98,124,0.06)',
 };
 export default colors;
