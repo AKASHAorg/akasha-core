f4e1fe7e524d4f8a565285188da6b5b0ccffd3cb
andreysaleba <andrey.saleba@gmail.com>
2019-11-21 16:29:02 +0300

    Search input component redesign (#246)
    * Search input component redesign
    
    * Changed padding fixed values to theme values

---
+++ ui/design/src/components/Input/search-input-content/index.ts
@@ -0,0 +1,5 @@
+import SearchInputApps from './search-input-apps';
+import SearchInputTags from './search-input-tags';
+import SearchInputUsers from './search-input-users';
+
+export { SearchInputUsers, SearchInputTags, SearchInputApps };
