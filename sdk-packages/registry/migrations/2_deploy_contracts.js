16b8fbb7f750aad9381a5e75e855275f442e9c03
Marius Darila <marius.darila@gmail.com>
2019-12-06 14:00:48 +0100

    refactor(): build config && add makefile steps

--- sdk-packages/registry/migrations/2_deploy_contracts.js
+++ sdk-packages/registry/migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
-const ConvertLib = artifacts.require('ConvertLib');
-const MetaCoin = artifacts.require('MetaCoin');
+const PackageRegistry = artifacts.require('PackageRegistry');
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.deploy(PackageRegistry);
 };
