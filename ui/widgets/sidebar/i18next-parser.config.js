7d595445d756c2a03318fe8fe171a33106a4d25a
Marius Darila <3396463+kenshyx@users.noreply.github.com>
2019-12-11 19:04:15 +0100

    Feature/extract i18 netlify (#303)
    * chore(): trigger build
    
    * chore(): trigger only translations
    
    * chore(): test abs path
    
    * chore(): change script for i18n-parse
    
    * chore(): prep for deploy
    
    * chore(): cleanup

--- ui/widgets/sidebar/i18next-parser.config.js
+++ ui/widgets/sidebar/i18next-parser.config.js
@@ -1,11 +1,7 @@
-
 const ns = 'ui-widget-sidebar';
 
 module.exports = {
   locales: ['en', 'ro'],
-  input: ['./src/components/**/*.tsx'],
-  output: '../../../locales/$LOCALE/$NAMESPACE.json',
   saveMissingTo: 'all',
   defaultNamespace: ns,
-  useKeysAsDefaultValue: true,
 };
