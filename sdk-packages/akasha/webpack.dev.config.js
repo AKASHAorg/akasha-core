02b19a5a6c3c4f352cea6aff740fba0fbc52ab84
Marius Darila <3396463+kenshyx@users.noreply.github.com>
2020-02-28 17:07:03 +0100

    feat(caching): add workbox sw handler (#431)

--- sdk-packages/akasha/webpack.dev.config.js
+++ sdk-packages/akasha/webpack.dev.config.js
@@ -2,7 +2,8 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-// const WorkboxPlugin = require('workbox-webpack-plugin');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
 const config = {
   entry: './lib/index.js',
   output: {
@@ -45,7 +46,10 @@ const config = {
       template: path.resolve(__dirname, '../../examples/ui/feed-app/public/template-index.html'),
       inject: true,
     }),
-    // new WorkboxPlugin.GenerateSW()
+    new InjectManifest({
+      swSrc: './lib/sw.js',
+      swDest: 'sw.js'
+    })
   ],
   module: {
     rules: [
