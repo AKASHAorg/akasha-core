b279b49f67ed24425765bad3efb4cea0bce1bde9
Sever Abibula <SeverS@users.noreply.github.com>
2020-03-25 17:25:01 +0200

    chore(): add themeselector wrapper (#439)

--- ui/storybook/.storybook/config.js
+++ ui/storybook/.storybook/config.js
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { addDecorator, configure } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import { withA11y } from '@storybook/addon-a11y';
@@ -7,13 +8,19 @@ import { withThemesProvider } from 'storybook-addon-styled-component-theme';
 // import prepared themes from theme-sync-addon
 // themes already have name prop.
 import DS from '@akashaproject/design-system';
+import { ThemeSelector } from '@akashaproject/design-system/lib/styles/themes/utils/theme-selector';
 
 const { lightTheme, darkTheme, Grommet } = DS;
 addDecorator(withInfo);
 addDecorator(withA11y);
 addDecorator(withKnobs);
 // add themes and Grommet as custom theme provider
-addDecorator(withThemesProvider([lightTheme, darkTheme], Grommet));
+addDecorator(withThemesProvider([lightTheme, darkTheme], (props) => {
+  return React.createElement(ThemeSelector, {
+    settings: {activeTheme: 'Light-Theme'},
+    availableThemes: [lightTheme, darkTheme]
+  }, props.children);
+}));
 // automatically import all files ending in *.stories.tsx
 const req = require.context('../src/components', true, /.stories.tsx?$/);
 
