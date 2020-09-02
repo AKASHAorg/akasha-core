4cecea27955575ef7f7de7be1b31e7104e8e721d
Vali Cotea <quininez@users.noreply.github.com>
2020-09-02 12:43:35 +0300

    Layout fix (#566)
    * fix(): brand label routing issue
    
    * fix(): update styled components types for widgets
    
    * fix(): iphone sidebar height issue
    
    * chore(): cleanup

--- ui/widgets/layout/src/components/styled-slots.ts
+++ ui/widgets/layout/src/components/styled-slots.ts
@@ -1,25 +1,37 @@
 import DS from '@akashaproject/design-system';
-import { css, StyledComponent } from 'styled-components';
+import { css } from 'styled-components';
 import { BaseContainer } from './styled-containers';
 
 const { styled } = DS;
 
-export const SidebarSlot: StyledComponent<any, any, any, any> = styled(BaseContainer)`
+export const SidebarSlot = styled(BaseContainer)<{ visible: boolean }>`
   flex-grow: 1;
+  @media screen and (max-width: ${props => props.theme.breakpoints.small.value}px) {
+    ${props => {
+      if (props.visible) {
+        return css`
+          height: calc(100vh - 3rem);
+        `;
+      }
+      return css`
+        display: none;
+      `;
+    }}
+  }
 `;
 
-export const TopbarSlot: StyledComponent<any, any, any, never> = styled.div`
+export const TopbarSlot = styled.div`
   z-index: 100;
   position: sticky;
   top: 0;
   width: 100%;
 `;
 
-export const PluginSlot: StyledComponent<any, any, any, never> = styled(BaseContainer)`
+export const PluginSlot = styled(BaseContainer)`
   flex-grow: 1;
   flex-shrink: 1;
   margin-top: 0.5em;
-  ${props => css<any>`
+  ${props => css`
     @media screen and (min-width: ${props.theme.breakpoints.small.value}px) {
       max-width: 30em;
     }
@@ -32,9 +44,9 @@ export const PluginSlot: StyledComponent<any, any, any, never> = styled(BaseCont
   `}
 `;
 
-export const WidgetSlot: StyledComponent<any, any, any, never> = styled(BaseContainer)`
+export const WidgetSlot = styled(BaseContainer)`
   display: none;
-  ${props => css<any>`
+  ${props => css`
     @media screen and (min-width: ${props.theme.breakpoints.small.value}px) {
       max-width: 30em;
       display: flex;
@@ -54,6 +66,6 @@ export const WidgetSlot: StyledComponent<any, any, any, never> = styled(BaseCont
     width: 100%;
   }
 `;
-export const ModalSlot: StyledComponent<any, any, any, never> = styled.div`
+export const ModalSlot = styled.div`
   z-index: 300;
 `;
