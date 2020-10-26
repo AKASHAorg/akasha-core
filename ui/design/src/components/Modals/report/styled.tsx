a3b92ece02ef4f270c4588b25e60c7a32aac8663
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2020-10-26 23:01:53 +0100

    style(ui): adjust report modal styles

--- ui/design/src/components/Modals/report/styled.tsx
+++ ui/design/src/components/Modals/report/styled.tsx
@@ -2,7 +2,7 @@ import styled, { css } from 'styled-components';
 import { Box, Text } from 'grommet';
 
 export const StyledBox = styled(Box)`
-  width: 50%;
+  width: 33%;
   position: absolute;
   top: 50%;
   left: 50%;
@@ -21,12 +21,17 @@ export const StyledTextArea = styled.textarea`
   background: transparent;
   border: none;
   box-sizing: content-box;
+  border-bottom: ${props => `0.5px solid ${props.theme.colors.secondaryText}`};
   ${props => css`
     font-family: ${props.theme.shapes.fontFamily};
     font-size: ${props.theme.shapes.fontSizes.large.size};
     line-height: ${props.theme.shapes.fontSizes.large.height};
     color: ${props.theme.colors.primaryText};
   `}
+  &::placeholder {
+    font-family: ${props => props.theme.shapes.fontFamily};
+    color: ${props => props.theme.colors.secondaryText};
+  }
 `;
 
 export const HiddenSpan = styled.span`
