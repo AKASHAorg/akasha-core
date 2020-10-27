78e406a51d1d815d3d6d4ef0482d79b135ac0e5a
Vali Cotea <quinine35@gmail.com>
2020-10-27 17:35:38 +0200

    refactor(): use DS login modal in login widget and new post page, add commons module to topbar widget

--- ui/widgets/login/src/components/login-widget.tsx
+++ ui/widgets/login/src/components/login-widget.tsx
@@ -3,76 +3,18 @@ import { useTranslation } from 'react-i18next';
 import DS from '@akashaproject/design-system';
 import { LearnMoreTutorial } from './tutorial-modal';
 import LoginWidgetIllustration from './icons/login-widget-illustration';
-import EthereumIcon from './icons/ethereum-icon';
 import { useLoginState } from '../state';
-import { EthProviders } from '@akashaproject/ui-awf-typings';
 
-const {
-  IconLink,
-  Icon,
-  Text,
-  styled,
-  Box,
-  ModalRenderer,
-  LoginCTAWidgetCard,
-  EthProviderListModal,
-  EthProviderModal,
-  createGlobalStyle,
-  useGlobalLogin,
-} = DS;
-
-const METAMASK_PROVIDER = 'metamask';
-const WALLETCONNECT_PROVIDER = 'walletconnect';
-
-const ETH_PROVIDERS = {
-  [METAMASK_PROVIDER]: 2,
-  [WALLETCONNECT_PROVIDER]: 3,
-};
-
-const VideoTutorialLink = styled(IconLink)`
-  vertical-align: top;
-`;
+const { ModalRenderer, LoginCTAWidgetCard, LoginModal, useGlobalLogin } = DS;
 
 export interface ILoginWidgetProps {
   sdkModules: any;
   logger: any;
   globalChannel?: any;
   layoutConfig: {
-    modalSlotId?: string;
+    modalSlotId: string;
   };
 }
-const IconCircleBg = styled.div`
-  border-radius: 50%;
-  background: ${props => props.theme.colors.border};
-  width: 64px;
-  height: 64px;
-  text-align: center;
-  padding: 14px 12px;
-`;
-const HorizontalDashedLine = styled.div`
-  border-bottom: 1px dashed ${props => props.theme.colors.border};
-  height: 1px;
-  flex: 1;
-`;
-
-export interface IEthProviderIllustrationProps {
-  providerIcon: React.ReactElement;
-}
-
-const EthProviderModalIllustration: React.FC<IEthProviderIllustrationProps> = props => (
-  <Box direction="row" align="center" justify="center" margin="1.5em 0">
-    <Box
-      alignContent="between"
-      justify="between"
-      style={{ maxWidth: '67%', width: '100%', alignItems: 'center' }}
-      direction="row"
-    >
-      <IconCircleBg>{props.providerIcon}</IconCircleBg>
-      <HorizontalDashedLine />
-      <EthereumIcon />
-    </Box>
-  </Box>
-);
 
 const LoginWidget: React.FC<ILoginWidgetProps> = props => {
   const { t } = useTranslation();
@@ -92,40 +34,27 @@ const LoginWidget: React.FC<ILoginWidgetProps> = props => {
     },
   );
 
-  const handleLearnMore = () => {
+  const handleTutorialModalOpen = () => {
     actions.setLearnMoreVisibility({ isVisible: true });
-  };
-
-  const handleLoginSelect = () => {
-    actions.setProviderListVisibility({ isVisible: true });
-  };
-
-  const handleMetamaskLogin = () => {
-    const provider: EthProviders = ETH_PROVIDERS[METAMASK_PROVIDER];
-    actions.authorize(provider);
-  };
-
-  const handleWalletConnectLogin = () => {
-    const provider: EthProviders = ETH_PROVIDERS[WALLETCONNECT_PROVIDER];
-    actions.authorize(provider);
-  };
-
-  const handleProvidersModalClose = () => {
-    actions.setProviderListVisibility({ isVisible: false });
   };
 
   const handleTutorialModalClose = () => {
     actions.setLearnMoreVisibility({ isVisible: false });
   };
 
-  const handleProviderClick = (
-    providerId: typeof METAMASK_PROVIDER | typeof WALLETCONNECT_PROVIDER,
-  ) => {
-    actions.setProviderListVisibility({ isVisible: false });
-    actions.setSelectedProvider({ selectedProvider: providerId });
+  const handleLoginModalOpen = () => {
+    actions.setShowLoginModal({ isVisible: true });
   };
-  const handleProviderModalClose = () => {
-    actions.setSelectedProvider({ selectedProvider: null });
+  const handleLogin = (provider: 2 | 3) => {
+    actions.authorize(provider);
+  };
+
+  const handleLoginModalClose = () => {
+    actions.setShowLoginModal({ isVisible: false });
+  };
+
+  const handleTutorialLinkClick = () => {
+    /* goto tutorials */
   };
 
   if (state.data.jwtToken) {
@@ -148,44 +77,24 @@ const LoginWidget: React.FC<ILoginWidgetProps> = props => {
         )}
         learnMoreLabel={t('Learn More')}
         connectLabel={t('Sign Up')}
-        onLearnMoreClick={handleLearnMore}
-        onLoginClick={handleLoginSelect}
+        onLearnMoreClick={handleTutorialModalOpen}
+        onLoginClick={handleLoginModalOpen}
       />
-      <ModalRenderer slotId={props.layoutConfig.modalSlotId}>
-        {state.data.providerListVisibility && (
-          <EthProviderListModal
-            onProviderClick={handleProviderClick}
-            onModalClose={handleProvidersModalClose}
-            providers={[
-              {
-                id: METAMASK_PROVIDER,
-                logo: <Icon type="metamask" size="xl" />,
-                title: t('MetaMask'),
-                description: t('Connect to your MetaMask wallet'),
-              },
-              {
-                id: WALLETCONNECT_PROVIDER,
-                logo: <Icon type="walletconnect" size="xl" />,
-                title: t('WalletConnect'),
-                description: t('Scan with WalletConnect'),
-              },
-            ]}
-            footer={
-              <Text textAlign="center" margin={{ top: '1em' }} style={{ userSelect: 'none' }}>
-                <>{t('What is a wallet? How do i get an Ethereum address?')}</>
-                <VideoTutorialLink
-                  onClick={() => {
-                    /* @TODO: do something! */
-                  }}
-                  icon={<Icon type="media" />}
-                  label={t('See Video Tutorial') as string}
-                  padded={true}
-                />
-              </Text>
-            }
-          />
-        )}
-        {state.data.learnMoreVisibility && (
+
+      <LoginModal
+        slotId={props.layoutConfig.modalSlotId}
+        onLogin={handleLogin}
+        onModalClose={handleLoginModalClose}
+        showModal={state.data.showLoginModal}
+        tutorialLinkLabel={t('Tutorial')}
+        metamaskModalHeadline={t('Connecting')}
+        metamaskModalMessage={t('Please complete the process in your wallet')}
+        onTutorialLinkClick={handleTutorialLinkClick}
+        helpText={t('What is a wallet? How do i get an Ethereum address?')}
+      />
+
+      {state.data.learnMoreVisibility && (
+        <ModalRenderer slotId={props.layoutConfig.modalSlotId}>
           <LearnMoreTutorial
             onModalClose={handleTutorialModalClose}
             slides={[
@@ -198,45 +107,10 @@ const LoginWidget: React.FC<ILoginWidgetProps> = props => {
               { title: 'Test Title', content: 'Test Content' },
             ]}
           />
-        )}
-        {state.data.selectedProvider === METAMASK_PROVIDER && (
-          <EthProviderModal
-            illustration={
-              <EthProviderModalIllustration providerIcon={<Icon type="metamask" size="lg" />} />
-            }
-            headLine={t('Just a few more steps! We are almost there…')}
-            message={t('Approve the message in your Web3 wallet to continue')}
-            onLogin={handleMetamaskLogin}
-            onModalClose={handleProviderModalClose}
-          />
-        )}
-        {state.data.selectedProvider === WALLETCONNECT_PROVIDER && (
-          <WalletConnectModalTrigger onLogin={handleWalletConnectLogin} />
-        )}
-      </ModalRenderer>
+        </ModalRenderer>
+      )}
     </>
   );
 };
-export interface IWalletConnectModalProps {
-  onLogin: () => void;
-}
 
-const GlobalStyle = createGlobalStyle`
-  #walletconnect-wrapper {
-    position: absolute;
-    .walletconnect-modal__base {
-      top: 0;
-      transform: none;
-      margin-top: 2em;
-    }
-  }
-`;
-
-const WalletConnectModalTrigger: React.FC<IWalletConnectModalProps> = props => {
-  React.useEffect(() => {
-    props.onLogin();
-  }, []);
-
-  return <GlobalStyle />;
-};
 export default LoginWidget;
