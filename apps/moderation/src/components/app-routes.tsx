1a9ddf679b3811e91eca349984aea19a56720738
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2020-12-01 21:34:08 +0100

    refactor(moderation): update routes and implement modals

--- apps/moderation/src/components/app-routes.tsx
+++ apps/moderation/src/components/app-routes.tsx
@@ -1,16 +1,21 @@
 import * as React from 'react';
+import { useTranslation } from 'react-i18next';
 import { RootComponentProps } from '@akashaproject/ui-awf-typings';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import DS from '@akashaproject/design-system';
 
-import routes, { HOME, rootRoute } from '../routes';
+import routes, { HOME, RESTRICTED, UNAUTHENTICATED, rootRoute } from '../routes';
 
 import ContentList from './content-list';
+import PromptAuthentication from './prompt-authentication';
+import PromptAuthorization from './prompt-authorization';
 
-const { Box, ViewportSizeProvider } = DS;
+const { Box, ModalRenderer, ViewportSizeProvider } = DS;
 
 const AppRoutes: React.FC<RootComponentProps> = props => {
   const { layout } = props;
+
+  const { t } = useTranslation();
 
   return (
     <ViewportSizeProvider>
@@ -27,6 +32,27 @@ const AppRoutes: React.FC<RootComponentProps> = props => {
                 // showLoginModal={showLoginModal}
                 // onError={onError}
               />
+            </Route>
+            <Route path={routes[UNAUTHENTICATED]}>
+              <ModalRenderer slotId={layout.app.modalSlotId}>
+                <PromptAuthentication
+                  titleLabel={t('This page is restricted to Ethereum World Moderators')}
+                  subtitleLabel={t(
+                    'To view this page, you must be an Ethereum World Moderator and log in with your wallet to continue.',
+                  )}
+                  buttonLabel={t('Connect a wallet')}
+                />
+              </ModalRenderer>
+            </Route>
+            <Route path={routes[RESTRICTED]}>
+              <ModalRenderer slotId={layout.app.modalSlotId}>
+                <PromptAuthorization
+                  titleLabel={t('You must be an Ethereum World Moderator to access this page')}
+                  subtitleLabel={t(
+                    'The wallet you connected does not match a moderator account in our system. Please try again with the correct wallet.',
+                  )}
+                />
+              </ModalRenderer>
             </Route>
             <Redirect exact={true} from={rootRoute} to={routes[HOME]} />
           </Switch>
--- apps/moderation/src/routes.ts
+++ apps/moderation/src/routes.ts
@@ -1,6 +1,10 @@
 export const HOME = 'Home';
+export const RESTRICTED = 'Restricted';
+export const UNAUTHENTICATED = 'Unauthenticated';
 export const rootRoute = '/moderation-app';
 
 export default {
   [HOME]: `${rootRoute}/home`,
+  [RESTRICTED]: `${rootRoute}/restricted`,
+  [UNAUTHENTICATED]: `${rootRoute}/unauthenticated`,
 };
