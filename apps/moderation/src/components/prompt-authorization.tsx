e98cab91d619b2c70f5b6d36bf62f4a8aff51bd6
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2020-12-01 21:32:41 +0100

    feat(moderation): create modal pages - update styles

---
+++ apps/moderation/src/components/prompt-authorization.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import DS from '@akashaproject/design-system';
+
+import illustration from '../assets/illustration-blocked';
+
+import { ContentWrapper, PageWrapper } from './styled';
+
+const { Box, Text, Image } = DS;
+
+export interface IPromptAuthorizationProps {
+  titleLabel: string;
+  subtitleLabel: string;
+}
+
+const PromptAuthorization: React.FC<IPromptAuthorizationProps> = props => {
+  const { titleLabel, subtitleLabel } = props;
+  return (
+    <PageWrapper>
+      <ContentWrapper pad={{ top: '0rem', horizontal: '1.4rem', bottom: '1.4rem' }}>
+        <Box height="14rem" width="14rem" margin={{ bottom: 'small' }} alignSelf="center">
+          <Image fit="contain" src={illustration} />
+        </Box>
+        <Text
+          weight={600}
+          margin={{ bottom: 'large', horizontal: 'auto' }}
+          size="large"
+          textAlign="center"
+        >
+          {titleLabel}
+        </Text>
+        <Text
+          weight="normal"
+          margin={{ top: 'xsmall' }}
+          color="secondaryText"
+          size="medium"
+          textAlign="center"
+        >
+          {subtitleLabel}
+        </Text>
+      </ContentWrapper>
+    </PageWrapper>
+  );
+};
+
+export default PromptAuthorization;
--- apps/moderation/src/components/styled/index.ts
+++ apps/moderation/src/components/styled/index.ts
@@ -2,6 +2,32 @@ import DS from '@akashaproject/design-system';
 
 const { styled, Box } = DS;
 
-export const StyledBox: any = styled(Box)`
+const StyledBox: any = styled(Box)`
   background-color: ${props => props.theme.colors.accentLight};
 `;
+
+const PageWrapper: any = styled(Box)`
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 199;
+  background: ${props => props.theme.colors.modalBackgroundAlt};
+`;
+
+const ContentWrapper: any = styled(Box)`
+  width: 28%;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  margin: auto;
+  background: white;
+  border-radius: 0.5rem;
+  span {
+    letter-spacing: 0.03em;
+  }
+`;
+
+export { StyledBox, PageWrapper, ContentWrapper };
