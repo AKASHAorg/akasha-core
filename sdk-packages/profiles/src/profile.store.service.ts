6914d09879df2bb85e7dbb33415a6c596d65f3eb
Marius Darila <3396463+kenshyx@users.noreply.github.com>
2020-12-16 12:47:02 +0100

    add comments api (#617)
    * chore(): add comments api
    
    * chore(): export api to sdk
    
    * chore(): linting
    
    * chore(): added extracted fields for profile
    
    * chore(): add profile default props list

--- sdk-packages/profiles/src/profile.store.service.ts
+++ sdk-packages/profiles/src/profile.store.service.ts
@@ -25,10 +25,10 @@ const service: AkashaService = (invoke, log) => {
     return result.data;
   };
 
-  const makeDefaultProvider = async (opt: LinkedProperty) => {
+  const makeDefaultProvider = async (opt: LinkedProperty[]) => {
     const token = await invoke(authServices[AUTH_SERVICE]).getToken();
     const mutation = `
-  mutation MakeDefaultProvider($data: DataProviderInput) {
+  mutation MakeDefaultProvider($data: [DataProviderInput]) {
        makeDefaultProvider(data: $data)
   }`;
     const result = await runGQL({
@@ -79,13 +79,24 @@ const service: AkashaService = (invoke, log) => {
   query GetProfile($ethAddress: String!) {
        getProfile(ethAddress: $ethAddress) {
          ${fields.join(' ')}
+         providers{
+          provider
+          property
+          value
+         }
+         default{
+          provider
+          property
+          value
+         }
        }
       }`;
-    return runGQL({
+    const result = await runGQL({
       query: query,
       variables: { ethAddress: opt.ethAddress },
       operationName: 'GetProfile',
     });
+    return result.data;
   };
 
   const follow = async (opt: { ethAddress: string }) => {
