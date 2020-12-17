0038b478c6236a02a4dfaf5fc2479bb8a5abefc4
SeverS <5903809+SeverS@users.noreply.github.com>
2020-12-17 12:02:01 +0200

    chore(): minor change

--- ui/storybook/src/components/FormCards.stories.tsx
+++ ui/storybook/src/components/FormCards.stories.tsx
@@ -63,7 +63,7 @@ storiesOf('Cards/Form Cards', module)
         onSave={() => action('Form submitted')('Synthetic Event')}
         onCancel={() => action('Form Cancelled')('Synthetic Event')}
         validateEns={() => action('validating ens')('Synthetic Event')}
-        validEns={boolean('valid ens', true)}
+        validEns={boolean('Valid ens', true)}
         changeButtonLabel={text('Label for Change ENS Button', 'Change ENS')}
         userNameProviderOptions={[
           { name: 'local', label: text('Label for local usernames', 'Do not use ENS') },
