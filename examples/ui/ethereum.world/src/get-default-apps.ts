e32143938e32cc8239742e0958d2a3d6cff3f1e0
SeverS <sever.abi@gmail.com>
2021-01-22 21:15:20 +0200

    chore(): remove ens and 3box, some cleanups

--- examples/ui/ethereum.world/src/get-default-apps.ts
+++ examples/ui/ethereum.world/src/get-default-apps.ts
@@ -3,14 +3,6 @@ import { MenuItemAreaType } from '@akashaproject/ui-awf-typings/lib/app-loader';
 export const getDefaultApps = async (appsPublicPath, pluginsPublicPath, _widgetsPublicPath) => {
   // get default apps and widgets from somewhere
   const appSources = [
-    // {
-    //   src: `${appsPublicPath}/ens/index.js`,
-    //   name: 'akashaproject__app_ens_integration',
-    //   moduleName: './app',
-    //   config: {
-    //     area: MenuItemAreaType.AppArea,
-    //   },
-    // },
     {
       src: `${appsPublicPath}/moderation/index.js`,
       name: 'akashaproject__app_moderation_ewa',
@@ -56,16 +48,7 @@ export const getDefaultApps = async (appsPublicPath, pluginsPublicPath, _widgets
     },
   ];
   // widgets
-  const widgetSources = [
-    // {
-    //   src: `${widgetsPublicPath}/login/index.js`,
-    //   name: 'akashaproject__ui_widget_login',
-    //   moduleName: './app',
-    //   config: {
-    //     notOnMobile: true,
-    //   },
-    // },
-  ];
+  const widgetSources = [];
 
   return {
     apps: appSources,
