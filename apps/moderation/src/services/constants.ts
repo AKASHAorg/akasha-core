5902a25026916aebef1824e73ae92b4cf5994008
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2021-02-09 11:36:46 +0100

    fix: remove trailing slash

--- apps/moderation/src/services/constants.ts
+++ apps/moderation/src/services/constants.ts
@@ -1,6 +1,6 @@
 const BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://api.ethereum.world/'
+    ? 'https://api.ethereum.world'
     : 'https://moderation.akasha.network';
 
 export const BASE_DECISION_URL = `${BASE_URL}/decisions`;
