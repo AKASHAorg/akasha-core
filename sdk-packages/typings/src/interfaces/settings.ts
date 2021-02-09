40d7f18371d155b27ca0efee4dfa01c2dd3639d4
Marius Darila <marius.darila@gmail.com>
2021-02-09 03:28:02 +0100

    chore(): omit refactored sdk

--- sdk-packages/typings/src/interfaces/settings.ts
+++
@@ -1,7 +0,0 @@
-import { ObservableCallResult } from './responses';
-
-export default interface ISettingsService {
-  set(moduleName: string, value: object): ObservableCallResult<object>;
-  get(moduleName: string): ObservableCallResult<object>;
-  remove(moduleName: string): void;
-}
