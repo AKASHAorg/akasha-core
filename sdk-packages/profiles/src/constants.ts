acef544bed9c01a72e04b531dd617d7683327c49
Marius Darila <marius.darila@gmail.com>
2021-02-09 12:43:46 +0100

    chore(): change buck entry

--- sdk-packages/profiles/src/constants.ts
+++ sdk-packages/profiles/src/constants.ts
@@ -3,7 +3,7 @@ import { buildServicePath } from '@akashaproject/sdk-core/lib/utils';
 
 export const PROFILE_STORE = 'PROFILE_STORE';
 export const PROFILE_LEGACY = 'PROFILE_LEGACY';
-export const PROFILE_MEDIA_FILES = 'awf.media.upload';
+export const PROFILE_MEDIA_FILES = 'awf.rc1.media.upload';
 export interface LinkedProperty {
   provider: string;
   property: string;
