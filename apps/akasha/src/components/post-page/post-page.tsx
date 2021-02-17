d47cf40ee0552ae562170d96a064a6613eadb55f
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2021-02-17 10:19:43 +0100

    fix(apps, ui): resolve moderation-app profiles - adjust report success modal label

--- apps/akasha/src/components/post-page/post-page.tsx
+++ apps/akasha/src/components/post-page/post-page.tsx
@@ -349,7 +349,7 @@ const PostPage: React.FC<IPostPage & RootComponentProps> = props => {
           <ToastProvider autoDismiss={true} autoDismissTimeout={5000}>
             <ReportModal
               titleLabel={t('Report a Post')}
-              successTitleLabel={t('Thank you for helping us keep Ethereum World Safe! 🙌')}
+              successTitleLabel={t('Thank you for helping us keep Ethereum World safe! 🙌')}
               successMessageLabel={t('We will investigate this post and take appropriate action.')}
               optionsTitleLabel={t('Please select a reason')}
               optionLabels={[
