3fdc63cb9d36347d8f53d01a8a84af6fd268da26
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2021-03-11 12:47:37 +0100

    chore(ui): remove redundant component

--- ui/design/src/components/Cards/index.ts
+++ ui/design/src/components/Cards/index.ts
@@ -13,7 +13,6 @@ import { EntryCardHidden } from './entry-cards/entry-card-hidden';
 import BoxFormCard from './form-cards/box-form-card';
 import EnsFormCard from './form-cards/ens-form-card';
 import ProfileCard from './profile-cards/profile-card';
-import ProfileCardMod from './profile-cards/profile-card-mod';
 import ProfileWidgetCard from './profile-cards/profile-widget-card';
 import { ProfileMiniCard } from './profile-cards/profile-mini-card';
 import AppInfoWidgetCard from './widget-cards/app-info-widget-card';
@@ -35,7 +34,6 @@ export {
   TopicsWidgetCard,
   MiniInfoWidgetCard,
   ProfileCard,
-  ProfileCardMod,
   ProfileWidgetCard,
   ProfileMiniCard,
   CommentEditor,
