9b4ff1acaff0f734c3fb8db38df886b35d34c195
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2021-03-16 13:51:42 +0100

    chore: create styled modal button

--- ui/design/src/components/Modals/common/styled-modal.ts
+++ ui/design/src/components/Modals/common/styled-modal.ts
@@ -1,5 +1,17 @@
 import { Box, Layer } from 'grommet';
-import styled from 'styled-components';
+import styled, { css, DefaultTheme, StyledComponent, ThemedStyledProps } from 'styled-components';
+
+import { Button } from '../../Buttons';
+import { IButtonProps } from '../../Buttons/default-buttons/button';
+
+type TModalButton = StyledComponent<
+  (props: IButtonProps & { isMobile: boolean }) => JSX.Element,
+  DefaultTheme,
+  {},
+  never
+>;
+
+type TModalButtonProps = ThemedStyledProps<IButtonProps & { isMobile: boolean }, DefaultTheme>;
 
 const StyledLayer = styled(Layer)`
   max-width: 36.313em;
@@ -20,4 +32,19 @@ const ModalWrapper = styled(Box)`
   background: ${props => props.theme.colors.modalBackground};
 `;
 
-export { StyledLayer, ModalWrapper };
+const ModalButton: TModalButton = styled(Button)`
+  height: auto;
+  border-width: 0.1rem;
+  font-size: ${(props: TModalButtonProps) => (props.isMobile ? '0.9rem' : '0.8rem')};
+  padding: ${(props: TModalButtonProps) => (props.isMobile ? '0.75rem 0' : '0.3rem 0.7rem')};
+  ${(props: TModalButtonProps) => {
+    if (props.isMobile) {
+      return css`
+        width: 50%;
+      `;
+    }
+    return;
+  }}
+`;
+
+export { StyledLayer, ModalWrapper, ModalButton };
