56c1e784d612e5d0d30ea77cd7f7f5379f1632df
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2021-03-16 16:10:26 +0100

    chore: adjust styles

--- apps/moderation/src/components/styled/index.ts
+++ apps/moderation/src/components/styled/index.ts
@@ -1,9 +1,15 @@
 import DS from '@akashaproject/design-system';
 
-const { styled, Box } = DS;
+const { styled, Box, Button } = DS;
 
 const StyledBox: any = styled(Box)`
   background-color: ${props => props.theme.colors.accentLight};
 `;
 
-export { StyledBox };
+const ContentCardButton: any = styled(Button)`
+  height: auto;
+  padding: 0.3rem 0.6rem;
+  border-width: 0.1rem;
+`;
+
+export { StyledBox, ContentCardButton };
