93f2f1207e44c7ec01f283e941bec9b4577f4edf
Vali Cotea <quininez@users.noreply.github.com>
2021-03-23 12:06:47 +0200

    Topbar updates (#973)
    * feat(): update topbar WIP
    
    * feat(): profile menu
    
    * chore() topbar changes
    
    * feat(): topbar finalised design and legal plugin bootstrap
    
    * fix(): active avatar on proper routes
    
    * feat(): add feedback modal, icons, nav fixes
    
    * chore(): circle
    
    * chore(): circle
    
    * chore(): animate search, show legal caret
    
    * chore(): discord and mail links

--- ui/design/src/components/TextIcon/subtitle-text/styled-subtitle-text-icon.ts
+++ ui/design/src/components/TextIcon/subtitle-text/styled-subtitle-text-icon.ts
@@ -3,17 +3,19 @@ import styled, { css } from 'styled-components';
 
 export interface IIconDiv {
   iconSize?: string;
+  iconBackground?: boolean;
 }
 
 const IconDiv = styled.div<IIconDiv>`
   ${props => {
     const size = props.iconSize ? props.iconSize : '1.75em';
     const radius = props.iconSize ? '100%' : '0.875em';
+    const background = props.iconBackground ? props.theme.colors.lightBackground : 'none';
     return css`
       border-radius: ${radius};
       width: ${size};
       height: ${size};
-      background: ${props.theme.colors.lightBackground};
+      background: ${background};
       display: flex;
       justify-content: center;
       align-items: center;
