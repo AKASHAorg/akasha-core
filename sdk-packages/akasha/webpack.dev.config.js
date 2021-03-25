234ee8b72c8426afe63a3455c450dde1c4e80042
SeverS <sever.abi@gmail.com>
2021-03-25 17:22:07 +0200

    fix(): revert endpoints

--- sdk-packages/akasha/webpack.dev.config.js
+++ sdk-packages/akasha/webpack.dev.config.js
@@ -59,13 +59,13 @@ const config = {
     new webpack.EnvironmentPlugin({
       GRAPHQL_URI:
         process.env.NODE_ENV === 'production'
-          ? 'https://api.akasha.network/graphql'
-          : 'https://api.ethereum.world/graphql',
+          ? 'https://api.ethereum.world/graphql'
+          : 'https://api.akasha.network/graphql',
       NODE_ENV: process.env.NODE_ENV || 'development',
       AUTH_ENDPOINT:
         process.env.NODE_ENV === 'production'
-          ? 'wss://api.akasha.network/ws/userauth'
-          : 'wss://api.ethereum.world/ws/userauth',
+          ? 'wss://api.ethereum.world/ws/userauth'
+          : 'wss://api.akasha.network/ws/userauth',
     }),
     new webpack.ProgressPlugin({
       entries: true,
