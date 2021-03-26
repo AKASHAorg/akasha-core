077dd98721bcac98af62bdc4b6437da0cd46c220
Vali Cotea <quinine35@gmail.com>
2021-03-26 21:57:33 +0200

    feat(): add developer guidelines legal page

--- ui/plugins/legal/src/components/routes/index.tsx
+++ ui/plugins/legal/src/components/routes/index.tsx
@@ -1,12 +1,13 @@
 import DS from '@akashaproject/design-system';
 import * as React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import route, { TOS, TOU, PP, COC } from '../../routes';
+import route, { TOS, TOU, PP, COC, DG } from '../../routes';
 import { RootComponentProps } from '@akashaproject/ui-awf-typings';
-import TermsOfService from './tos';
-import TermsOfUse from './tou';
-import CodeOfConduct from './coc';
-import PrivacyPolicy from './pp';
+import TermsOfService from './terms-of-service';
+import TermsOfUse from './terms-of-use';
+import CodeOfConduct from './code-of-conduct';
+import PrivacyPolicy from './privacy-policy';
+import DeveloperGuidelines from './developer-guidelines';
 
 const { Box, ViewportSizeProvider } = DS;
 
@@ -27,6 +28,9 @@ const Routes: React.FC<RootComponentProps> = props => {
             </Route>
             <Route path={route[COC]}>
               <CodeOfConduct {...props} />
+            </Route>
+            <Route path={route[DG]}>
+              <DeveloperGuidelines {...props} />
             </Route>
           </Switch>
         </Box>
