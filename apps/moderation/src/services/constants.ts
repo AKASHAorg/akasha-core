65fc625111540617ac525405c4054cdbf814a48f
Marius Darila <marius.darila@gmail.com>
2021-03-30 15:23:43 +0200

    chore(config): update env conf for ui apps

--- apps/moderation/src/services/constants.ts
+++ apps/moderation/src/services/constants.ts
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.MODERATION_API || 'https://staging-moderation.ethereum.world';
+const BASE_URL = process.env.MODERATION_API;
 
 export const BASE_DECISION_URL = `${BASE_URL}/decisions`;
 export const BASE_FLAG_URL = `${BASE_URL}/flags`;
