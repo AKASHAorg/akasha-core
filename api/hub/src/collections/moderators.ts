175848b0c8281df80169ddcd365db045815d9cf5
deiu <andrei.sambra@akasha.worl>
2021-04-20 14:49:53 +0200

    moderators collection

---
+++ api/hub/src/collections/moderators.ts
@@ -0,0 +1,53 @@
+import { Client, ThreadID } from '@textile/hub';
+import { Moderator } from './interfaces';
+
+const schema = {
+  title: 'Moderator',
+  type: 'object',
+  required: ['_id', 'address', 'admin', 'active'],
+  properties: {
+    _id: { type: 'string' },
+    creationDate: { type: 'number' },
+    address: { type: 'string' },
+    admin: { type: 'boolean' },
+    active: { type: 'boolean' },
+  },
+};
+
+const writeValidator = (writer: string, event: any, instance: Moderator) => {
+  return writer === 'bbaareihqdqd3me37e3pfs6pmy72cam7viai4lbcpxlqt365wicrnsi6q7e';
+};
+
+const readFilter = (reader: string, instance: Moderator) => {
+  return instance;
+};
+
+const indexes = [
+  {
+    path: 'address',
+    unique: true,
+  },
+  {
+    path: 'creationDate',
+    unique: false,
+  },
+];
+export function newCollection(client: Client, threadID: ThreadID) {
+  return client.newCollection(threadID, {
+    schema,
+    writeValidator,
+    readFilter,
+    indexes,
+    name: 'Moderators',
+  });
+}
+
+export function updateCollection(client: Client, threadID: ThreadID) {
+  return client.updateCollection(threadID, {
+    schema,
+    writeValidator,
+    readFilter,
+    indexes,
+    name: 'Moderators',
+  });
+}
