2a0752f1084af8b3492a6b4bd9f9c20a222f33f6
deiu <andrei.sambra@akasha.worl>
2021-04-20 14:50:28 +0200

    moderation decisions datasource

---
+++ api/hub/src/datasources/moderation-decision.ts
@@ -0,0 +1,89 @@
+import { DataSource } from 'apollo-datasource';
+import { getAppDB, logger } from '../helpers';
+import { Client, ThreadID, Where } from '@textile/hub';
+import { ModerationDecision, ModerationReport } from '../collections/interfaces';
+
+class ModerationDecisionAPI extends DataSource {
+  private readonly collection: string;
+  private context: any;
+  private readonly dbID: ThreadID;
+  constructor({ collection, dbID }) {
+    super();
+    this.collection = collection;
+    this.dbID = dbID;
+  }
+
+  async initialize(config) {
+    this.context = config.context;
+  }
+
+  async countDecisions() {
+    // pending
+    const db: Client = await getAppDB();
+    let query = new Where('moderated').eq(false);
+    let results = await db.find<ModerationReport>(this.dbID, this.collection, query);
+    const pending = results.length;
+    // delisted
+    query = new Where('moderated').eq(true).and('delisted').eq(true);
+    results = await db.find<ModerationReport>(this.dbID, this.collection, query);
+    const delisted = results.length;
+    // kept
+    query = new Where('moderated').eq(false).and('delisted').eq(false);
+    results = await db.find<ModerationReport>(this.dbID, this.collection, query);
+    const kept = results.length;
+
+    return {
+      pending,
+      delisted,
+      kept,
+    };
+  }
+
+  async listDecisions(delisted: boolean, moderated: boolean, offset?: number, limit?: number) {
+    limit = limit ? limit : 10;
+    offset = offset ? offset : 0;
+    const db: Client = await getAppDB();
+    const query = new Where('delisted')
+      .eq(delisted)
+      .and('moderated')
+      .eq(moderated)
+      .skipNum(offset)
+      .limitTo(limit);
+    return await db.find<ModerationReport>(this.dbID, this.collection, query);
+  }
+
+  async getDecision(contentId: string) {
+    const db: Client = await getAppDB();
+    const query = new Where('contentId').eq(contentId);
+    const decision = await db.find<ModerationDecision>(this.dbID, this.collection, query);
+    if (decision.length) {
+      return decision[0];
+    }
+    logger.warn(`moderation decision not found for contentId:${contentId}`);
+    throw new Error('decision not found');
+  }
+
+  async makeDecision(
+    contentId: string,
+    moderatedBy: string,
+    explanation: string,
+    delisted: boolean,
+  ) {
+    const db: Client = await getAppDB();
+    if (!moderatedBy) {
+      throw new Error('Not authorized');
+    }
+    // load existing (i.e. pending) decision data
+    let decision = await this.getDecision(contentId);
+
+    decision.moderatedBy = moderatedBy;
+    decision.moderatedDate = new Date().getTime();
+    decision.explanation = explanation;
+    decision.delisted = delisted;
+    decision.moderated = true;
+
+    return await db.save(this.dbID, this.collection, [decision]);
+  }
+}
+
+export default ModerationDecisionAPI;
