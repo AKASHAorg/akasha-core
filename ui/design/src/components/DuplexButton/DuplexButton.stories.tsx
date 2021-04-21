2b95ad4d1f70461746c5f362cabfa680d82a1b06
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2021-04-21 13:46:06 +0100

    feat(ui): add Button stories

---
+++ ui/design/src/components/DuplexButton/DuplexButton.stories.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Grommet } from 'grommet';
+
+import DuplexButton from '.';
+
+import lightTheme from '../../styles/themes/light/light-theme';
+
+export default {
+  title: 'Buttons/DuplexButton',
+  component: DuplexButton,
+  argTypes: {
+    activeLabel: { control: 'text' },
+    inactiveLabel: { control: 'text' },
+    activeHoverLabel: { control: 'text' },
+    active: { control: 'boolean' },
+  },
+};
+
+const Template = (args: any) => {
+  const [active, setActive] = React.useState(false);
+  const handleClick = () => {
+    if (!active) {
+      setActive(true);
+    } else {
+      setActive(false);
+    }
+  };
+  return (
+    <Grommet theme={lightTheme}>
+      <DuplexButton
+        {...args}
+        active={args.active || active}
+        onClickActive={handleClick}
+        onClickInactive={handleClick}
+      />
+    </Grommet>
+  );
+};
+
+export const BaseDuplexButton = Template.bind({});
+BaseDuplexButton.args = {
+  activeLabel: 'Active state',
+  inactiveLabel: 'Inactive state',
+  activeHoverLabel: 'Active hover',
+  active: false,
+};
