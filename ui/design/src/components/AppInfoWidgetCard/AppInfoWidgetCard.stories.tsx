525d9d846422aaf52d534fce2dc7ed1438249f64
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2021-04-27 10:55:47 +0100

    feat(storybook): add widget cards stories

---
+++ ui/design/src/components/AppInfoWidgetCard/AppInfoWidgetCard.stories.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Box, Grommet } from 'grommet';
+
+import AppInfoWidgetCard from '.';
+
+import lightTheme from '../../styles/themes/light/light-theme';
+import { appInfo } from '../../utils/dummy-data';
+
+export default {
+  title: 'Cards/AppInfoWidgetCard',
+  component: AppInfoWidgetCard,
+  argTypes: {
+    handleSubscribe: { action: 'clicked subscribe' },
+    handleReport: { action: 'clicked report' },
+  },
+};
+
+const Template = (args: any) => (
+  <Grommet theme={lightTheme}>
+    <Box width="30%" pad="none" align="center">
+      <AppInfoWidgetCard {...args} />
+    </Box>
+  </Grommet>
+);
+
+export const BaseAppInfoWidgetCard = Template.bind({});
+
+BaseAppInfoWidgetCard.args = {
+  appInfo: appInfo,
+  versionLabel: 'Version',
+  statusLabel: 'Status',
+  lastUpdateLabel: 'Last updated',
+  submittedLabel: 'Submitted',
+  adminLabel: 'Admin',
+  categoryLabel: 'Category',
+  receiveUpdatesLabel: 'Receive updates from',
+  subscribeLabel: 'Subscribe',
+  unsubscribeLabel: 'Unsubscribe',
+  reportLabel: 'Flag as inappropriate',
+};
---
+++ ui/design/src/components/MiniInfoWidgetCard/MiniInfoWidgetCard.stories.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Box, Grommet } from 'grommet';
+
+import MiniInfoWidgetCard from '.';
+
+import lightTheme from '../../styles/themes/light/light-theme';
+
+export default {
+  title: 'Cards/MiniInfoWidgetCard',
+  component: MiniInfoWidgetCard,
+  argTypes: {
+    handleLearnMore: { action: 'clicked learn more' },
+    handleCallToAction: { action: 'clicked call to action' },
+    handleDismiss: { action: 'clicked dismiss' },
+  },
+};
+
+const Template = (args: any) => (
+  <Grommet theme={lightTheme}>
+    <Box width="30%" pad="none" align="center">
+      <MiniInfoWidgetCard {...args} />
+    </Box>
+  </Grommet>
+);
+
+export const BaseMiniInfoWidgetCard = Template.bind({});
+
+BaseMiniInfoWidgetCard.args = {
+  titleLabel: 'Example title',
+  subtitleLabel: 'Description of a call to action. Something to prompt the user to click',
+  learnMoreLabel: 'Learn more',
+  callToActionLabel: 'Do something',
+};
