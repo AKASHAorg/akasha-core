dc5e8c8cd1c7331b2f2bed2e7f8ede9389a5e9f9
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2021-05-17 07:55:26 +0100

    fix(ui): jest preset

--- ui/design/jest.config.js
+++ ui/design/jest.config.js
@@ -1,7 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const baseConfig = require('../../jest.config.base');
 
 module.exports = Object.assign(baseConfig, {
   automock: false,
-  preset: 'ts-jest/presets/js-with-babel-esm',
+  preset: 'ts-jest',
   testEnvironment: 'jsdom',
 });
