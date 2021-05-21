1f9f4d51dc9f7325b3ab55a2a5f18f4e8ab4c168
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2021-05-21 07:16:57 +0100

    test(ui): add tests

--- ui/design/src/components/TagCard/__tests__/index.test.tsx
+++ ui/design/src/components/TagCard/__tests__/index.test.tsx
@@ -1,21 +1,56 @@
 import * as React from 'react';
-// import { create } from 'react-test-renderer';
-import { render } from '@testing-library/react';
+import { act, cleanup } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
 
 import TagCard from '../';
-import { wrapWithTheme } from '../../../test-utils';
+import { customRender, wrapWithTheme } from '../../../test-utils';
 import { trendingTagsData } from '../../../utils/dummy-data';
+import userEvent from '@testing-library/user-event';
 
-describe('TagCard component', () => {
+describe('<TagCard /> Component', () => {
+  let componentWrapper = customRender(<></>, {});
+
+  const handleSubscribe = jest.fn();
+  const handleUnsubscribe = jest.fn();
+
+  beforeEach(() => {
+    act(() => {
+      componentWrapper = customRender(
+        wrapWithTheme(
+          <TagCard
+            tag={trendingTagsData[0]}
+            handleSubscribe={handleSubscribe}
+            handleUnsubscribe={handleUnsubscribe}
+          />,
+        ),
+        {},
+      );
+    });
+  });
+
+  afterEach(() => {
+    act(() => componentWrapper.unmount());
+    cleanup();
+  });
+
   it('renders correctly', () => {
-    render(
-      wrapWithTheme(
-        <TagCard
-          tag={trendingTagsData[0]}
-          handleSubscribe={() => null}
-          handleUnsubscribe={() => null}
-        />,
-      ),
-    );
+    expect(componentWrapper).toBeDefined();
+  });
+
+  it('has correct tag name', () => {
+    const { getByText } = componentWrapper;
+    const akashaTag = getByText('AKASHA');
+
+    expect(akashaTag).toBeDefined();
+  });
+
+  it('calls handler when clicked', () => {
+    const { getByRole } = componentWrapper;
+    const subscribedButton = getByRole('button', { name: 'Subscribed' });
+
+    expect(handleSubscribe).toBeCalledTimes(0);
+
+    userEvent.click(subscribedButton);
+    expect(handleSubscribe).toBeCalledTimes(1);
   });
 });
