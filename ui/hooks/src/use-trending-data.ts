4d41990f0079f0416d866ba6db4514d1d76745c5
Vali Cotea <quinine35@gmail.com>
2021-06-10 23:33:03 +0300

    refactor(): useTrending actions refactor

--- ui/hooks/src/use-trending-data.ts
+++ ui/hooks/src/use-trending-data.ts
@@ -14,38 +14,58 @@ export interface ITrendingActions {
   /**
    *  get trending tags
    */
-  setTrendingTags: (payload: any) => void;
+  getTrendingTags: () => void;
   /**
    *  get trending profiles
    */
-  setTrendingProfiles: (payload: any) => void;
+  getTrendingProfiles: () => void;
 }
 
 interface ITrendingState {
   tags: { name: string; totalPosts: number }[];
   profiles: IProfileData[];
+  // reducer related
+  getTagsQuery: boolean;
+  getProfilesQuery: boolean;
 }
 
 export type ITrendingAction =
-  | { type: 'SET_TRENDING_TAGS_DATA'; payload: any }
-  | { type: 'SET_TRENDING_PROFILES_DATA'; payload: any };
+  | { type: 'GET_TRENDING_TAGS' }
+  | { type: 'GET_TRENDING_TAGS_SUCCESS'; payload: { name: string; totalPosts: number }[] }
+  | { type: 'GET_TRENDING_PROFILES' }
+  | { type: 'GET_TRENDING_PROFILES_SUCCESS'; payload: IProfileData[] };
 
 const initialTrendingState: ITrendingState = {
   tags: [],
   profiles: [],
+  getTagsQuery: false,
+  getProfilesQuery: false,
 };
 
 const trendingStateReducer = (state: ITrendingState, action: ITrendingAction) => {
   switch (action.type) {
-    case 'SET_TRENDING_TAGS_DATA':
+    case 'GET_TRENDING_TAGS':
       return {
         ...state,
+        getTagsQuery: true,
+      };
+    case 'GET_TRENDING_TAGS_SUCCESS':
+      return {
+        ...state,
+        getTagsQuery: false,
         tags: action.payload,
       };
 
-    case 'SET_TRENDING_PROFILES_DATA':
+    case 'GET_TRENDING_PROFILES':
       return {
         ...state,
+        getProfilesQuery: true,
+      };
+
+    case 'GET_TRENDING_PROFILES_SUCCESS':
+      return {
+        ...state,
+        getProfilesQuery: false,
         profiles: action.payload,
       };
 
@@ -64,57 +84,64 @@ const useTrendingData = (
   const [trendingState, dispatch] = React.useReducer(trendingStateReducer, initialTrendingState);
 
   React.useEffect(() => {
-    const trendingTagsCall = sdkModules.posts.tags.getTrending(null);
-    const tagsSub: Subscription | undefined = trendingTagsCall.subscribe({
-      next: (resp: any) => {
-        if (resp.data.searchTags) {
-          actions.setTrendingTags(resp.data.searchTags);
-        }
-      },
-      error: createErrorHandler('useTrendingData.getTrendingTags', false, onError),
-    });
-
-    const ipfsGatewayCall = sdkModules.commons.ipfsService.getSettings(null);
-    const trendingProfilesCall = sdkModules.profiles.profileService.getTrending(null);
-    const getTrendingProfiles = combineLatest({
-      ipfsGatewayCall: ipfsGatewayCall,
-      trendingProfilesCall: trendingProfilesCall,
-    });
-    const profilesSub: Subscription | undefined = getTrendingProfiles.subscribe({
-      next: (resp: any) => {
-        const ipfsGateway = resp.ipfsGatewayCall.data;
-        if (resp.trendingProfilesCall.data.searchProfiles) {
-          const profiles = resp.trendingProfilesCall.data.searchProfiles.map((profile: any) => {
-            const profileData = Object.assign({}, profile);
-            if (profile.avatar && !profile.avatar.startsWith(ipfsGateway)) {
-              const profileAvatarWithGateway = `${ipfsGateway}/${profile.avatar}`;
-              profileData.avatar = profileAvatarWithGateway;
-            }
-            // should replace with real data once we integrate follow functionality
-            profileData.followers = profileData.followers || 0;
-            return profileData;
-          });
-          actions.setTrendingProfiles(profiles);
-        }
-      },
-      error: createErrorHandler('useTrendingData.getTrendingProfiles', false, onError),
-    });
-    return () => {
-      if (tagsSub) {
-        tagsSub.unsubscribe();
-      }
-      if (profilesSub) {
-        profilesSub.unsubscribe();
-      }
-    };
+    actions.getTrendingTags();
+    actions.getTrendingProfiles();
   }, []);
 
+  React.useEffect(() => {
+    if (trendingState.getTagsQuery) {
+      const trendingTagsCall = sdkModules.posts.tags.getTrending(null);
+      const tagsSub: Subscription | undefined = trendingTagsCall.subscribe({
+        next: (resp: any) => {
+          if (resp.data.searchTags) {
+            dispatch({ type: 'GET_TRENDING_TAGS_SUCCESS', payload: resp.data.searchTags });
+          }
+        },
+        error: createErrorHandler('useTrendingData.getTrendingTags', false, onError),
+      });
+      return () => tagsSub?.unsubscribe();
+    }
+    return;
+  }, [trendingState.getTagsQuery]);
+
+  React.useEffect(() => {
+    if (trendingState.getProfilesQuery) {
+      const ipfsGatewayCall = sdkModules.commons.ipfsService.getSettings(null);
+      const trendingProfilesCall = sdkModules.profiles.profileService.getTrending(null);
+      const getTrendingProfiles = combineLatest({
+        ipfsGatewayCall: ipfsGatewayCall,
+        trendingProfilesCall: trendingProfilesCall,
+      });
+      const profilesSub: Subscription | undefined = getTrendingProfiles.subscribe({
+        next: (resp: any) => {
+          const ipfsGateway = resp.ipfsGatewayCall.data;
+          if (resp.trendingProfilesCall.data.searchProfiles) {
+            const profiles = resp.trendingProfilesCall.data.searchProfiles.map((profile: any) => {
+              const profileData = Object.assign({}, profile);
+              if (profile.avatar && !profile.avatar.startsWith(ipfsGateway)) {
+                const profileAvatarWithGateway = `${ipfsGateway}/${profile.avatar}`;
+                profileData.avatar = profileAvatarWithGateway;
+              }
+              // should replace with real data once we integrate follow functionality
+              profileData.followers = profileData.followers || 0;
+              return profileData;
+            });
+            dispatch({ type: 'GET_TRENDING_PROFILES_SUCCESS', payload: profiles });
+          }
+        },
+        error: createErrorHandler('useTrendingData.getTrendingProfiles', false, onError),
+      });
+      return () => profilesSub.unsubscribe();
+    }
+    return;
+  }, [trendingState.getProfilesQuery]);
+
   const actions: ITrendingActions = {
-    setTrendingTags: (tagsData: any) => {
-      dispatch({ type: 'SET_TRENDING_TAGS_DATA', payload: tagsData });
+    getTrendingTags: () => {
+      dispatch({ type: 'GET_TRENDING_TAGS' });
     },
-    setTrendingProfiles: (profilesData: any) => {
-      dispatch({ type: 'SET_TRENDING_PROFILES_DATA', payload: profilesData });
+    getTrendingProfiles: () => {
+      dispatch({ type: 'GET_TRENDING_PROFILES' });
     },
   };
 
