9580747c5f1a96b9b7d36b9b0ca98918ee560b84
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2021-07-21 09:34:19 +0100

    Feat: dynamic report reasons (#1227)
    * feat(ui): create and export hook
    - update constants
    - export method for reuse
    
    * feat(apps): implement useReasons hook
    
    * refactor(ui): loading indicator

--- ui/hooks/src/constants.ts
+++ ui/hooks/src/constants.ts
@@ -4,5 +4,12 @@ const BASE_REPORT_URL = `${BASE_URL}/reports`;
 const BASE_STATUS_URL = `${BASE_URL}/status`;
 const BASE_DECISION_URL = `${BASE_URL}/decisions`;
 const BASE_MODERATOR_URL = `${BASE_URL}/moderators`;
+const BASE_REASONS_URL = `${BASE_URL}/reasons`;
 
-export default { BASE_REPORT_URL, BASE_STATUS_URL, BASE_DECISION_URL, BASE_MODERATOR_URL };
+export default {
+  BASE_REPORT_URL,
+  BASE_STATUS_URL,
+  BASE_DECISION_URL,
+  BASE_MODERATOR_URL,
+  BASE_REASONS_URL,
+};
---
+++ ui/hooks/src/use-reasons.ts
@@ -0,0 +1,80 @@
+import * as React from 'react';
+import { IAkashaError } from '@akashaproject/ui-awf-typings';
+
+import constants from './constants';
+import { fetchRequest } from './moderation-request';
+import { createErrorHandler } from './utils/error-handler';
+
+const { BASE_REASONS_URL } = constants;
+
+export interface UseReasonsActions {
+  /**
+   *  fetch reasons, pass active as true to get only active reasons
+   */
+  fetchReasons: (data: { active: boolean }) => void;
+}
+
+export interface IReasonsState {
+  reasons: string[];
+}
+
+export interface UseReasonsProps {
+  onError?: (error: IAkashaError) => void;
+}
+
+const initialReasonstate = {
+  reasons: [],
+};
+
+export type IReasonsAction = {
+  type: 'FETCH_REASONS_SUCCESS';
+  payload: string[];
+};
+
+const ReasonStateReducer = (state: IReasonsState, action: IReasonsAction) => {
+  switch (action.type) {
+    case 'FETCH_REASONS_SUCCESS': {
+      return {
+        ...state,
+        reasons: action.payload,
+      };
+    }
+    default:
+      throw new Error('[ReasonStateReducer] action is not defined!');
+  }
+};
+
+/* A hook to get predefined reasons from moderation API */
+export const useReasons = (props: UseReasonsProps): [string[], UseReasonsActions] => {
+  const { onError } = props;
+
+  const [reasonsState, dispatch] = React.useReducer(ReasonStateReducer, initialReasonstate);
+
+  const actions: UseReasonsActions = {
+    async fetchReasons(data) {
+      try {
+        const res = await fetchRequest({
+          method: 'POST',
+          url: BASE_REASONS_URL,
+          data,
+        });
+
+        if (Array.isArray(res)) {
+          // pick only labels for each reason object
+          const labels = res.reduce(
+            (acc: string[], cur: Record<string, unknown>) => [...acc, cur.label],
+            [],
+          );
+          // dispatch labels, adding last option - 'Other'
+          dispatch({ type: 'FETCH_REASONS_SUCCESS', payload: [...labels, 'Other'] });
+        } else throw new Error(res);
+      } catch (error) {
+        return createErrorHandler('useReasons.fetchReasons', false, onError);
+      }
+    },
+  };
+
+  return [reasonsState.reasons, actions];
+};
+
+export default useReasons;
