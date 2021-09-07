601af67cd9bbd4dcd391c6f1398db08898a1a8e5
SeverS <sever.abi@gmail.com>
2021-09-07 19:16:28 +0300

    chore(): rm useProfile

--- apps/moderation/src/components/explanations-box-entry.tsx
+++ apps/moderation/src/components/explanations-box-entry.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import DS from '@akashaproject/design-system';
-import { useProfile } from '@akashaproject/ui-awf-hooks';
+import { useGetProfile } from '@akashaproject/ui-awf-hooks/lib/use-profile.new';
 import { ILogger } from '@akashaproject/awf-sdk/typings/lib/interfaces/log';
 
 const { Text } = DS;
@@ -26,40 +26,35 @@ export interface IExplanationsBoxEntryProps {
 const ExplanationsBoxEntry: React.FC<IExplanationsBoxEntryProps> = props => {
   const { flagEntry, reportedByLabel, forLabel } = props;
 
-  const [reporterProfileState, reporterProfileActions] = useProfile({
-    onError: error => {
-      props.logger.error(`[explanations-box-entry.tsx]: useProfile err, ${JSON.stringify(error)}`);
-    },
-  });
-
-  React.useEffect(() => {
-    if (flagEntry.author) {
-      reporterProfileActions.getProfileData({ pubKey: flagEntry.author });
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [flagEntry]);
+  const getProfileQuery = useGetProfile(flagEntry.author);
 
   return (
     <>
-      <Text as="p" margin={{ top: 'none', bottom: 'xxsmall' }}>
-        {reportedByLabel}
-        <Text as="span" margin={{ left: '0.2rem' }} color="accentText">
-          {reporterProfileState.name
-            ? reporterProfileState.name
-            : `${flagEntry.author.slice(0, 6)}...${flagEntry.author.slice(
-                flagEntry.author.length - 4,
-              )}`}
-        </Text>
-        <Text as="span" margin={{ left: '0.2rem' }}>
-          {forLabel}
-        </Text>
-        <Text as="span" margin={{ left: '0.2rem' }}>
-          {flagEntry.reason}
-        </Text>
-      </Text>
-      <Text as="p" margin={{ top: 'none', bottom: 'medium' }} color="secondaryText">
-        {flagEntry.explanation}
-      </Text>
+      {getProfileQuery.status === 'loading' && <Text>Loading...</Text>}
+      {getProfileQuery.status === 'error' && <Text>Error loading profile</Text>}
+      {getProfileQuery.status === 'success' && (
+        <>
+          <Text as="p" margin={{ top: 'none', bottom: 'xxsmall' }}>
+            {reportedByLabel}
+            <Text as="span" margin={{ left: '0.2rem' }} color="accentText">
+              {getProfileQuery.data.name
+                ? getProfileQuery.data.name
+                : `${flagEntry.author.slice(0, 6)}...${flagEntry.author.slice(
+                    flagEntry.author.length - 4,
+                  )}`}
+            </Text>
+            <Text as="span" margin={{ left: '0.2rem' }}>
+              {forLabel}
+            </Text>
+            <Text as="span" margin={{ left: '0.2rem' }}>
+              {flagEntry.reason}
+            </Text>
+          </Text>
+          <Text as="p" margin={{ top: 'none', bottom: 'medium' }} color="secondaryText">
+            {flagEntry.explanation}
+          </Text>
+        </>
+      )}
     </>
   );
 };
