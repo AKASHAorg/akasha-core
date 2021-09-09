a606668d8790620b51da763da005a785ce1070c6
Sever Abibula <5903809+SeverS@users.noreply.github.com>
2021-09-09 11:34:48 +0300

    chore(): fix scroll restoration (#1274)

--- ui/widgets/layout/src/components/cookie-widget.tsx
+++ ui/widgets/layout/src/components/cookie-widget.tsx
@@ -20,41 +20,39 @@ const CookieWidget: React.FC<ICookieWidgetProps> = props => {
     <>
       {!analytics.cookieBannerDismissed && (
         <div style={{ ...props.style }}>
-          <React.Suspense fallback={<></>}>
-            <Translation>
-              {t => (
-                <CookieWidgetCard
-                  titleLabel={`${t('But first, cookies!')} 🙈🍪`}
-                  contentLabel={
-                    <>
-                      {t(
-                        'This website requires essential cookies for security and stability purposes. ',
-                      )}
-                      <Text
-                        color="accentText"
-                        size="medium"
-                        style={{ cursor: 'pointer' }}
-                        onClick={() =>
-                          window.open('https://matomo.org', 'Matomo', '_blank noopener noreferrer')
-                        }
-                      >
-                        Matomo
-                      </Text>
-                      {t(
-                        ' cookies and tracking can be accepted for product improvement according to our',
-                      )}
-                    </>
-                  }
-                  privacyUrlLabel={t('Privacy Policy.')}
-                  privacyUrl={`${window.location.protocol}//${window.location.host}/legal/privacy-policy`}
-                  onlyEssentialLabel={t('Only essential')}
-                  acceptAllLabel={t('Accept all')}
-                  onClickOnlyEssential={() => acceptCookie()}
-                  onClickAcceptAll={() => acceptCookie(true)}
-                />
-              )}
-            </Translation>
-          </React.Suspense>
+          <Translation>
+            {t => (
+              <CookieWidgetCard
+                titleLabel={`${t('But first, cookies!')} 🙈🍪`}
+                contentLabel={
+                  <>
+                    {t(
+                      'This website requires essential cookies for security and stability purposes. ',
+                    )}
+                    <Text
+                      color="accentText"
+                      size="medium"
+                      style={{ cursor: 'pointer' }}
+                      onClick={() =>
+                        window.open('https://matomo.org', 'Matomo', '_blank noopener noreferrer')
+                      }
+                    >
+                      Matomo
+                    </Text>
+                    {t(
+                      ' cookies and tracking can be accepted for product improvement according to our',
+                    )}
+                  </>
+                }
+                privacyUrlLabel={t('Privacy Policy.')}
+                privacyUrl={`${window.location.protocol}//${window.location.host}/legal/privacy-policy`}
+                onlyEssentialLabel={t('Only essential')}
+                acceptAllLabel={t('Accept all')}
+                onClickOnlyEssential={() => acceptCookie()}
+                onClickAcceptAll={() => acceptCookie(true)}
+              />
+            )}
+          </Translation>
         </div>
       )}
     </>
