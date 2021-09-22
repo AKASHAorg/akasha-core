95aa817b88b6fba643c5f4f42a12d58a3abd7762
alpharius <damir.sabolic@akasha.world>
2021-09-22 14:08:52 +0200

    chore(): update useEditComment

--- ui/hooks/src/use-comments.new.ts
+++ ui/hooks/src/use-comments.new.ts
@@ -170,13 +170,13 @@ export function useCreateComment() {
 }
 
 // hook used for editing a comment
-export function useEditComment(commentID: string, hasCommentData: boolean) {
+export async function useEditComment(commentID: string, hasCommentData: boolean) {
   const sdk = getSDK();
   const queryClient = useQueryClient();
 
   return useMutation(
     (comment: IPublishData & { postID: string }) => {
-      if (!hasCommentData) return;
+      if (!hasCommentData) return Promise.resolve(null);
       const { postID, ...commentData } = comment;
       const publishObj = buildPublishObject(commentData, postID);
       return lastValueFrom(
