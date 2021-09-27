dc01dd41a347f0872cb030d240f756210a87cde2
Marius Darila <3396463+kenshyx@users.noreply.github.com>
2021-09-27 12:37:55 +0200

    refactor(stash, moderation-req): add cache strategy (#1328)

--- sdk/src/stash/index.ts
+++ sdk/src/stash/index.ts
@@ -3,18 +3,34 @@ import { injectable } from 'inversify';
 import { IStashService, ServiceCallResult } from '@akashaproject/sdk-typings/lib/interfaces';
 import QuickLRU from 'quick-lru';
 import { createObservableValue } from '../helpers/observable';
+import hash from 'object-hash';
 
 export type IQuickLRU = QuickLRU<string, any>;
 @injectable()
 class Stash implements IStashService<IQuickLRU> {
+  private _uiStash: QuickLRU<string, unknown>;
   /**
    *
    * @param args - Object with props maxSize: number and maxAge: number
    * @returns a new instance of QuickLRU
    */
   create(args: LRUOptions): ServiceCallResult<IQuickLRU> {
-    const cache = new QuickLRU<string, never>(args);
+    const cache = new QuickLRU<never, never>(args);
     return createObservableValue(cache);
+  }
+
+  /**
+   * General purpose stash for caching ui related data
+   */
+  getUiStash() {
+    if (!this._uiStash) {
+      this._uiStash = new QuickLRU<never, unknown>({ maxSize: 999, maxAge: 1000 * 60 });
+    }
+    return this._uiStash;
+  }
+
+  computeKey(objKey: Record<never, unknown>): string {
+    return hash(objKey, { algorithm: 'sha1', unorderedObjects: true });
   }
 }
 
--- sdk/typings/src/interfaces/stash.ts
+++ sdk/typings/src/interfaces/stash.ts
@@ -8,4 +8,6 @@ export interface LRUOptions {
 export default interface IStashService<IStash> {
   // returns a new lru-cache instance
   create(args: LRUOptions): ServiceCallResult<IStash>;
+  getUiStash(): IStash;
+  computeKey(objKey: Record<string, unknown>): string;
 }
