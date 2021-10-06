068a8cfc53958a60085f1dfb330622badccc8e1f
Sever Abibula <5903809+SeverS@users.noreply.github.com>
2021-10-06 14:57:07 +0300

    Fix/query params (#1348)
    * chore(): restore scroll only on pathname change
    
    * chore(): fix pathname
    
    * chore(): allow horizontal overscroll (back button gesture)

--- ui/widgets/layout/src/components/global-style.ts
+++ ui/widgets/layout/src/components/global-style.ts
@@ -29,7 +29,7 @@ export const GlobalStyle: any = createGlobalStyle<{
     padding: 0;
   }
   body {
-    overscroll-behavior-x: none;
+    /* overscroll-behavior-x: none; */
   }
   ${props => css`
     // 1920 and lower
