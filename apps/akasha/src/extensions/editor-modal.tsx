91f1cfc5b71c17e4711dc0a104325c5fed426ddf
SeverS <sever.abi@gmail.com>
2021-10-13 08:44:48 +0300

    fix(): 1383

--- apps/akasha/src/extensions/editor-modal.tsx
+++ apps/akasha/src/extensions/editor-modal.tsx
@@ -144,7 +144,7 @@ const EditorModalContainer = (props: RootComponentProps) => {
             titleLabel={isEditing ? t('Edit Post') : t('New Post')}
             avatar={profileDataReq.data?.avatar}
             ethAddress={loginQuery.data?.ethAddress}
-            postLabel={t('Publish')}
+            postLabel={isEditing ? t('Save changes') : t('Publish')}
             placeholderLabel={t('Write something')}
             emojiPlaceholderLabel={t('Search')}
             discardPostLabel={t('Discard Post')}
