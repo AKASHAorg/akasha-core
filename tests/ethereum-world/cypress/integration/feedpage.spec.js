ba83d7defbf7153ca767ab8159b2c2bbf0cc1ee8
alpharius <damir.sabolic@akasha.world>
2021-11-08 20:05:24 +0100

    test(): add more sample tests, add local and prod script and config

--- tests/ethereum-world/cypress/integration/feedpage.spec.js
+++ tests/ethereum-world/cypress/integration/feedpage.spec.js
@@ -1,12 +1,35 @@
 import { TopbarTest } from './partials/topbar.spec';
+import { TrendingWidgetTest } from './partials/trendingWidget.spec';
 
 describe('Feed Page', () => {
   context('Feed Page', () => {
     before(() => {
       cy.visit('/social-app/feed');
     });
-    describe('Should test top bar', () => {
-      TopbarTest();
+    describe('Should test top bar', () => TopbarTest());
+    describe('Should test trending widget', () => TrendingWidgetTest());
+  });
+
+  describe('Posts', () => {
+    it('should render posts on the page', () => {
+      cy.get('a[href^="/social-app/post"]').its('length').should('be.gt', 0);
+      cy.get('[data-testid="avatar-image"]')
+        .first()
+        .should('have.attr', 'src')
+        .and('contain', 'https://');
+
+      cy.get('svg[type="moreDark"]').click();
+    });
+
+    it('should render user avatars in posts', () => {
+      cy.get('[data-testid="avatar-image"]')
+        .first()
+        .should('have.attr', 'src')
+        .and('contain', 'https://');
+    });
+
+    it('should open report popup on click', () => {
+      cy.get('svg[type="moreDark"]').first().click();
     });
   });
 });
