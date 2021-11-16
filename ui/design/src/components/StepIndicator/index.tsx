955beea488d07e08765338a4a2ec94e58c65b08d
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2021-11-16 11:45:48 +0100

    Feat: onboarding v2 UI (#1483)
    * refactor(ui): pass clickhandler as prop
    
    * feat(ui): create and export SelectAddressButton
    - add story and test
    
    * feat(ui): create and export Web3ConnectButton
    - add story and test
    
    * chore(ui): update style
    
    * feat(ui): create and export StepIndicator
    - add story and test
    
    * feat(ui): create and export SignUpCard
    - add story and test

---
+++ ui/design/src/components/StepIndicator/index.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import { Box, Text } from 'grommet';
+import { EdgeSizeType } from 'grommet/utils';
+
+import Icon from '../Icon';
+
+export interface IStepIndicatorProps {
+  activeIndex: number;
+  stepLabels: string[];
+  margin?: EdgeSizeType;
+}
+
+const StyledText = styled(Text)`
+  line-height: 0;
+`;
+
+const StepIndicator: React.FC<IStepIndicatorProps> = props => {
+  const { stepLabels, activeIndex, margin } = props;
+  return (
+    <Box
+      direction="row"
+      align="center"
+      width={{ max: 'fit-content' }}
+      margin={{ vertical: margin }}
+    >
+      {stepLabels.map((step, idx) => (
+        <>
+          <Box
+            key={idx + step}
+            height="1.5rem"
+            width="1.5rem"
+            round={true}
+            justify="center"
+            align="center"
+            border={{
+              color: idx <= activeIndex ? 'accentText' : 'secondaryText',
+              size: 'xsmall',
+            }}
+            background={idx < activeIndex ? 'accentText' : 'none'}
+          >
+            {idx < activeIndex ? (
+              <Icon type="checkSimple" size="xxs" color="white" />
+            ) : (
+              <StyledText color={idx === activeIndex ? 'accentText' : 'secondaryText'}>
+                {idx + 1}
+              </StyledText>
+            )}
+          </Box>
+          {idx !== stepLabels.length - 1 && (
+            <Box width="1rem" border={{ side: 'bottom', color: 'border', size: 'xsmall' }} />
+          )}
+        </>
+      ))}
+    </Box>
+  );
+};
+
+export default StepIndicator;
