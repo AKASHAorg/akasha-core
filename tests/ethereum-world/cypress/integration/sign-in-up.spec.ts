f2a170fe07c6ab6122f7d42351acc02ae5f5cca5
SeverS <sever.abi@gmail.com>
2021-11-16 12:06:38 +0200

    chore(): adjust routes, adjust cy timeouts

--- tests/ethereum-world/cypress/integration/sign-in-up.spec.ts
+++ tests/ethereum-world/cypress/integration/sign-in-up.spec.ts
@@ -3,7 +3,7 @@ import { LayoutWidgetFocused } from './partials/layout-widget.spec';
 describe('SignIn/SignUp app', () => {
   context('Sign in', () => {
     before(() => {
-      cy.visit('/sign-in');
+      return cy.visit('/sign-in');
     });
     describe('Should have focused plugin element', () => LayoutWidgetFocused());
   });
