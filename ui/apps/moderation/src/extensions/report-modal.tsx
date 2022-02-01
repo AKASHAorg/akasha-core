7c36b3a8c865b3aff9d74f51a4dc9c520eb92ae4
Damir Sabolic <damir.sabolic@akasha.world>
2022-02-01 17:30:15 +0100

    fix(): replace i18n template strings with string interpolation (#1552)

--- ui/apps/moderation/src/extensions/report-modal.tsx
+++ ui/apps/moderation/src/extensions/report-modal.tsx
@@ -5,7 +5,13 @@ import DS from '@akashaproject/design-system';
 import { I18nextProvider, useTranslation } from 'react-i18next';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ModerationItemTypes, RootComponentProps } from '@akashaproject/ui-awf-typings';
-import { withProviders, useReasons, useGetLogin, useReport, useAnalytics } from '@akashaproject/ui-awf-hooks';
+import {
+  withProviders,
+  useReasons,
+  useGetLogin,
+  useReport,
+  useAnalytics,
+} from '@akashaproject/ui-awf-hooks';
 import { BASE_REPORT_URL } from '../services/constants';
 import i18n, { setupI18next } from '../i18n';
 
@@ -58,11 +64,13 @@ const ReportModalComponent = (props: RootComponentProps) => {
 
   return (
     <ReportModal
-      titleLabel={t(
-        `Report ${itemType === ModerationItemTypes.ACCOUNT ? activeModal.user : itemType}`,
-      )}
+      titleLabel={t('Report {{itemType}}', {
+        itemType: itemType === ModerationItemTypes.ACCOUNT ? activeModal.user : itemType,
+      })}
       successTitleLabel={t('Thank you for helping us keep Ethereum World safe! 🙌')}
-      successMessageLabel={t(`We will investigate this ${itemType} and take appropriate action.`)}
+      successMessageLabel={t('We will investigate this {{itemType}} and take appropriate action.', {
+        itemType,
+      })}
       optionsTitleLabel={t('Please select a reason')}
       optionLabels={reasons.map((el: string) => t(el))}
       optionValues={reasons}
