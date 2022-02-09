7c66bde35d8efdb300362e9965cefb73fc3e4d91
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2022-02-09 10:14:23 +0100

    feat(ui): update settings page

--- ui/apps/settings-app/src/components/settings-page.tsx
+++ ui/apps/settings-app/src/components/settings-page.tsx
@@ -1,20 +1,35 @@
 import * as React from 'react';
+import { useTranslation } from 'react-i18next';
+import DS from '@akashaproject/design-system';
 import { RootComponentProps } from '@akashaproject/ui-awf-typings';
 import { COOKIE_CONSENT_NAME, CookieConsentTypes } from '@akashaproject/ui-awf-hooks';
-// import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
-import DS from '@akashaproject/design-system';
-import { useTranslation } from 'react-i18next';
 
-const { Box, BasicCardBox, Text, RadioButtonGroup, Checkbox } = DS;
+import PrivacyOption from './option-privacy';
+import AppsOption from './option-apps';
+import { ISettingsItem, SettingsOption } from '../interfaces';
+import { settingsItems } from '../utils/settings-items';
+import AppearanceOption from './option-appearance';
+
+const { Box, BasicCardBox, Icon, Text } = DS;
 
 const SettingsPage: React.FC<RootComponentProps> = () => {
   const { t } = useTranslation();
 
+  const [activeOption, setActiveOption] = React.useState<SettingsOption>('Settings');
   const [theme, setTheme] = React.useState<string>('Light Theme');
   const cookieType = window.localStorage.getItem(COOKIE_CONSENT_NAME);
-  const [checked, setChecked] = React.useState(cookieType === CookieConsentTypes.ALL);
-  const onChange = event => {
-    setChecked(event.target.checked);
+  const [checkedTracking, setCheckedTracking] = React.useState(
+    cookieType === CookieConsentTypes.ALL,
+  );
+  const [checkedAutoUpdates, setCheckedAutoUpdates] = React.useState<boolean>(false);
+  const [checkedDataAnalytics, setCheckedDataAnalytics] = React.useState<boolean>(false);
+
+  const handleSettingsOptionClick = (option: SettingsOption) => () => setActiveOption(option);
+
+  const handleChevronLeftClick = () => setActiveOption('Settings');
+
+  const handleTrackingOptionChange = event => {
+    setCheckedTracking(event.target.checked);
     if (cookieType) {
       if (event.target.checked) {
         if (cookieType === CookieConsentTypes.ESSENTIAL) {
@@ -32,92 +47,107 @@ const SettingsPage: React.FC<RootComponentProps> = () => {
     }
   };
 
+  const handleAutoUpdatesChange = event => {
+    setCheckedAutoUpdates(event.target.checked);
+    // @TODO: handle auto updates subscription
+  };
+
+  const handleDataAnalyticsChange = event => {
+    setCheckedDataAnalytics(event.target.checked);
+    // @TODO: handle data analytics subscription
+  };
+
+  const handleThemeSelect = event => {
+    setTheme(event.target.value);
+    // @TODO: handle theme select
+  };
+
   return (
     <Box direction="column" gap="small">
       <BasicCardBox>
-        <Box
-          pad="medium"
-          justify="center"
-          align="start"
-          border={{ side: 'bottom', color: 'border' }}
-        >
-          <Text weight="bold" size="large">{`${t('Settings')}`}</Text>
-        </Box>
-        <Box pad="medium" justify="center" align="start">
-          <Text textAlign="start" weight="bold" size="large">{`${t('Privacy')}`}</Text>
-        </Box>
-        <Box margin="medium" justify="center" align="start" gap="xsmall">
-          <Text weight="bold">{`${t('Essential Cookies')}`}</Text>
-          <div>
-            <Text>{`${t(
-              'The collection and processing of these data are carried out for the purpose of enabling the use of our App (establishing a connection), ensuring system security and stability over the long term and optimising our internet presence as well as for internal statistical purposes. It is within our legitimate interest to process such data to offer you a well-functioning App. You can find out more by going to our ',
-            )}`}</Text>
-            <Text
-              color="accentText"
-              size="medium"
-              style={{ cursor: 'pointer' }}
-              onClick={() =>
-                window.open(
-                  `${window.location.protocol}//${window.location.host}/legal/privacy-policy`,
-                  'Privacy Policy',
-                  '_blank noopener noreferrer',
-                )
-              }
+        {activeOption === 'Settings' && (
+          <>
+            <Box
+              pad="medium"
+              justify="center"
+              align="center"
+              border={{ side: 'bottom', color: 'border' }}
             >
-              {`${t('Privacy Policy')}`}
-            </Text>
-            <Text>.</Text>
-          </div>
-        </Box>
-        <Box margin="medium" justify="center" align="start" gap="xsmall">
-          <Text weight="bold">{`${t('Tracking & Analytics')}`}</Text>
-          <Box direction="row" justify="between" align="start">
-            <div style={{ maxWidth: '80%' }}>
-              <Text>{`${t('We use ')}`}</Text>
-              <Text
-                color="accentText"
-                size="medium"
-                style={{ cursor: 'pointer' }}
-                onClick={() =>
-                  window.open('https://matomo.org', 'Matomo', '_blank noopener noreferrer')
+              <Text weight="bold" size="large">{`${t('Settings')}`}</Text>
+            </Box>
+            {settingsItems.map((item: ISettingsItem, idx: number) => (
+              <Box
+                key={`${idx}${item.label}`}
+                direction="row"
+                pad="medium"
+                justify={item.isSubheading ? 'start' : 'between'}
+                align="center"
+                border={idx !== settingsItems.length - 1 && { side: 'bottom', color: 'border' }}
+                onClick={
+                  item.clickable ? handleSettingsOptionClick(item.label as SettingsOption) : null
                 }
               >
-                Matomo,
-              </Text>
-              <Text>{` ${t(
-                " an open source analytics platform that will help us improve Ethereum World. As we respect your privacy, rest assured that we don't store personal identifiable information (PPI).",
-              )}`}</Text>
-            </div>
-            <Box pad={{ top: 'small' }}>
-              <Checkbox
-                checked={checked}
-                onChange={onChange}
-                toggle={true}
-                disabled={!cookieType}
-              />
-            </Box>
-          </Box>
-        </Box>
-      </BasicCardBox>
-      <BasicCardBox>
-        <Box
-          pad="medium"
-          justify="center"
-          align="start"
-          border={{ side: 'bottom', color: 'border' }}
-        >
-          <Text weight="bold">{`${t('App Theme')}`}</Text>
-        </Box>
-        <Box margin="medium" justify="center" align="start" gap="small">
-          <Text>{`${t("Choose your preference for the Ethereum World's theme")}: `}</Text>
-          <RadioButtonGroup
-            gap="small"
-            name="reasons"
-            options={['Dark Theme', 'Light Theme']}
-            value={theme}
-            onChange={(event: React.ChangeEvent<HTMLInputElement>) => setTheme(event.target.value)}
+                <Text
+                  weight={item.isSubheading ? 'bold' : 'normal'}
+                  size="large"
+                  margin={item.isSubheading && { vertical: 'medium' }}
+                >
+                  {`${t(item.label)}`}
+                </Text>
+                {!item.isSubheading && <Icon type="chevronRight" />}
+              </Box>
+            ))}
+          </>
+        )}
+        {activeOption === 'Privacy' && (
+          <PrivacyOption
+            titleLabel={t('Privacy')}
+            essentialCookiesLabel={t('Essential Cookies')}
+            essentialCookiesInfo={t(
+              'The collection and processing of these data are carried out for the purpose of enabling the use of our App (establishing a connection), ensuring system security and stability over the long term and optimising our internet presence as well as for internal statistical purposes. It is within our legitimate interest to process such data to offer you a well-functioning App. You can find out more by going to our ',
+            )}
+            trackingAnalyticsLabel={t('Tracking and Analytics')}
+            trackingAnalyticsInfoIntro={t('We use ')}
+            trackingAnalyticsInfo={t(
+              " an open source analytics platform that will help us improve Ethereum World. As we respect your privacy, rest assured that we don't store personal identifiable information (PPI).",
+            )}
+            privacyPolicyLabel={t('Privacy Policy')}
+            checkedTracking={checkedTracking}
+            cookieType={cookieType}
+            onTrackingOptionChange={handleTrackingOptionChange}
+            OnChevronLeftClick={handleChevronLeftClick}
           />
-        </Box>
+        )}
+
+        {activeOption === 'Apps' && (
+          <AppsOption
+            titleLabel={t('Apps')}
+            autoUpdatesLabel={t('Automatic Updates')}
+            autoUpdatesInfo={t(
+              'Ensure you have all the latest version of your dapps, widgets & plugins. ',
+            )}
+            dataAnalyticsLabel={t('Data & Analytics')}
+            dataAnalyticsinfo={t(
+              'We track usage data and report any bugs or issues, by activting data will be able to report any issues one app usage.',
+            )}
+            checkedAutoUpdates={checkedAutoUpdates}
+            checkedDataAnalytics={checkedDataAnalytics}
+            onAutoUpdatesChange={handleAutoUpdatesChange}
+            onDataAnalyticsChange={handleDataAnalyticsChange}
+            OnChevronLeftClick={handleChevronLeftClick}
+          />
+        )}
+
+        {activeOption === 'Appearance' && (
+          <AppearanceOption
+            titleLabel="Appearance"
+            appThemeLabel={t('App Theme')}
+            appThemeInfo={t("Choose your preference for the Ethereum World's theme")}
+            theme={theme}
+            onThemeSelect={handleThemeSelect}
+            OnChevronLeftClick={handleChevronLeftClick}
+          />
+        )}
       </BasicCardBox>
     </Box>
   );
