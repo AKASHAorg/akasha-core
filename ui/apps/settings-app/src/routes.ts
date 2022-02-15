07a9b7917784a909a0a7b7eb886708c65b9e6528
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2022-02-15 08:44:02 +0100

    chore(ui): define settings app routes

--- ui/apps/settings-app/src/routes.ts
+++ ui/apps/settings-app/src/routes.ts
@@ -1,6 +1,15 @@
 export const HOME = 'Home';
+export const PRIVACY = 'Privacy';
+export const APPEARANCE = 'Appearance';
+export const GENERAL = 'General';
+export const APPS = 'Apps';
+export const PLUGINS = 'Plugins';
+export const WIDGETS = 'Widgets';
 export const rootRoute = '/settings';
 
 export default {
   [HOME]: `${rootRoute}`,
+  [PRIVACY]: `${rootRoute}/privacy`,
+  [APPEARANCE]: `${rootRoute}/appearance`,
+  [APPS]: `${rootRoute}/apps`,
 };
