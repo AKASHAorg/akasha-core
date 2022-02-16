49f00891be34bc5f059cc1c0b1adf968cccdec15
alpharius <damir.sabolic@akasha.world>
2022-02-16 12:16:43 +0100

    chore(): fix test absolute path

--- tools/executors/__tests__/i18n.spec.ts
+++ tools/executors/__tests__/i18n.spec.ts
@@ -1,20 +1,15 @@
 import { getParserConfig } from '../i18n/impl';
 
-const expectedParserConfig = {
-  useKeysAsDefaultValue: true,
-  locales: ['en'],
-  verbose: true,
-  keySeparator: false,
-  nsSeparator: null,
-  defaultNamespace: 'some-namespace',
-  output:
-    '/home/blkns/akasha/akasha-world-framework/tools/executors/locales/$LOCALE/$NAMESPACE.json',
-  input: ['ui/apps/akasha/src/**/*.{ts,tsx}'],
-};
-
 describe('i18nExecutor', () => {
   it('should generate the correct parser config', async () => {
     const parserConfig = getParserConfig('some-namespace', 'ui/apps/akasha');
-    expect(parserConfig).toEqual(expectedParserConfig);
+    expect(parserConfig.useKeysAsDefaultValue).toBeTruthy();
+    expect(parserConfig.locales).toContain('en');
+    expect(parserConfig.verbose).toBeTruthy();
+    expect(parserConfig.keySeparator).toBeFalsy();
+    expect(parserConfig.nsSeparator).toBeNull();
+    expect(parserConfig.defaultNamespace).toEqual('some-namespace');
+    expect(parserConfig.output).toContain('/locales/$LOCALE/$NAMESPACE.json');
+    expect(parserConfig.input).toContain('ui/apps/akasha/src/**/*.{ts,tsx}');
   });
 });
