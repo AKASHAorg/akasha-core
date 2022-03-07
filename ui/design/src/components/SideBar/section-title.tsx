aa35fb6c1f7528b0457827aa5010a6b8d74576c5
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2022-03-07 21:28:00 +0100

    refactor(ui): update SectionTitle - update dummy data

--- ui/design/src/components/SideBar/section-title.tsx
+++ ui/design/src/components/SideBar/section-title.tsx
@@ -5,21 +5,29 @@ import Icon from '../Icon';
 
 interface ISectionTitleProps {
   titleLabel: string;
+  subtitleLabel?: string;
 }
 
 const SectionTitle: React.FC<ISectionTitleProps> = props => {
+  const { titleLabel, subtitleLabel } = props;
   return (
-    <Box direction="row" justify="start" align="center" fill={true}>
+    <Box direction="row" justify="start" align="start" fill={true} margin={{ bottom: 'xsmall' }}>
       <Icon type="app" plain={true} />
-      <Text
-        margin={{ left: 'medium' }}
-        weight={700}
-        color="secondaryText"
-        size="large"
-        style={{ padding: '0.5rem 0rem', textTransform: 'uppercase' }}
-      >
-        {props.titleLabel}
-      </Text>
+      <Box margin={{ left: 'medium' }}>
+        <Text
+          weight={700}
+          color="secondaryText"
+          size="large"
+          style={{ padding: '0rem', textTransform: 'uppercase' }}
+        >
+          {titleLabel}
+        </Text>
+        {subtitleLabel && (
+          <Text size="medium" color="subtitleText">
+            {subtitleLabel}
+          </Text>
+        )}
+      </Box>
     </Box>
   );
 };
