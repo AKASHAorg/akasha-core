0e9c9ea7a43b2ee232184ce7478274340a698fff
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2022-03-25 19:22:52 +0100

    chore(ui): revert default app-center route

--- ui/apps/app-center/src/index.tsx
+++ ui/apps/app-center/src/index.tsx
@@ -5,7 +5,7 @@ import {
   MenuItemAreaType,
   IntegrationRegistrationOptions,
 } from '@akashaproject/ui-awf-typings/lib/app-loader';
-import routes, { EXPLORE, rootRoute } from './routes';
+import routes, { rootRoute } from './routes';
 
 /**
  * All the plugins must export an object like this:
@@ -28,7 +28,7 @@ export const register: (opts: IntegrationRegistrationOptions) => IAppConfig = op
     label: 'Integration Center',
     area: [MenuItemAreaType.QuickAccessArea, MenuItemAreaType.AppArea],
     logo: { type: LogoTypeSource.ICON, value: 'appCenter' },
-    route: routes[EXPLORE],
+    route: rootRoute,
     subRoutes: [],
   },
   extends: [
