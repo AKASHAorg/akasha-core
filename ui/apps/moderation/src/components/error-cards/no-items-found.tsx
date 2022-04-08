2e11fc0fc4fff24a9762e050cdabfbd42b21c864
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2022-04-08 22:37:43 +0100

    refactor(ui): update component

--- ui/apps/moderation/src/components/error-cards/no-items-found.tsx
+++ ui/apps/moderation/src/components/error-cards/no-items-found.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next';
 
 import DS from '@akashaproject/design-system';
 
-const { Text } = DS;
+const { Box, Icon, Text } = DS;
 
 export interface INoItemsFound {
   activeTab: string;
@@ -12,9 +12,17 @@ export interface INoItemsFound {
 const NoItemsFound: React.FC<INoItemsFound> = ({ activeTab }) => {
   const { t } = useTranslation('app-moderation-ewa');
   return (
-    <Text textAlign="center">
-      {t('No {{activeTab}} items found. Please check again later', { activeTab })}
-    </Text>
+    <Box pad={{ top: 'xlarge', horizontal: 'small', bottom: 'small' }} align="center">
+      <Icon type="appModeration" size="xxl" accentColor={true} />
+      <Text size="large" textAlign="center" weight="bold">
+        {t('No {{activeTab}} items', { activeTab })}
+      </Text>
+      <Text size="large" textAlign="center" margin={{ top: 'small' }}>
+        {t('There are no {{activeTab}} items at the moment. Please check back later', {
+          activeTab,
+        })}
+      </Text>
+    </Box>
   );
 };
 
