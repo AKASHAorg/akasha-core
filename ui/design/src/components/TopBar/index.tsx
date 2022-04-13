144eb5630476db0c380e6697cd069fb3c8fa964e
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2022-04-13 15:31:15 +0100

    chore(ui): update styles

--- ui/design/src/components/TopBar/index.tsx
+++ ui/design/src/components/TopBar/index.tsx
@@ -290,7 +290,12 @@ const Topbar: React.FC<ITopbarProps> = props => {
               quickAccessItems.map(renderPluginButton)}
             {!isMobileOnly && !loggedProfileData?.ethAddress && !shouldRenderOnboarding && (
               <Box direction="row" align="center" gap="xsmall">
-                <Button onClick={onLoginClick} label={signInLabel} />
+                <Button
+                  onClick={onLoginClick}
+                  label={signInLabel}
+                  slimBorder={true}
+                  hoverIndicator="accentText"
+                />
                 <Button primary={true} onClick={onSignUpClick} label={signUpLabel} />
               </Box>
             )}
