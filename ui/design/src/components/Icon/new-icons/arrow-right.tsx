572d587af55587a486d88f16dfb5360efccb3a5b
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2022-04-27 08:49:04 +0100

    chore(ui): update icons

--- ui/design/src/components/Icon/new-icons/arrow-right.tsx
+++ ui/design/src/components/Icon/new-icons/arrow-right.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ArrowRight = (props: React.SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" {...props}>
     <g fill="none" fillRule="evenodd">
-      <g stroke="#4E71FF" strokeLinecap="round" strokeLinejoin="round" transform="translate(2 4)">
+      <g stroke="#8b9FFF" strokeLinecap="round" strokeLinejoin="round" transform="translate(2 4)">
         <line x2="11.018" y1="4" y2="4" />
         <polyline points="8 0 12 4 8 8" />
       </g>
