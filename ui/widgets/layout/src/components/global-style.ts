26490464e91f4305f121f0b9000884a3ed66cc0d
SeverS <sever.abi@gmail.com>
2022-05-09 16:57:19 +0300

    chore(): revert body width

--- ui/widgets/layout/src/components/global-style.ts
+++ ui/widgets/layout/src/components/global-style.ts
@@ -29,9 +29,6 @@ export const GlobalStyle = createGlobalStyle<{
     padding: 0;
     background-color: ${props => props.theme.colors.background};
   }
-  body {
-    width: 100%;
-  }
   body.noscroll {
     overflow: hidden;
   }
