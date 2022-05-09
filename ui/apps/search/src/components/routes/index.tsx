ec976274d61ba2e0668e80856bbf3418e3ff8a4f
Vali Cotea <quinine35@gmail.com>
2022-05-09 16:09:40 +0300

    fix(): search page redirect

--- ui/apps/search/src/components/routes/index.tsx
+++ ui/apps/search/src/components/routes/index.tsx
@@ -1,7 +1,7 @@
 import DS from '@akashaproject/design-system';
 import * as React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import routes, { ONBOARDING, RESULTS } from '../../routes';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import routes, { rootRoute, ONBOARDING, RESULTS } from '../../routes';
 import SearchPage from './search-page';
 import OnboardingPage from './onboarding-page';
 import { RootComponentProps } from '@akashaproject/ui-awf-typings';
@@ -20,6 +20,7 @@ const Routes: React.FC<RootComponentProps> = props => {
     <Router>
       <Box>
         <Switch>
+          <Redirect exact={true} from={rootRoute} to={routes[RESULTS]} />
           <Route path={`${routes[RESULTS]}/:searchKeyword?`}>
             <SearchPage {...props} showLoginModal={showLoginModal} loginState={loginQuery.data} />
           </Route>
