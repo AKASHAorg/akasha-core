76d096a178349a0f6d39ecbaadb78ca8360cf712
SeverS <sever.abi@gmail.com>
2022-05-27 17:10:58 +0300

    chore(): rebuild preview

--- ui/app-loader/src/extensions.ts
+++ ui/app-loader/src/extensions.ts
@@ -151,7 +151,7 @@ export const mountMatchingExtensionParcels = (opts: {
       }
       if (!parcel && parentConfig) {
         try {
-          logger.info(`[extensions]: Mounting parcel: ${extInfo.extID}`);
+          logger.info(`[extensions]: Mounting parcel: ${extInfo.extID}, ${state.extensionParcels}`);
           parcel = singleSpa.mountRootParcel<RootExtensionProps>(
             extInfo.extension.loadingFn,
             extensionProps,
