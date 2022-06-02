703d59e770338bee2f2c2f1a8fbc3360250ca058
alpharius <damir.sabolic@akasha.world>
2022-06-02 18:11:25 +0200

    tests(): setup working config for integration testing of apps - akasha
    e2e(): force reload of feed page on failed nav

---
+++ ui/apps/akasha/src/components/__tests__/feed-page.spec.tsx
@@ -0,0 +1,70 @@
+import * as React from 'react';
+import * as singleSpa from 'single-spa';
+import FeedPage from '../feed-page/feed-page';
+
+import {
+  RenderResult,
+  renderWithAllProviders,
+  uiEventsMock,
+  genWorldConfig,
+} from '@akashaorg/af-testing';
+import { act } from 'react-dom/test-utils';
+import { AnalyticsProvider } from '@akashaorg/ui-awf-hooks/lib/use-analytics';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+jest.mock('@akashaorg/ui-awf-typings/lib/app-loader', () => ({
+  ItemTypes: {
+    ENTRY: 0,
+    PROFILE: 1,
+    COMMENT: 2,
+    TAG: 3,
+  },
+}));
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      cacheTime: 0,
+    },
+  },
+});
+
+describe('< FeedPage /> component', () => {
+  let renderResult: RenderResult;
+  const BaseComponent = (
+    <QueryClientProvider contextSharing={true} client={queryClient}>
+      <AnalyticsProvider uiEvents={uiEventsMock}>
+        <FeedPage
+          showLoginModal={jest.fn()}
+          loginState={{ isReady: true, pubKey: '0x00', ethAddress: '0x00' }}
+          uiEvents={uiEventsMock}
+          domElement={document.body}
+          layoutConfig={{
+            modalSlotId: '',
+            pluginSlotId: '',
+            topbarSlotId: '',
+            rootWidgetSlotId: '',
+            widgetSlotId: '',
+            sidebarSlotId: '',
+          }}
+          logger={null}
+          singleSpa={singleSpa}
+          activeModal={null}
+          worldConfig={genWorldConfig()}
+          parseQueryString={jest.fn()}
+          navigateToModal={jest.fn()}
+        />
+      </AnalyticsProvider>
+    </QueryClientProvider>
+  );
+  beforeEach(() => {
+    act(() => {
+      renderResult = renderWithAllProviders(BaseComponent, {});
+    });
+  });
+
+  it('should be defined', async () => {
+    expect(true).toBe(true);
+  });
+});
