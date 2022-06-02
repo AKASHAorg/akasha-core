0cf6a98e61824ed9ebd84764612faa5ae9968b3d
SeverS <sever.abi@gmail.com>
2022-06-02 10:58:26 +0300

    chore(): add messaging app

---
+++ ui/apps/messaging/src/components/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import singleSpaReact from 'single-spa-react';
+import App from './App';
+import { ThemeWrapper, withProviders } from '@akashaorg/ui-awf-hooks';
+import { RootComponentProps } from '@akashaorg/ui-awf-typings';
+import DS from '@akashaorg/design-system';
+
+const { ErrorLoader } = DS;
+
+const reactLifecycles = singleSpaReact({
+  React,
+  ReactDOM,
+  rootComponent: withProviders(App),
+  errorBoundary: (error, errorInfo, props: RootComponentProps) => {
+    if (props.logger) {
+      props.logger.error(`${JSON.stringify(error)}, ${errorInfo}`);
+    }
+    return (
+      <ThemeWrapper {...props}>
+        <ErrorLoader type="script-error" title="Error in messaging app" details={error.message} />
+      </ThemeWrapper>
+    );
+  },
+});
+
+export const bootstrap = reactLifecycles.bootstrap;
+
+export const mount = reactLifecycles.mount;
+
+export const unmount = reactLifecycles.unmount;
---
+++ ui/apps/messaging/src/routes.tsx
@@ -0,0 +1,8 @@
+export const rootRoute = '/messaging';
+export const inboxRoute = `${rootRoute}/inbox`;
+
+export const MESSAGING = 'Messaging';
+
+export default {
+  [MESSAGING]: rootRoute,
+};
