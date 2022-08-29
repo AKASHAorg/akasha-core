34b7a984150b1ff83b0f8d56c8a4aa5e69e1ad42
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2022-08-29 13:53:34 +0100

    feat(ui): define licenses

---
+++ ui/apps/articles/src/utils/licenses.ts
@@ -0,0 +1,134 @@
+export const licences = [
+  {
+    id: '1',
+    label: 'All rights reserved',
+    description: [
+      {
+        icon: 'copyright-1',
+        text:
+          'Others cannot copy, distribute, or perform your work without your' +
+          ' permission (or as permitted by fair use).',
+      },
+    ],
+  },
+  {
+    id: '2',
+    label: 'Some rights reserved',
+    description: [
+      { icon: 'copyright-2', text: 'There are some rights reserved with this licence.' },
+    ],
+  },
+  {
+    id: '3',
+    label: 'No rights reserved',
+    description: [{ icon: 'CCZERO', text: 'You will not have any rights.' }],
+  },
+  {
+    id: '4',
+    label: 'Attribution',
+    description: [
+      {
+        icon: 'CCBY',
+        text: `Others can distribute, remix, and build upon your work as long as they credit you.`,
+      },
+    ],
+  },
+  {
+    id: '5',
+    label: 'Attribution, no derivatives',
+    description: [
+      {
+        icon: 'CCBY',
+        text: `Others can distribute, remix, and build upon your work as long as they credit you.`,
+      },
+      {
+        icon: 'CCND',
+        text: 'Others can only distribute non-derivative copies of your work.',
+      },
+    ],
+  },
+  {
+    id: '6',
+    label: 'Attribution, share-alike',
+    description: [
+      {
+        icon: 'CCBY',
+        text: `Others can distribute, remix, and build upon your work as long as they credit you.`,
+      },
+      {
+        icon: 'CCSA',
+        text: 'Others must distribute derivatives of your work under the same license.',
+      },
+    ],
+  },
+  {
+    id: '7',
+    label: 'Attribution, non-commercial',
+    description: [
+      {
+        icon: 'CCBY',
+        text: `Others can distribute, remix, and build upon your work as long as they credit you.`,
+      },
+      {
+        icon: 'CCNC',
+        text: 'Others can use your work for non-commercial purposes only. ',
+      },
+    ],
+  },
+  {
+    id: '8',
+    label: 'Attribution, non-commercial, no-derivatives',
+    description: [
+      {
+        icon: 'CCBY',
+        text: `Others can distribute, remix, and build upon your work as long as they credit you.`,
+      },
+      {
+        icon: 'CCNC',
+        text: 'Others can use your work for non-commercial purposes only. ',
+      },
+      {
+        icon: 'CCND',
+        text: 'Others can only distribute non-derivative copies of your work.',
+      },
+    ],
+  },
+  {
+    id: '9',
+    label: 'Attribution, non-commercial, share-alike',
+    description: [
+      {
+        icon: 'CCBY',
+        text: `Others can distribute, remix, and build upon your work as long as they credit you.`,
+      },
+      {
+        icon: 'CCNC',
+        text: 'Others can use your work for non-commercial purposes only. ',
+      },
+      {
+        icon: 'CCSA',
+        text: 'Others must distribute derivatives of your work under the same license.',
+      },
+    ],
+  },
+  {
+    id: '10',
+    label: 'Creative Commons copyright waiver',
+    description: [
+      {
+        icon: 'CCZERO',
+        text: 'You waive all your copyright and related rights in this work, worldwide.',
+      },
+    ],
+  },
+  {
+    id: '11',
+    label: 'Public Domain',
+    description: [
+      {
+        icon: 'CCPD',
+        text: 'This work is already in the public domain and free of copyright restrictions.',
+      },
+    ],
+  },
+];
