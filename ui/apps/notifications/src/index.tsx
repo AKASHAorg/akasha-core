3424fe5e580d0a2132bd083f43c497cd49eb4ecc
SeverS <sever.abi@gmail.com>
2022-09-05 11:34:48 +0300

    chore(): pass notifications via the plugin

--- ui/apps/notifications/src/index.tsx
+++ ui/apps/notifications/src/index.tsx
@@ -7,6 +7,22 @@ import {
   RootComponentProps,
 } from '@akashaorg/typings/ui';
 import { NotificationPlugin } from './plugins/notification-plugin';
+import getSDK from '@akashaorg/awf-sdk';
+export const initialize = (options: IntegrationRegistrationOptions) => {
+  const notification: any = options.plugins.notification;
+  const sdk = getSDK();
+
+  if (notification && typeof notification.notify === 'function') {
+    sdk.api.auth.getMessages({}).subscribe({
+      next: messages => {
+        notification.notify('@akashaproject/notifications', messages);
+      },
+      error: err => {
+        options.logger.error(`Error in receiving notification messages: ${err}`);
+      },
+    });
+  }
+};
 
 /**
  * All the plugins must export an object like this:
