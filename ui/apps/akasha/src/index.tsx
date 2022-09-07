e8001c0c0647a91b71b93662caf34899628b2c1d
SeverS <sever.abi@gmail.com>
2022-09-07 15:52:16 +0300

    chore(): improve initialization example

--- ui/apps/akasha/src/index.tsx
+++ ui/apps/akasha/src/index.tsx
@@ -9,24 +9,25 @@ import {
 } from '@akashaorg/typings/ui';
 
 /**
- * Example of initialization and triggerin of a notification
+ * Initialization of the integration is optional.
+ * It is called before all `register` calls but after all `getPlugin`;
+ * @example Initialization an integration and triggerin of a notification
+ * ```
+ * export const initialize: (opts: IntegrationInitOptions) => void = opts => {
+ *  const notificationPlugin: any = opts.plugins.notification;
+ *  if (notificationPlugin) {
+ *   notificationPlugin.listenLogin(
+ *     (userData: { ethAddress: string; filAddress: string; pubKey: string }) => {
+ *       // user is now logged in,
+ *       // we can fetch some notification data for eth address (or pubKey)
+ *       // then trigger a notification
+ *       notificationPlugin.notify('test_notif_domain', { message: 'This is a notification' });
+ *     },
+ *   );
+ *  }
+ * };
+ * ```
  */
-
-/*
-export const initialize: (opts: IntegrationInitOptions) => void = opts => {
-  const notificationPlugin: any = opts.plugins.notification;
-  if (notificationPlugin) {
-    notificationPlugin.listenLogin(
-      (userData: { ethAddress: string; filAddress: string; pubKey: string }) => {
-        // user is now logged in,
-        // we can fetch some notification data for eth address (or pubKey)
-        // then trigger a notification
-        notificationPlugin.notify('test_notif_domain', { message: 'This is a notification' });
-      },
-    );
-  }
-};
-*/
 
 export const register: (opts: IntegrationRegistrationOptions) => IAppConfig = opts => ({
   loadingFn: () => import('./components'),
