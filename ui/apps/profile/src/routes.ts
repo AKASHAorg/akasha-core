3de65f659826ea8e19a4299b17755e9c16afa058
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2022-10-07 04:47:17 +0100

    chore(apps): update routes

--- ui/apps/profile/src/routes.ts
+++ ui/apps/profile/src/routes.ts
@@ -6,6 +6,14 @@ export const ONBOARDING_STEP_ONE = 'OnboardingStepOne';
 export const ONBOARDING_STEP_TWO = 'OnboardingStepTwo';
 export const ONBOARDING_STEP_THREE = 'OnboardingStepThree';
 export const ONBOARDING_STEP_FOUR = 'OnboardingStepFour';
+export const DEV_KEYS = 'DevKeys';
+export const ADD_DEV_KEY = 'AddDevKey';
+export const EDIT_MESSAGE_NAME = 'EditMessageName';
+export const REMOVE_DEV_KEY = 'RemoveDevKey';
+export const PUBLISHED_APPS = 'PublishedApps';
+export const SIGN_MESSAGE = 'SignMessage';
+export const VERIFY_SIGNATURE = 'VerifySignature';
+
 export const baseDeveloperRoute = '/my-profile/developer';
 export const rootRoute = '';
 
@@ -17,4 +25,11 @@ export default {
   [ONBOARDING_STEP_TWO]: `${baseDeveloperRoute}/onboarding/download-cli-tool`,
   [ONBOARDING_STEP_THREE]: `${baseDeveloperRoute}/onboarding/message`,
   [ONBOARDING_STEP_FOUR]: `${baseDeveloperRoute}/onboarding/dev-key-confirmation`,
+  [DEV_KEYS]: `${baseDeveloperRoute}/dev-keys`,
+  [ADD_DEV_KEY]: `${baseDeveloperRoute}/dev-keys/add`,
+  [EDIT_MESSAGE_NAME]: `${baseDeveloperRoute}/dev-keys/:pubKey/edit`,
+  [REMOVE_DEV_KEY]: `${baseDeveloperRoute}/dev-keys/:pubKey/delete`,
+  [PUBLISHED_APPS]: `${baseDeveloperRoute}/published-apps`,
+  [SIGN_MESSAGE]: `${baseDeveloperRoute}/sign-message`,
+  [VERIFY_SIGNATURE]: `${baseDeveloperRoute}/verify-signature`,
 };
