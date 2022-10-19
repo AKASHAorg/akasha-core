6ec751bd751d1b63264bde14f0ed452b2293edfd
Marius Darila <marius.darila@gmail.com>
2022-10-19 12:39:23 +0200

    chore(): publish apps

--- sdk/src/contracts/scripts/update-app-registry.js
+++ sdk/src/contracts/scripts/update-app-registry.js
@@ -1,40 +1,44 @@
 /* eslint-disable */
 const { ethers } = require('hardhat');
 const semver = require('semver');
-const pkgInfos = require('../../../../build/integrations_bucket.json');
-async function main() {
-  const IntegrationRegistry = await ethers.getContractFactory('IntegrationRegistry');
+
+const pkgInfos = process.env.DEPLOY_PRODUCTION_REGISTRY
+  ? require('../../../../build/integrations_bucket_prod.json')
+  : require('../../../../build/integrations_bucket.json');
+
+async function main () {
+  const IntegrationRegistry = await ethers.getContractFactory("IntegrationRegistry");
   const integrationRegistry = await IntegrationRegistry.attach(
-    process.env.INTEGRATION_REGISTRY_ADDRESS,
+    process.env.INTEGRATION_REGISTRY_ADDRESS
   );
 
-  for(const pkg of pkgInfos){
-    if(typeof pkg.type !== "number"){
+  for (const pkg of pkgInfos) {
+    if (typeof pkg.type !== "number") {
       continue;
     }
     const packageInfo = await integrationRegistry.getPackageInfo(pkg.id);
-    const manifestHash = '0x'+pkg.ipfsManifest.substring(1);
+    const manifestHash = "0x" + pkg.ipfsManifest.substring(1);
     let version;
-    if(!packageInfo || !packageInfo.integrationName){
-      version = 'v0.1.0';
+    if (!packageInfo || !packageInfo.integrationName) {
+      version = "v0.1.0";
     } else {
       const currentRelease = await integrationRegistry.getReleaseData(packageInfo.latestReleaseId);
-      version = 'v' + semver.inc(currentRelease.version, 'minor');
-      if(currentRelease.manifestHash === manifestHash){
-        console.info('skipping new release for ', pkg.name);
+      version = "v" + semver.inc(currentRelease.version, "minor");
+      if (currentRelease.manifestHash === manifestHash) {
+        console.info("skipping new release for ", pkg.name);
         continue;
       }
     }
-    console.log('deploying: ', pkg.name, ' version: ',version );
+    console.log("deploying: ", pkg.name, " version: ", version);
     const tx = await integrationRegistry.release(
       pkg.name,
       version,
       manifestHash,
-      pkg.type,
+      pkg.type
     );
-    console.info('tx.hash:', tx.hash);
+    console.info("tx.hash:", tx.hash);
     await tx.wait(1);
-    console.log('deployed: ', pkg.name, "\n");
+    console.log("deployed: ", pkg.name, "\n");
   }
 }
 
