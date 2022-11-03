8ea581449ee713c393e2576f19e47da1c76fa78b
Didd <didd.tuni@akasha.world>
2022-11-03 17:06:19 +0300

    fix: remove code introduced by merge conflict fix

--- ui/apps/akasha/src/extensions/entry-edit-button.tsx
+++ ui/apps/akasha/src/extensions/entry-edit-button.tsx
@@ -19,17 +19,6 @@ const EntryEditButton: React.FC<RootExtensionProps> = props => {
         }/${props.extensionData.entryId}?action=edit`,
       );
     }
-
-    if (
-      props.extensionData &&
-      props.extensionData.hasOwnProperty('commentId') &&
-      props.extensionData?.entryType === EntityTypes.COMMENT
-    ) {
-      props.singleSpa.navigateToUrl(
-        `${window.location.origin}/@akashaorg/app-akasha-integration/reply/${props.extensionData.commentId}?action=edit`,
-      );
-      return;
-    }
   };
 
   const entryTypeLabel = React.useMemo(() => {
