e694a0f035663ab28d0ca23bb10e22c3bc31a18f
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2022-11-03 09:49:13 +0100

    feat(moderation): create and implement values card

---
+++ ui/apps/moderation/src/components/values-card/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import DS from '@akashaorg/design-system';
+
+type ModerationValue = {
+  path: string;
+  title: string;
+  assetName: string;
+  description: string;
+};
+export interface ModerationValuesCardProps {
+  titleLabel: string;
+  subtitleLabel: string;
+  ctaLabel: string;
+  ctaUrl?: string;
+  values: ModerationValue[];
+  onValueClick: (path: ModerationValue['path']) => () => void;
+}
+
+const { Anchor, BasicCardBox, Box, ModerationValueCard, Text } = DS;
+
+const ModerationValuesCard: React.FC<ModerationValuesCardProps> = props => {
+  const { titleLabel, subtitleLabel, ctaLabel, ctaUrl, values, onValueClick } = props;
+
+  return (
+    <BasicCardBox pad="medium" gap="medium" margin={{ bottom: 'medium' }}>
+      <Text size="large" weight="bold" textAlign="center">
+        {titleLabel}
+      </Text>
+      <Text size="medium" textAlign="center" margin={{ bottom: 'xxsmall' }}>
+        {subtitleLabel}
+      </Text>
+      <Box direction="row" wrap={true} justify="between">
+        {values.map((value, idx) => (
+          <Box
+            key={value.title + idx}
+            width="31.5%"
+            height="7.5rem"
+            margin={{ ...(idx < values.length - 3 && { bottom: 'medium' }) }}
+            onClick={onValueClick(value.path)}
+          >
+            <ModerationValueCard isMini={true} label={value.title} assetName={value.assetName} />
+          </Box>
+        ))}
+      </Box>
+      {ctaLabel && (
+        <Anchor
+          href={ctaUrl}
+          size="large"
+          weight="normal"
+          target="_blank"
+          color="accentText"
+          margin={{ vertical: 'small' }}
+          style={{ textDecoration: 'none', textAlign: 'end' }}
+        >
+          {ctaLabel}
+        </Anchor>
+      )}
+    </BasicCardBox>
+  );
+};
+
+export default ModerationValuesCard;
--- ui/apps/moderation/src/pages/intro.tsx
+++ ui/apps/moderation/src/pages/intro.tsx
@@ -4,11 +4,14 @@ import { useTranslation } from 'react-i18next';
 import DS from '@akashaorg/design-system';
 import { RootComponentProps } from '@akashaorg/typings/ui';
 
+import ModerationValuesCard from '../components/values-card';
+
 import { ISharedModerationProps } from '../interfaces';
 
 import routes, { GUEST, HOME } from '../routes';
+import { values } from '../services/values';
 
-const { ModerationIntroCard } = DS;
+const { Box, ModerationIntroCard } = DS;
 
 const IntroPage: React.FC<ISharedModerationProps & RootComponentProps> = props => {
   const { user, isAuthorised, plugins } = props;
@@ -41,22 +44,45 @@ const IntroPage: React.FC<ISharedModerationProps & RootComponentProps> = props =
     });
   };
 
+  const handleValueClick = (path: string) => () => {
+    routing.navigateTo({
+      appName: '@akashaorg/app-moderation-ewa',
+      getNavigationUrl: () => `/values/${path}`,
+    });
+  };
+
   return (
-    <ModerationIntroCard
-      titleLabel={t('Moderating')}
-      subtitleLabel={t('Welcome to the Dashboard')}
-      isIntro={true}
-      introLabel={t('Are you interested in Moderating?')}
-      descriptionLine1Label={t(
-        "We are currently creating new Moderating Systems for Ethereum World. If you'd like to join us click",
-      )}
-      ctaLabel="here"
-      ctaUrl="https://www.notion.so/akasha-foundation/The-AKASHA-Moderating-Open-Design-Challenge-15cb49cf57e740be92534958828ca210"
-      descriptionLine2IntroLabel={t('Visit our')}
-      codeOfConductLabel={t('Code of Conduct')}
-      descriptionLine2Label={t('to learn more about our moderation criteria')}
-      onCodeOfConductClick={handleCodeOfConductClick}
-    />
+    <Box gap="small">
+      <ModerationIntroCard
+        titleLabel={t('Moderating')}
+        subtitleLabel={t('Welcome to the Dashboard')}
+        isIntro={true}
+        introLabel={t('Are you interested in Moderating?')}
+        descriptionLine1Label={t(
+          "We are currently creating new Moderating Systems for Ethereum World. If you'd like to join us click",
+        )}
+        ctaLabel="here"
+        ctaUrl="https://www.notion.so/akasha-foundation/The-AKASHA-Moderating-Open-Design-Challenge-15cb49cf57e740be92534958828ca210"
+        descriptionLine2IntroLabel={t('Visit our')}
+        codeOfConductLabel={t('Code of Conduct')}
+        descriptionLine2Label={t('to learn more about our moderation criteria')}
+        onCodeOfConductClick={handleCodeOfConductClick}
+      />
+
+      <ModerationValuesCard
+        titleLabel={t('Our Values')}
+        subtitleLabel={t('The principles guiding our behaviour')}
+        ctaLabel={t('Propose your own')}
+        ctaUrl=""
+        values={values.map(value => ({
+          path: value.path,
+          title: t('{{title}}', { title: value.title }),
+          assetName: value.assetName,
+          description: value.description,
+        }))}
+        onValueClick={handleValueClick}
+      />
+    </Box>
   );
 };
 
