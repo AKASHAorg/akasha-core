e9df567bdaee8891dee5d0a91bd1b969c02cf247
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2022-11-08 18:29:07 +0100

    chore(moderation): update values descriptions

--- ui/apps/moderation/src/services/values.ts
+++ ui/apps/moderation/src/services/values.ts
@@ -4,62 +4,62 @@ export const values = [
     title: 'Transparency',
     assetName: 'transparency',
     description:
-      'We act with transparency as individuals and live by our values as a community. We will never compromise our values, even if this impacts our revenues.',
+      'It needs to be easy for everyone to see what actions are performed. Our communities shall be build on tools and processes that strive for openness, communication and accountability.',
   },
   {
     path: 'people-first',
     title: 'People First',
     assetName: 'peoplefirst',
     description:
-      'We act with transparency as individuals and live by our values as a community. We will never compromise our values, even if this impacts our revenues.',
+      'Everyone is welcome according to their abilities. We do not discriminate against anyone based on their specific, inborn traits. We take care of everyone.',
   },
   {
     path: 'security',
     title: 'Security',
     assetName: 'security',
     description:
-      'We act with transparency as individuals and live by our values as a community. We will never compromise our values, even if this impacts our revenues.',
+      'We expect that our communities actions are such that they avert danger from its members. We expect everyone to be free from fear or anxiety.',
   },
   {
     path: 'pragmatism',
     title: 'Pragmatism',
     assetName: 'pragmatism',
     description:
-      'We act with transparency as individuals and live by our values as a community. We will never compromise our values, even if this impacts our revenues.',
+      'Good values are those for which we have good reason. We act in a humanistic way, that means, there is no ultimate test of morality beyond what matters for us.',
   },
   {
     path: 'human-centric-products',
     title: 'Human-Centric Products',
     assetName: 'humancentricproducts',
     description:
-      'We act with transparency as individuals and live by our values as a community. We will never compromise our values, even if this impacts our revenues.',
+      'Our work places humans rather than customers at the center. Tools and services available need to resonate with the audience. We expect designers and creators to listen to the community.',
   },
   {
     path: 'community-resilience',
     title: 'Community Resilience',
     assetName: 'communityresilience',
     description:
-      'We act with transparency as individuals and live by our values as a community. We will never compromise our values, even if this impacts our revenues.',
+      'We strive for the sustained ability of our community to use available resources to respond to, withstand, and recover from adverse situations.',
   },
   {
     path: 'generosity',
     title: 'Generosity',
     assetName: 'generosity',
     description:
-      'We act with transparency as individuals and live by our values as a community. We will never compromise our values, even if this impacts our revenues.',
+      'We strive giving good things to others freely and in abundance. Acts of generosity help the person receiving it as well as the giver making our community ever more strong and resilient.',
   },
   {
     path: 'intellectual-honesty',
     title: 'Intellectual Honesty',
     assetName: 'intellectualhonesty',
     description:
-      'We act with transparency as individuals and live by our values as a community. We will never compromise our values, even if this impacts our revenues.',
+      'Honesty is the best policy. Our actions in this community are driven by sincerity and integrity. We call out obvious contradictions to our peers.',
   },
   {
     path: 'inclusivity',
     title: 'Inclusivity',
     assetName: 'inclusivity',
     description:
-      'We act with transparency as individuals and live by our values as a community. We will never compromise our values, even if this impacts our revenues.',
+      'Inclusion is a value and a way of thinking. We value people for who they are. We appreciate diversity, equality and celebrate cooperativeness. We want to create sustainable communities.',
   },
 ];
