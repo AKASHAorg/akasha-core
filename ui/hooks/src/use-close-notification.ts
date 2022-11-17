0b265de4dd421ccf23994bc5a39ff9569d3e53fd
themonster2015 <quynhyen.vothi@akasha.world>
2022-11-17 19:47:56 +0000

    refactor

--- ui/hooks/src/use-close-notification.ts
+++ ui/hooks/src/use-close-notification.ts
@@ -5,23 +5,25 @@ export const enum CloseNotificationFlags {
   CLOSE_PRIVATE_ALPHA_NOTIFICATION_FLAG = 'close-private-alpha-notification',
 }
 
-export function useCloseNotification(key: CloseNotificationFlags, initialValue = false) {
-  const [closed, setClosed] = useState<boolean>(() => {
-    if (window?.localStorage) {
-      try {
-        const closedFlag = window.localStorage.getItem(key);
-        if (closedFlag) {
-          return JSON.parse(closedFlag);
-        } else {
-          window.localStorage.setItem(key, JSON.stringify(initialValue));
-          return initialValue;
-        }
-      } catch (err) {
-        // console.log(err);
+const getInitialCloseState = (key: CloseNotificationFlags, initialValue: boolean) => {
+  if (window?.localStorage) {
+    try {
+      const closedFlag = window.localStorage.getItem(key);
+      if (closedFlag) {
+        return JSON.parse(closedFlag);
+      } else {
+        window.localStorage.setItem(key, JSON.stringify(initialValue));
         return initialValue;
       }
+    } catch (err) {
+      // console.log(err);
+      return initialValue;
     }
-  });
+  }
+};
+
+export function useCloseNotification(key: CloseNotificationFlags, initialValue = false) {
+  const [closed, setClosed] = useState<boolean>(() => getInitialCloseState(key, initialValue));
 
   const setValue = (newClosedValue: boolean) => {
     try {
