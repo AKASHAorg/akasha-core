9e82c8e456ff5325590fd526032ef247799ff9ab
themonster2015 <quynhyen.vothi@akasha.world>
2022-11-25 14:07:30 +0100

    refactor

--- ui/design/src/components/Button/styled-button.ts
+++ ui/design/src/components/Button/styled-button.ts
@@ -9,7 +9,7 @@ const StyledButton = styled(Button)<{
   height: ${props => (props.height ? `${props.height}rem` : '2rem')};
   border-width: ${props => (props.slimBorder ? '1px' : 'initial')};
   border-radius: ${props => props.theme.shapes.smallBorderRadius};
-  border-color: ${props => (props.accentBorder ? props.theme.colors.accent : '')};
+  ${props => props.accentBorder && `border-color: ${props.theme.colors.accent}`};
   color: ${props => props.theme.colors.btnAccentColor};
   white-space: nowrap;
   &:hover {
