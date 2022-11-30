8648af5db60b7155a7d86f1ce781980b2859c218
Didd <35398733+didd@users.noreply.github.com>
2022-11-30 14:37:58 +0300

    Fix: failing widgets unit tests (#1824)

--- ui/widgets/top-bar/jest.setup.js
+++ ui/widgets/top-bar/jest.setup.js
@@ -1,4 +1,30 @@
+import { mockSDK } from '@akashaorg/af-testing';
+
+jest.mock('@akashaorg/awf-sdk', () => {
+  return () => mockSDK({});
+});
+
 jest.mock('react-i18next', () => ({
   ...jest.requireActual('react-i18next'),
-  useTranslation: () => ({ t: (key) => key, i18n: { languages: ['en'] } }),
+  // this mock makes sure any components using the translate hook can use it without a warning being shown
+  useTranslation: () => {
+    return {
+      t: str => str,
+      i18n: {
+        changeLanguage: () => new Promise(() => {}),
+      },
+    };
+  },
 }));
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(() => ({
+    matches: false,
+  })),
+});
+
+Object.defineProperty(window, '__DEV__', {
+  writable: false,
+  value: false,
+});
