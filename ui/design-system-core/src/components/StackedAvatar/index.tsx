5236ed2f514af248e612f09af5a6e7542e7841cc
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2023-01-18 09:33:25 +0100

    feat(ds): create stackedAvatar component and story

---
+++ ui/design-system-core/src/components/StackedAvatar/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { tw } from 'twind';
+
+import Avatar from '../Avatar';
+
+import { IStackedAvatarProps } from '../../interfaces/stackedAvatar.interface';
+
+const StackedAvatar: React.FC<IStackedAvatarProps> = props => {
+  const { userData, maxAvatars, size } = props;
+  let data = userData;
+  if (maxAvatars) {
+    data = userData.slice(0, maxAvatars);
+  }
+
+  const renderStack = (level: number) => {
+    const zIndex = level + 1;
+
+    const className = `inline-flex z-${zIndex} -ml-${zIndex > 1 ? '3' : '0'}`;
+
+    return (
+      <div className={tw`${className}`}>
+        <Avatar
+          // clickable avatars
+          onClick={() => {
+            /** */
+          }}
+          ethAddress={data[level].ethAddress}
+          src={data[level].avatar}
+          size={size}
+          border="xs"
+        />
+        {level + 1 < data.length && renderStack(level + 1)}
+      </div>
+    );
+  };
+
+  const wrapperClass = `flex flex-row`;
+
+  return <div className={tw`${wrapperClass}`}>{renderStack(0)}</div>;
+};
+
+export default StackedAvatar;
