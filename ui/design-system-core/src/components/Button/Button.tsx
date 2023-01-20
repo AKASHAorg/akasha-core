e469aee3722c05baf6defd35285e600c0db939fe
themonster2015 <quynhyen.vothi@akasha.world>
2023-01-20 09:14:08 +0100

    chore():refactor

--- ui/design-system-core/src/components/Button/Button.tsx
+++ ui/design-system-core/src/components/Button/Button.tsx
@@ -19,13 +19,13 @@ export type ButtonProps = {
 };
 
 const sizing = {
-  xsmall: { textSize: 'text-xs', iconSize: 'h-3 w-3', buttonSize: 'w-1/12' },
-  small: { textSize: 'text-sm', iconSize: DefaultIconSize, buttonSize: 'w-2/12' },
-  regular: { textSize: 'text-lg', iconSize: 'h-5, w-5', buttonSize: 'w-3/12' },
-  large: { textSize: 'text-3xl', iconSize: 'h-7, w-7', buttonSize: 'w-4/12' },
+  xsmall: { textSize: 'text-xs', iconSize: 'h-3 w-3', buttonSize: 'w-24' },
+  small: { textSize: 'text-sm', iconSize: DefaultIconSize, buttonSize: 'w-48' },
+  regular: { textSize: 'text-lg', iconSize: 'h-5, w-5', buttonSize: 'w-64' },
+  large: { textSize: 'text-3xl', iconSize: 'h-7, w-7', buttonSize: 'w-96' },
 };
 
-const baseStyles = apply`text-center flex items-center justify-center	font-medium`;
+const baseStyles = apply`text-center flex items-center justify-center	font-medium md:max-md:w-full`;
 const textOnlyStyles = apply`
 ${baseStyles}
 text-secondary-light dark:text-secondary-dark hover:opacity-50 
@@ -33,35 +33,33 @@ disabled:opacity-50 disabled:cursor-not-allowed space-x-2
 `;
 
 export const Button: React.FC<PropsWithChildren<ButtonProps>> = props => {
+  const { label, size, greyBg, primary } = props;
   const loading = props.loading === true;
   const textonly = props.textonly === true || props.textonly !== undefined;
   const greyBgIcon =
-    (props.greyBg === true && props.label === undefined) ||
-    (props.greyBg === true && props.size === 'xsmall');
+    (greyBg === true && label === undefined) || (greyBg === true && size === 'xsmall');
 
   const isIconButton =
-    props.label !== undefined && !greyBgIcon
-      ? 'rounded-2xl space-x-2 py-2.5'
-      : 'rounded-full p-2.5';
-  const bgColor = props.primary
+    label !== undefined && !greyBgIcon ? 'rounded-2xl space-x-2 py-2.5 px-5' : 'rounded-full p-2.5';
+  const bgColor = primary
     ? greyBgIcon
       ? 'bg-grey8'
       : 'bg-primary'
     : ' bg-white hover:bg-secondary-dark';
   const textColor =
-    props.primary && !textonly
+    primary && !textonly
       ? 'text-white'
-      : props.primary && textonly
+      : primary && textonly
       ? 'text-secondary-light dark:text-secondary-dark'
       : 'text-secondary-light dark:text-white';
-  const border = props.primary
+  const border = primary
     ? ''
     : 'dark:border-1 dark:border-secondary-dark border-1 border-secondary-light';
   const iconRight = props.iconRight === true ? 'flex-row-reverse space-x-2 space-x-reverse' : '';
 
-  const buttonSize = (props.label !== undefined && sizing[props.size]?.buttonSize) ?? 'w-1/12';
-  const textSize = sizing[props.size]?.textSize ?? 'text-sm';
-  const iconSize = sizing[props.size] ? sizing[props.size]?.iconSize : DefaultIconSize;
+  const buttonSize = (label !== undefined && sizing[size]?.buttonSize) ?? 'w-48';
+  const textSize = sizing[size]?.textSize ?? 'text-sm';
+  const iconSize = sizing[size]?.iconSize ?? DefaultIconSize;
   const iconColor = greyBgIcon
     ? 'text-secondary-light'
     : textonly
@@ -95,7 +93,7 @@ export const Button: React.FC<PropsWithChildren<ButtonProps>> = props => {
   ) : (
     <button type="button" className={tw(instanceStyles)}>
       {<Icon icon={props.icon} styling={tw(instanceIconStyles)} />}
-      {!loading && !greyBgIcon && <p>{props.label}</p>}
+      {!loading && !greyBgIcon && <p>{label}</p>}
     </button>
   );
 };
