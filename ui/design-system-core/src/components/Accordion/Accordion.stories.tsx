4a9b9cf125ff789c5f8fcdab17f97fa95ccc000c
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2023-02-01 13:31:54 +0100

    feat(ds): create accordion component - add story

---
+++ ui/design-system-core/src/components/Accordion/Accordion.stories.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { tw } from '@twind/core';
+
+import Accordion, { IAccordionProps } from '.';
+import Avatar from '../Avatar';
+
+export default {
+  title: 'Accordion/Accordion',
+  component: Accordion,
+};
+
+const ethAddress = '0x003410490050000320006570034567114572000';
+
+const Template = (args: IAccordionProps) => (
+  <div style={{ width: '15%' }}>
+    <Accordion {...args} />
+  </div>
+);
+
+const Title = (
+  <div className={tw('flex flex-row items-center')}>
+    <Avatar ethAddress={ethAddress} src={{ url: 'https://placebeard.it/360x360' }} />
+    <p className={tw('ml-2.5 text-black dark: text-white')}>Item name</p>
+  </div>
+);
+
+const Content = (
+  <div className={tw('text-black dark: text-white')}>
+    <p>Accordion content</p>
+    <p>Accordion content</p>
+    <p>Accordion content</p>
+  </div>
+);
+
+export const BaseAccordion = Template.bind({});
+
+BaseAccordion.args = {
+  titleNode: Title,
+  contentNode: Content,
+};
