13a5f3b1cccede2c80255c19f7cd954c205f7389
Didd <didd.tuni@akasha.world>
2023-02-13 11:54:16 +0300

    fix: implementation of progress arc

--- ui/design-system-core/src/components/Meter/index.tsx
+++ ui/design-system-core/src/components/Meter/index.tsx
@@ -2,6 +2,7 @@ import React, { PropsWithChildren } from 'react';
 import { tw, apply } from '@twind/core';
 import { getColorClasses } from '../../utils/getColorClasses';
 import { Color } from '../types/common.types';
+import { arcCommands, calculateEndAngle } from '../../utils/graphics';
 
 export type MeterProps = {
   size: number;
@@ -31,8 +32,21 @@ const Meter: React.FC<PropsWithChildren<MeterProps>> = ({
   const centerX = size / 2;
   const centerY = size / 2;
   const radius = size / 2 - thickness / 2;
-  const circumference = size * Math.PI;
-  const strokeDashoffset = circumference - (value / max) * circumference;
+
+  const scalePower = Math.max(5, Math.ceil(Math.log10(max)) + 3);
+  const scale = 10 ** scalePower;
+
+  const anglePer = Math.floor((360 / max) * scale) / scale;
+
+  const startAngle = 0;
+  let endAngle;
+  if (value >= max) {
+    endAngle = 360;
+  } else {
+    endAngle = calculateEndAngle(startAngle, anglePer, value);
+  }
+
+  const d = arcCommands(centerX, centerY, radius, startAngle, endAngle);
 
   return (
     <div className={tw(apply`inline-flex items-center justify-center overflow-hidden]`)}>
@@ -46,17 +60,13 @@ const Meter: React.FC<PropsWithChildren<MeterProps>> = ({
           cx={centerX}
           cy={centerY}
         />
-        <circle
-          className={tw(apply(`${progressStyles} -rotate-90 origin-center`))}
+        <path
+          className={tw(apply(`${progressStyles}`))}
+          d={d}
           strokeWidth={thickness}
-          strokeDasharray={circumference}
-          strokeDashoffset={strokeDashoffset < 0 ? 0 : strokeDashoffset}
           strokeLinecap="butt"
           stroke="currentColor"
           fill="transparent"
-          r={radius}
-          cx={centerX}
-          cy={centerY}
         />
       </svg>
       <div className="absolute"> {children} </div>
