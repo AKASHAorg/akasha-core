1edc41819b709f9f70e98b4e03d0d7fec547235a
Didd <didd.tuni@akasha.world>
2023-02-21 11:00:50 +0300

    feat: enhance circular and line placeholders

--- ui/design-system-core/src/components/TextLine/index.tsx
+++ ui/design-system-core/src/components/TextLine/index.tsx
@@ -1,36 +1,39 @@
 import React from 'react';
 import { tw, apply } from '@twind/core';
+import { getWidthClasses } from '../../utils/getWidthClasses';
+import { getHeightClasses } from '../../utils/getHeightClasses';
 
 export type TextLineProps = {
+  title?: string;
   animated?: boolean;
   width?: string;
   height?: string;
-  background?: string;
   round?: string;
   className?: string;
 };
 
-const baseStyle = apply`min-h-[18px]`;
+const baseStyle = apply`min-h-[18px] bg-placeholder-gradient-light dark:placeholder-gradient-dark`;
 
 const TextLine: React.FC<TextLineProps> = props => {
   const {
+    title,
     animated,
     width = 'w-[19.4375rem]',
     height = 'h-[1.1875rem]',
-    background = 'bg-gradient-to-r from-grey6 to-white dark:from-grey5 dark:to-grey7',
     round = 'rounded',
     className,
   } = props;
+  const widthStyle = getWidthClasses(width);
+  const heightStyle = getHeightClasses(height);
   const instanceStyles = apply`
     ${baseStyle}
     ${animated ? 'animate-pulse' : ''}
-    ${width}
-    ${height}
-    ${background}
+    ${widthStyle}
+    ${heightStyle}
     ${round}
     ${className}
   `;
 
-  return <div className={tw(instanceStyles)} />;
+  return <div title={title} className={tw(instanceStyles)} />;
 };
 export default TextLine;
