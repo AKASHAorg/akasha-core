4b39f33e0178c41e9752ba12dc54e98e28095469
Didd <didd.tuni@akasha.world>
2023-02-21 11:13:28 +0300

    feat(divider): add class name prop

--- ui/design-system-core/src/components/Divider/index.tsx
+++ ui/design-system-core/src/components/Divider/index.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import { apply, tw } from '@twind/core';
 import { PropsWithChildren } from 'react';
 
-const Divider: React.FC<PropsWithChildren> = ({ children }) => {
+type DividerProps = {
+  className?: string;
+};
+
+const Divider: React.FC<PropsWithChildren<DividerProps>> = ({ className, children }) => {
   return (
-    <div className={tw(apply('rounded-[1.25rem] h-px w-full bg-grey8 dark:bg-grey5'))}>
+    <div className={tw(apply(`rounded-[1.25rem] h-px w-full bg-grey8 dark:bg-grey5 ${className}`))}>
       {children}
     </div>
   );
