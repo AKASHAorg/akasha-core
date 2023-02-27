2c87faf816c7b862b063ddb8fcfd969c326e3115
Didd <didd.tuni@akasha.world>
2023-02-27 15:20:25 +0300

    feat: add bar type to meter component

--- ui/design-system-core/src/components/Meter/Meter.stories.tsx
+++ ui/design-system-core/src/components/Meter/Meter.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import Meter, { MeterProps } from './index';
+import Meter from './index';
+import { MeterProps } from './types';
 
 export default {
   title: 'Meter/Meter',
@@ -14,4 +15,21 @@ BaseMeter.args = {
   size: 400,
   value: 60,
   thickness: 48,
+};
+
+export const HorizontalBarMeter = Template.bind({});
+HorizontalBarMeter.args = {
+  size: 400,
+  value: 60,
+  thickness: 48,
+  type: 'bar',
+};
+
+export const VerticalBarMeter = Template.bind({});
+VerticalBarMeter.args = {
+  size: 400,
+  value: 90,
+  thickness: 48,
+  type: 'bar',
+  direction: 'vertical',
 };
