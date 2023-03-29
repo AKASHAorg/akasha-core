e46c15918661ce64b8009fb342ff4e0e9384de37
Didd <didd.tuni@akasha.world>
2023-03-29 11:43:53 +0300

    fix: temporarily remove ref

--- ui/design-system-core/src/components/TextField/Multiline/index.tsx
+++ ui/design-system-core/src/components/TextField/Multiline/index.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, useState } from 'react';
+import React, { useState } from 'react';
 import EditorMeter from '../../EditorMeter';
 import Stack from '../../Stack';
 import { getContainerClasses } from '../Input/getContainerClasses';
@@ -8,29 +8,26 @@ import { tw } from '@twind/core';
 
 const MAX_LENGTH = 280;
 
-export const Multiline: React.FC<MultlineProps> = forwardRef(
-  ({ disabled }, ref?: React.RefObject<HTMLTextAreaElement>) => {
-    const [letterCount, setLetterCount] = useState(0);
-    const containerStyle = getContainerClasses(disabled);
-    const textAreaStyle = getInputClasses(disabled);
+export const Multiline: React.FC<MultlineProps> = ({ disabled }) => {
+  const [letterCount, setLetterCount] = useState(0);
+  const containerStyle = getContainerClasses(disabled);
+  const textAreaStyle = getInputClasses(disabled);
 
-    return (
-      <Stack customStyle={`${containerStyle} py-2.5`} spacing="gap-x-2">
-        <textarea
-          className={tw(`resize-none w-full ${textAreaStyle}`)}
-          maxLength={MAX_LENGTH}
-          onChange={event => {
-            setLetterCount(event.target.value.length);
-          }}
-          ref={ref}
-        ></textarea>
-        <EditorMeter
-          max={MAX_LENGTH}
-          value={letterCount}
-          background="grey6"
-          customStyle="ml-auto mt-auto"
-        />
-      </Stack>
-    );
-  },
-);
+  return (
+    <Stack customStyle={`${containerStyle} py-2.5`} spacing="gap-x-2">
+      <textarea
+        className={tw(`resize-none w-full ${textAreaStyle}`)}
+        maxLength={MAX_LENGTH}
+        onChange={event => {
+          setLetterCount(event.target.value.length);
+        }}
+      ></textarea>
+      <EditorMeter
+        max={MAX_LENGTH}
+        value={letterCount}
+        background="grey6"
+        customStyle="ml-auto mt-auto"
+      />
+    </Stack>
+  );
+};
--- ui/design-system-core/src/components/TextField/index.tsx
+++ ui/design-system-core/src/components/TextField/index.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from 'react';
+import React from 'react';
 import Caption from './Caption';
 import Label from './Label';
 import Stack from '../Stack';
@@ -6,30 +6,24 @@ import { Input } from './Input';
 import { TextFieldProps } from './types';
 import { Multiline } from './Multiline';
 
-const TextField: React.FC<TextFieldProps> = forwardRef(
-  (props, ref?: React.RefObject<HTMLInputElement & HTMLTextAreaElement>) => {
-    const { required, label, status, caption, customStyle, disabled } = props;
+const TextField: React.FC<TextFieldProps> = props => {
+  const { required, label, status, caption, customStyle, disabled } = props;
 
-    return (
-      <Stack direction="column" spacing="gap-y-2" customStyle={customStyle}>
-        {label && (
-          <Label required={required} disabled={disabled}>
-            {label}
-          </Label>
-        )}
-        {props.type === 'multiline' ? (
-          <Multiline {...props} ref={ref} />
-        ) : (
-          <Input {...props} ref={ref} />
-        )}
-        {caption && (
-          <Caption status={status} disabled={disabled}>
-            {caption}
-          </Caption>
-        )}
-      </Stack>
-    );
-  },
-);
+  return (
+    <Stack direction="column" spacing="gap-y-2" customStyle={customStyle}>
+      {label && (
+        <Label required={required} disabled={disabled}>
+          {label}
+        </Label>
+      )}
+      {props.type === 'multiline' ? <Multiline {...props} /> : <Input {...props} />}
+      {caption && (
+        <Caption status={status} disabled={disabled}>
+          {caption}
+        </Caption>
+      )}
+    </Stack>
+  );
+};
 
 export default TextField;
