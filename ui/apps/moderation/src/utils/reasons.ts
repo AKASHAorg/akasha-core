7025972f5dd7cb6c8b34325fb295241b75f39609
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2023-04-10 06:39:42 +0100

    chore(moderation): dummy reasons

---
+++ ui/apps/moderation/src/utils/reasons.ts
@@ -0,0 +1,12 @@
+export const reasons = [
+  'Threats of violence and incitement',
+  'Hate speech',
+  'Bullying and harassment',
+  'Sexual or human exploitation',
+  'Impersonation',
+  'Illegal or certain regulated goods or services',
+  'Privacy and copyright infringement',
+  'Spam and malicious links',
+];
+
+export const preSelectedReasons = ['Threats of violence and incitement', 'Bullying and harassment'];
