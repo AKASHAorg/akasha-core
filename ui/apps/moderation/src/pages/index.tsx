e97ce5620bf77af5573aac073869789cf6729679
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2023-04-17 20:18:52 +0100

    feat(moderation): Moderator Detail component - add moderator detail page

--- ui/apps/moderation/src/pages/index.tsx
+++ ui/apps/moderation/src/pages/index.tsx
@@ -1,6 +1,7 @@
 export * from './dashboard';
 export * from './overview';
 export * from './moderators';
+export * from './moderator-detail';
 export * from './transparency-log';
 export * from './transparency-log-item';
 export * from './moderation-value';
