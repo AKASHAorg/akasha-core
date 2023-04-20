d141a273fb61db017e9a9f4ab7485735d9977f2d
Quynh Yen Vo T <quynhyen.vothi@akasha.world>
2023-04-20 10:03:21 +0200

    chore: create notification customization pages (#1908)
    * chore: create notification customization pages
    
    * chore: modify tests
    
    * chore: minor fixes
    
    * chore: change structure of data
    
    * chore: change route names

--- ui/apps/notifications/src/components/App.tsx
+++ ui/apps/notifications/src/components/App.tsx
@@ -2,7 +2,7 @@ import React, { PureComponent } from 'react';
 import DS from '@akashaorg/design-system';
 import { I18nextProvider } from 'react-i18next';
 import { RootComponentProps } from '@akashaorg/typings/ui';
-import NotificationsPage from './notifications-page';
+import AppRoutes from './app-routes';
 
 const { Box } = DS;
 
@@ -33,7 +33,7 @@ class App extends PureComponent<RootComponentProps> {
     return (
       <Box>
         <I18nextProvider i18n={this.props.plugins['@akashaorg/app-translation']?.translation?.i18n}>
-          <NotificationsPage {...this.props} />
+          <AppRoutes {...this.props} />
         </I18nextProvider>
       </Box>
     );
--- ui/apps/notifications/src/routes.ts
+++ ui/apps/notifications/src/routes.ts
@@ -1,1 +1,11 @@
 export const rootRoute = '/@akashaorg/app-notifications';
+
+export const CUSTOMIZE_NOTIFICATION_WELCOME_PAGE = 'CustomizeNotificationWelcomePage';
+export const CUSTOMIZE_NOTIFICATION_OPTIONS_PAGE = 'CustomizeNotificationOptionsPage';
+export const CUSTOMIZE_NOTIFICATION_CONFIRMATION_PAGE = 'CustomizeNotificationConfirmationPage';
+
+export default {
+  [CUSTOMIZE_NOTIFICATION_WELCOME_PAGE]: `/customize-notifications/welcome-page`,
+  [CUSTOMIZE_NOTIFICATION_OPTIONS_PAGE]: `/customize-notifications/options-page`,
+  [CUSTOMIZE_NOTIFICATION_CONFIRMATION_PAGE]: `/customize-notifications/confirmation-page`,
+};
