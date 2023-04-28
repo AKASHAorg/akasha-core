b69b459bb5bca22839c6ba496ad18a4280500e23
Vali Cotea <quinine35@gmail.com>
2023-04-28 11:46:17 +0300

    chore()

--- ui/design-system-core/src/components/ModalContainer/index.tsx
+++ ui/design-system-core/src/components/ModalContainer/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { tw, keyframes, css, apply } from '@twind/core';
+import { tw, css, apply } from '@twind/core';
 
 export interface IModalContainerProps {
   onModalClose?: () => void;
@@ -10,14 +10,14 @@ export interface IModalContainerProps {
 
 const positionClass = apply(`top-0 left-0 bottom-0 right-0`);
 
-const fadeIn = keyframes`
-  0% {
-    opacity: 0
-  }
-  100% {
-    opacity: 1
-  }
-`;
+// const fadeIn = keyframes`
+//   0% {
+//     opacity: 0
+//   }
+//   100% {
+//     opacity: 1
+//   }
+// `;
 
 const fadeInClass = css`
   animation: 0.25s ${fadeIn} ease-out;
