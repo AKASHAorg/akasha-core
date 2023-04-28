d19e06196565bcfaa36588f5b12b46c02a9fd714
Vali Cotea <quinine35@gmail.com>
2023-04-28 12:56:25 +0300

    chore(): remove modalcontainer

--- ui/design-system-components/src/components/ImageOverlay/index.tsx
+++ ui/design-system-components/src/components/ImageOverlay/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { tw, apply, tx } from '@twind/core';
-import ModalContainer from '@akashaorg/design-system-core/lib/components/ModalContainer';
+// import ModalContainer from '@akashaorg/design-system-core/lib/components/ModalContainer';
 import Icon from '@akashaorg/design-system-core/lib/components/Icon';
 import { Portal } from '../Editor/helpers';
 import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
@@ -47,40 +47,40 @@ const ImageOverlay: React.FC<IImageOverlay> = props => {
 
   return (
     <Portal>
-      <ModalContainer style={{ zIndex: 103 }}>
-        <div
-          className={tx(`${flexCenteredClass} w-screen h-screen bg-grey3`)}
-          onClick={(ev: React.SyntheticEvent) => {
-            /**
-             * prevents click bubbling to parent so the user doesn't get redirected
-             */
-            ev.stopPropagation();
-          }}
-        >
-          <div className={tw(`flex flex-row gap-3 p-3 absolute top-1 right-1 z-1`)}>
-            <div className={tx(`${closeDivClass}`)} onClick={handleZoomIn}>
-              <Icon type="MagnifyingGlassPlusIcon" />
-            </div>
-            <div className={tx(`${closeDivClass}`)} onClick={handleZoomOut}>
-              <Icon type="MagnifyingGlassMinusIcon" />
-            </div>
-            <div className={tx(`${closeDivClass}`)} onClick={closeModal.current}>
-              <Icon type="XMarkIcon" />
-            </div>
+      {/* <ModalContainer style={{ zIndex: 103 }}> */}
+      <div
+        className={tx(`${flexCenteredClass} w-screen h-screen bg-grey3`)}
+        onClick={(ev: React.SyntheticEvent) => {
+          /**
+           * prevents click bubbling to parent so the user doesn't get redirected
+           */
+          ev.stopPropagation();
+        }}
+      >
+        <div className={tw(`flex flex-row gap-3 p-3 absolute top-1 right-1 z-1`)}>
+          <div className={tx(`${closeDivClass}`)} onClick={handleZoomIn}>
+            <Icon type="MagnifyingGlassPlusIcon" />
           </div>
-
-          {src && (
-            <TransformWrapper ref={transformRef} centerOnInit={true} centerZoomedOut={true}>
-              <TransformComponent wrapperStyle={{ width: '100%', height: '100%' }}>
-                <picture className={tw('flex')}>
-                  <source srcSet={src.url} />
-                  <img src={src.fallbackUrl} alt="" />
-                </picture>
-              </TransformComponent>
-            </TransformWrapper>
-          )}
+          <div className={tx(`${closeDivClass}`)} onClick={handleZoomOut}>
+            <Icon type="MagnifyingGlassMinusIcon" />
+          </div>
+          <div className={tx(`${closeDivClass}`)} onClick={closeModal.current}>
+            <Icon type="XMarkIcon" />
+          </div>
         </div>
-      </ModalContainer>
+
+        {src && (
+          <TransformWrapper ref={transformRef} centerOnInit={true} centerZoomedOut={true}>
+            <TransformComponent wrapperStyle={{ width: '100%', height: '100%' }}>
+              <picture className={tw('flex')}>
+                <source srcSet={src.url} />
+                <img src={src.fallbackUrl} alt="" />
+              </picture>
+            </TransformComponent>
+          </TransformWrapper>
+        )}
+      </div>
+      {/* </ModalContainer> */}
     </Portal>
   );
 };
