845b833a2424d6c639666bb27ad544c22b38b156
Didd <didd.tuni@akasha.world>
2023-05-22 09:30:58 +0300

    feat: create new my apps, apps and my widgets page

---
+++ ui/design-system-core/src/components/TabList/index.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import Button from '../Button';
+import Text, { TextProps } from '../Text';
+import { getColorClasses } from '../../utils/getColorClasses';
+import { apply, tw } from '@twind/core';
+
+export type TabListProps = {
+  labels: string[];
+  selected: number;
+  tabListDivider?: boolean;
+  labelTextVariant?: TextProps['variant'];
+  onChange: (selectedIndex: number, previousIndex: number) => void;
+};
+
+const TabList: React.FC<TabListProps> = ({
+  labels,
+  selected,
+  tabListDivider,
+  labelTextVariant,
+  onChange,
+}) => {
+  const baseStyle = 'group p-2';
+  const activeStyle = `border-b ${getColorClasses(
+    {
+      light: 'secondaryLight',
+      dark: 'secondaryDark',
+    },
+    'border',
+  )}`;
+  const hoverStyle = `hover:border-b ${getColorClasses(
+    {
+      light: 'secondaryLight',
+      dark: 'secondaryDark',
+    },
+    'hover:border',
+  )}`;
+  const tabListDividerStyle = tabListDivider
+    ? `border-b ${getColorClasses({ light: 'grey8', dark: 'grey5' }, 'border')}`
+    : '';
+
+  return (
+    <div className={tw(apply`grid grid-cols-${labels.length} ${tabListDividerStyle}`)}>
+      {labels.map((label, index) => (
+        <Button
+          key={index}
+          onClick={() => onChange(index, selected)}
+          customStyle={`${baseStyle} ${hoverStyle} ${selected === index ? activeStyle : ''}`}
+          plain
+        >
+          <Text
+            variant={labelTextVariant ? labelTextVariant : getTabTextVariant(selected, index)}
+            color={
+              selected === index
+                ? {
+                    light: 'secondaryLight',
+                    dark: 'secondaryDark',
+                  }
+                : 'grey7'
+            }
+            weight={selected === index ? 'bold' : 'normal'}
+            align="center"
+            customStyle={getColorClasses(
+              {
+                light: 'secondaryLight',
+                dark: 'secondaryDark',
+              },
+              'group-hover:text',
+            )}
+          >
+            {label}
+          </Text>
+        </Button>
+      ))}
+    </div>
+  );
+};
+
+const getTabTextVariant = (selected: number, index: number) =>
+  selected === index ? 'button-sm' : 'footnotes2';
+
+export default TabList;
