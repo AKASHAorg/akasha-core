1201191ae9a3e9f8c0592351c9fc1946f048c066
SeverS <sever.abi@gmail.com>
2023-06-20 13:41:16 +0300

    fix(): theme

--- ui/apps/settings-app/src/components/app-routes.tsx
+++ ui/apps/settings-app/src/components/app-routes.tsx
@@ -26,6 +26,8 @@ const AppRoutes: React.FC<RootComponentProps> = props => {
 
   const { t } = useTranslation('app-settings-ewa');
 
+  console.log(theme, '<<< the theme');
+
   const routing = props.plugins['@akashaorg/app-routing']?.routing;
 
   const handlePrivacyPolicyClick = () => {
