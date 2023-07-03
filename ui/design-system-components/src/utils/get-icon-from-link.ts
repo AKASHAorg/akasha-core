b68130f970b70815a3ec51bc1fa36639c197d56a
Didd Tuni <didd.tuni@akasha.world>
2023-07-03 17:19:37 +0300

    fix: profile app bugs

--- ui/design-system-components/src/utils/get-icon-from-link.ts
+++ ui/design-system-components/src/utils/get-icon-from-link.ts
@@ -1,11 +1,9 @@
 import { IconType } from '@akashaorg/typings/ui';
 
 export const getIconFromLink = (href: string): IconType => {
-  /*@TODO revisit the following logic
-      if (href.includes('github.com')) return 'github';
-      if (href.includes('twitter.com')) return 'twitter';
-      if (href.includes('discordapp.com')) return 'discord';
-      if (href.includes('telegram.me') || href.includes('t.me')) return 'telegram';
-  */
+  if (href.includes('github.com')) return 'github';
+  if (href.includes('twitter.com')) return 'twitter';
+  if (href.includes('discordapp.com')) return 'discord';
+  if (href.includes('telegram.me') || href.includes('t.me')) return 'telegram';
   return 'LinkIcon';
 };
