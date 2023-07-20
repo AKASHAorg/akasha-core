ad4e818155d56629038f82f806f6dcbd3c10a824
Yen <quynhyen.vothi@akasha.world>
2023-07-20 10:53:45 +0200

    chore: update jest setup for failing tests

--- ui/design-system-components/jest.setup.js
+++ ui/design-system-components/jest.setup.js
@@ -1,5 +1,5 @@
 require('@testing-library/jest-dom/extend-expect');
 
 import { install } from '@twind/core';
-import twindConfig from '.storybook/twind.config.js';
+import twindConfig from './.storybook/twind.config.js';
 install(twindConfig);
