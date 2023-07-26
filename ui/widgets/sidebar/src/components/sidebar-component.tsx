b24411f191db7045f51f451534f691a259da5f21
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2023-07-26 14:22:40 +0100

    refactor(sidebar): implement cta card

--- ui/widgets/sidebar/src/components/sidebar-component.tsx
+++ ui/widgets/sidebar/src/components/sidebar-component.tsx
@@ -1,33 +1,70 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useQueryClient } from '@tanstack/react-query';
+
 import { RootComponentProps, EventTypes, MenuItemAreaType, IMenuItem } from '@akashaorg/typings/ui';
+import { AUTH_EVENTS, WEB3_EVENTS } from '@akashaorg/typings/sdk/events';
 import { useGetMyProfileQuery } from '@akashaorg/ui-awf-hooks/lib/generated/hooks-new';
-import Avatar from '@akashaorg/design-system-core/lib/components/Avatar';
-import Box from '@akashaorg/design-system-core/lib/components/Box';
-import DidField from '@akashaorg/design-system-core/lib/components/DidField';
-import Stack from '@akashaorg/design-system-core/lib/components/Stack';
-import Text from '@akashaorg/design-system-core/lib/components/Text';
-import TextLine from '@akashaorg/design-system-core/lib/components/TextLine';
-import Button from '@akashaorg/design-system-core/lib/components/Button';
-import ListSidebarApps from './list-sidebar-apps';
-import BasicCardBox from '@akashaorg/design-system-core/lib/components/BasicCardBox';
-import { startMobileSidebarHidingBreakpoint } from '@akashaorg/design-system-core/lib/utils/breakpoints';
 import {
   getProfileImageVersionsWithMediaUrl,
   useGetLogin,
   useLogout,
   LOGIN_STATE_KEY,
+  useDismissedCard,
 } from '@akashaorg/ui-awf-hooks';
-import { useQueryClient } from '@tanstack/react-query';
 import getSDK from '@akashaorg/awf-sdk';
-import { AUTH_EVENTS, WEB3_EVENTS } from '@akashaorg/typings/sdk/events';
+
+import Anchor from '@akashaorg/design-system-core/lib/components/Anchor';
+import Avatar from '@akashaorg/design-system-core/lib/components/Avatar';
+import BasicCardBox from '@akashaorg/design-system-core/lib/components/BasicCardBox';
+import Box from '@akashaorg/design-system-core/lib/components/Box';
+import Button from '@akashaorg/design-system-core/lib/components/Button';
+import DidField from '@akashaorg/design-system-core/lib/components/DidField';
+import Stack from '@akashaorg/design-system-core/lib/components/Stack';
+import Text from '@akashaorg/design-system-core/lib/components/Text';
+import TextLine from '@akashaorg/design-system-core/lib/components/TextLine';
+import { startMobileSidebarHidingBreakpoint } from '@akashaorg/design-system-core/lib/utils/breakpoints';
+
+import ListSidebarApps from './list-sidebar-apps';
+import SidebarCTACard from './cta-card';
 
 const SidebarComponent: React.FC<RootComponentProps> = props => {
-  const [isMobile, setIsMobile] = React.useState(
+  const {
+    uiEvents,
+    plugins,
+    worldConfig: { defaultApps, socialLinks },
+  } = props;
+
+  const [isMobile, setIsMobile] = useState(
     window.matchMedia(startMobileSidebarHidingBreakpoint).matches,
   );
+  const [routeData, setRouteData] = useState(null);
+  const [activeOption, setActiveOption] = useState<IMenuItem | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [profileName, setProfileName] = useState('Guest');
 
-  React.useEffect(() => {
+  const subtitleUi = useRef(null);
+
+  const { t } = useTranslation('ui-widget-sidebar');
+
+  const loginQuery = useGetLogin();
+  const logoutQuery = useLogout();
+  const queryClient = useQueryClient();
+  const [dismissed, setDismissed] = useDismissedCard();
+  const myProfileQuery = useGetMyProfileQuery(null, {
+    enabled: !!loginQuery.data?.id,
+    select: data => data.viewer,
+    onSuccess: data => {
+      if (data) setIsLoading(false);
+    },
+  });
+
+  const sdk = getSDK();
+  const routing = plugins['@akashaorg/app-routing']?.routing;
+
+  const dismissedCardId = 'dismiss-sidebar-cta-card';
+
+  useEffect(() => {
     const mql = window.matchMedia(startMobileSidebarHidingBreakpoint);
     const resize = () => {
       setIsMobile(mql.matches);
@@ -38,24 +75,7 @@ const SidebarComponent: React.FC<RootComponentProps> = props => {
     };
   }, []);
 
-  const {
-    uiEvents,
-    plugins,
-    worldConfig: { defaultApps, socialLinks },
-  } = props;
-  const [routeData, setRouteData] = useState(null);
-  const [activeOption, setActiveOption] = useState<IMenuItem | null>(null);
-  const [isLoading, setIsLoading] = React.useState(false);
-  const [profileName, setProfileName] = React.useState('Guest');
-  const { t } = useTranslation('ui-widget-sidebar');
-
-  const loginQuery = useGetLogin();
-  const logoutQuery = useLogout();
-  const queryClient = useQueryClient();
-
-  const sdk = getSDK();
-
-  React.useEffect(() => {
+  useEffect(() => {
     const subSDK = sdk.api.globalChannel.subscribe({
       next: (eventData: { data: { name: string }; event: AUTH_EVENTS | WEB3_EVENTS }) => {
         if (
@@ -80,14 +100,6 @@ const SidebarComponent: React.FC<RootComponentProps> = props => {
     };
   }, [sdk.api.globalChannel]);
 
-  const myProfileQuery = useGetMyProfileQuery(null, {
-    enabled: !!loginQuery.data?.id,
-    select: data => data.viewer,
-    onSuccess: data => {
-      if (data) setIsLoading(false);
-    },
-  });
-
   useEffect(() => {
     const invalidateQuery = async () => {
       await queryClient.invalidateQueries({
@@ -102,8 +114,6 @@ const SidebarComponent: React.FC<RootComponentProps> = props => {
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [loginQuery.data?.id]);
-
-  const routing = plugins['@akashaorg/app-routing']?.routing;
 
   useEffect(() => {
     let sub;
@@ -121,103 +131,11 @@ const SidebarComponent: React.FC<RootComponentProps> = props => {
     };
   }, [routing]);
 
-  // sort according to worldConfig index
-  const worldApps = useMemo(() => {
-    return routeData?.[MenuItemAreaType.AppArea]?.sort(
-      (a: { name: string }, b: { name: string }) => {
-        if (defaultApps.indexOf(a.name) < defaultApps.indexOf(b.name)) {
-          return -1;
-        } else if (defaultApps.indexOf(a.name) > defaultApps.indexOf(b.name)) {
-          return 1;
-        }
-        return 0;
-      },
-    );
-  }, [defaultApps, routeData]);
-
-  const userInstalledApps = useMemo(() => {
-    return routeData?.[MenuItemAreaType.UserAppArea];
-  }, [routeData]);
-
-  // const allApps = React.useMemo(() => {
+  // const allApps = useMemo(() => {
   //   return [...(worldApps || []), ...(userInstalledApps || [])];
   // }, [worldApps, userInstalledApps]);
-  const handleNavigation = (appName: string, route: string) => {
-    routing?.navigateTo({
-      appName,
-      getNavigationUrl: () => route,
-    });
-  };
-  const handleAvatarClick = function (did: string) {
-    if (!did) {
-      return;
-    }
-    routing?.navigateTo({
-      appName: '@akashaorg/app-profile',
-      getNavigationUrl: navRoutes => `${navRoutes.rootRoute}/${did}`,
-    });
 
-    if (isMobile) {
-      handleSidebarClose();
-    }
-  };
-
-  const handleClickExplore = () => {
-    routing?.navigateTo({
-      appName: '@akashaorg/app-akasha-verse',
-      getNavigationUrl: routes => routes.explore,
-    });
-
-    if (isMobile) {
-      handleSidebarClose();
-    }
-  };
-  const handleSidebarClose = () => {
-    // emit HideSidebar event to trigger corresponding action in associated widgets
-    uiEvents.next({
-      event: EventTypes.HideSidebar,
-    });
-  };
-
-  const handleLoginClick = () => {
-    handleNavigation('@akashaorg/app-auth-ewa', '/');
-
-    if (isMobile) {
-      handleSidebarClose();
-    }
-  };
-
-  async function handleLogout() {
-    await logoutQuery.mutateAsync();
-    await queryClient.invalidateQueries({
-      queryKey: [LOGIN_STATE_KEY],
-    });
-  }
-  const handleLogoutClick = () => {
-    handleLogout();
-  };
-
-  const handleAppIconClick = (menuItem: IMenuItem) => {
-    if (menuItem.subRoutes && menuItem.subRoutes.length === 0) {
-      setActiveOption(null);
-      handleNavigation(menuItem.name, menuItem.route);
-      if (isMobile) {
-        handleSidebarClose();
-      }
-    }
-  };
-
-  const handleOptionClick = (menuItem: IMenuItem, subrouteMenuItem: IMenuItem) => {
-    setActiveOption(subrouteMenuItem);
-    handleNavigation(menuItem.name, subrouteMenuItem.route);
-    if (isMobile) {
-      handleSidebarClose();
-    }
-  };
-
-  const subtitleUi = React.useRef(null);
-
-  React.useEffect(() => {
+  useEffect(() => {
     if (loginQuery.data?.id) {
       subtitleUi.current = (
         <DidField
@@ -236,7 +154,7 @@ const SidebarComponent: React.FC<RootComponentProps> = props => {
     }
   }, [isLoading, loginQuery.data?.id, t]);
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (myProfileQuery.data?.profile?.did?.id) {
       setProfileName(myProfileQuery.data?.profile?.name);
     } else if (loginQuery.data?.id) {
@@ -250,6 +168,24 @@ const SidebarComponent: React.FC<RootComponentProps> = props => {
     myProfileQuery.data?.profile?.did?.id,
     myProfileQuery.data?.profile?.name,
   ]);
+
+  // sort according to worldConfig index
+  const worldApps = useMemo(() => {
+    return routeData?.[MenuItemAreaType.AppArea]?.sort(
+      (a: { name: string }, b: { name: string }) => {
+        if (defaultApps.indexOf(a.name) < defaultApps.indexOf(b.name)) {
+          return -1;
+        } else if (defaultApps.indexOf(a.name) > defaultApps.indexOf(b.name)) {
+          return 1;
+        }
+        return 0;
+      },
+    );
+  }, [defaultApps, routeData]);
+
+  const userInstalledApps = useMemo(() => {
+    return routeData?.[MenuItemAreaType.UserAppArea];
+  }, [routeData]);
 
   const ConnectButton = useMemo(
     () =>
@@ -274,6 +210,87 @@ const SidebarComponent: React.FC<RootComponentProps> = props => {
     [isLoading, loginQuery.isStale, myProfileQuery.data?.profile?.did?.id],
   );
 
+  const handleNavigation = (appName: string, route: string) => {
+    routing?.navigateTo({
+      appName,
+      getNavigationUrl: () => route,
+    });
+  };
+
+  const handleAvatarClick = function (did: string) {
+    if (!did) {
+      return;
+    }
+    routing?.navigateTo({
+      appName: '@akashaorg/app-profile',
+      getNavigationUrl: navRoutes => `${navRoutes.rootRoute}/${did}`,
+    });
+
+    if (isMobile) {
+      handleSidebarClose();
+    }
+  };
+
+  const handleClickExplore = () => {
+    routing?.navigateTo({
+      appName: '@akashaorg/app-akasha-verse',
+      getNavigationUrl: routes => routes.explore,
+    });
+
+    if (isMobile) {
+      handleSidebarClose();
+    }
+  };
+
+  const handleSidebarClose = () => {
+    // emit HideSidebar event to trigger corresponding action in associated widgets
+    uiEvents.next({
+      event: EventTypes.HideSidebar,
+    });
+  };
+
+  function handleLoginClick() {
+    handleNavigation('@akashaorg/app-auth-ewa', '/');
+
+    if (isMobile) {
+      handleSidebarClose();
+    }
+  }
+
+  async function handleLogout() {
+    await logoutQuery.mutateAsync();
+    await queryClient.invalidateQueries({
+      queryKey: [LOGIN_STATE_KEY],
+    });
+  }
+
+  const handleLogoutClick = () => {
+    handleLogout();
+  };
+
+  const handleAppIconClick = (menuItem: IMenuItem) => {
+    if (menuItem.subRoutes && menuItem.subRoutes.length === 0) {
+      setActiveOption(null);
+      handleNavigation(menuItem.name, menuItem.route);
+      if (isMobile) {
+        handleSidebarClose();
+      }
+    }
+  };
+
+  const handleOptionClick = (menuItem: IMenuItem, subrouteMenuItem: IMenuItem) => {
+    setActiveOption(subrouteMenuItem);
+    handleNavigation(menuItem.name, subrouteMenuItem.route);
+    if (isMobile) {
+      handleSidebarClose();
+    }
+  };
+
+  const handleDismissCTACard = useCallback(() => {
+    setDismissed(dismissedCardId);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dismissed]);
+
   return (
     <BasicCardBox
       customStyle="w-[19.5rem] max-w-[19.5rem] max-h-screen min-[1440px]:max-h-[calc(100vh-20px)]"
@@ -289,9 +306,11 @@ const SidebarComponent: React.FC<RootComponentProps> = props => {
             onClick={() => handleAvatarClick(loginQuery.data?.id)}
           />
         </Box>
+
         {isLoading ? (
           <Stack direction="column" spacing="gap-y-1" customStyle="w-fit flex-grow ">
             <TextLine title="tagName" animated={true} width="w-[40px]" />
+
             <TextLine title="tagName" animated={true} width="w-[100px]" />
           </Stack>
         ) : (
@@ -303,6 +322,7 @@ const SidebarComponent: React.FC<RootComponentProps> = props => {
 
         <Box customStyle="w-fit h-fit ml-6 self-start">{ConnectButton}</Box>
       </Box>
+
       {/*
           this container will grow up to a max height of 68vh, 32vh currently accounts for the height of other sections and paddings. Adjust accordingly, if necessary.
         */}
@@ -316,6 +336,7 @@ const SidebarComponent: React.FC<RootComponentProps> = props => {
             onClickMenuItem={handleAppIconClick}
           />
         )}
+
         {/* container for user-installed apps */}
         {userInstalledApps?.length > 0 && (
           <ListSidebarApps
@@ -327,25 +348,30 @@ const SidebarComponent: React.FC<RootComponentProps> = props => {
           />
         )}
       </Box>
-      <Box customStyle="flex flex-col px-8 py-4 bg-grey9 dark:bg-grey3">
-        <Text variant="footnotes2" customStyle="text-grey5">
-          {t('Add magic to your world by installing cool apps developed by the community')}
-        </Text>
-        <Box customStyle="w-fit h-fit mt-6 self-end">
-          <Button onClick={handleClickExplore} label={t('Check them out!')} variant="secondary" />
-        </Box>
-      </Box>
+
+      {!dismissed.includes(dismissedCardId) && (
+        <SidebarCTACard
+          ctaText={`🪄${t(
+            'Add magic to your world by installing cool apps developed by the community',
+          )}`}
+          ctaButtonLabel={t('Check them out')}
+          onClickCTAButton={handleClickExplore}
+          onDismissCard={handleDismissCTACard}
+        />
+      )}
+
       {socialLinks.length > 0 && (
-        <Box customStyle="flex flex-col px-8 py-4">
+        <Box customStyle="flex flex-col px-8 py-4 border-t-1 border(grey9 dark:grey3)">
           <Text variant="footnotes2" customStyle="text-grey5">
             {t('Get in touch')}
           </Text>
+
           <Box customStyle="flex w-fit h-fit mt-6">
             {socialLinks.map((socialLink, idx) => (
               <Box key={socialLink.icon + idx} customStyle="mr-4">
-                <a href={socialLink.link} target="_blank" rel="noreferrer noopener">
+                <Anchor href={socialLink.link} target="_blank" rel="noreferrer noopener">
                   <Button icon={socialLink.icon} variant="primary" greyBg={true} iconOnly={true} />
-                </a>
+                </Anchor>
               </Box>
             ))}
           </Box>
