31a52366b45533a4f35e96d573be689d9c1fb14c
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2023-08-09 16:55:22 +0100

    refactor(typings): update typings - remove redundant code

--- typings/src/ui/icon.ts
+++ typings/src/ui/icon.ts
@@ -1,18 +1,5 @@
 import * as HeroIcons from '@heroicons/react/24/outline';
 
-/* @TODO: remove the following once the old design system is fully replaced */
-export type AppIcons =
-  | 'appSocial'
-  | 'appCenter'
-  | 'appModeration'
-  | 'bookmark'
-  | 'chatBubble'
-  | 'explore'
-  | 'message'
-  | 'notifications'
-  | 'search'
-  | 'settingsAlt';
-
 export type IconType =
   | keyof typeof HeroIcons
   | 'akasha'
@@ -20,6 +7,7 @@ export type IconType =
   | 'alignLeft'
   | 'alignRight'
   | 'alignJustify'
+  | 'antenna'
   | 'boldAlt'
   | 'didKey'
   | 'discord'
@@ -41,28 +29,14 @@ export type IconType =
   | 'metamask'
   | 'noEth'
   | 'shield'
-  | 'underline'
-  | 'strikethrough'
   | 'solana'
+  | 'strikethrough'
   | 'telegram'
   | 'textcaseLower'
   | 'textcaseSentence'
   | 'textcaseUpper'
   | 'twitter'
+  | 'underline'
+  | 'vibe'
   | 'walletconnect'
-  | 'widget'
-  | AppIcons;
-
-/* @TODO: remove the following once the old design system is fully replaced */
-export const APP_ICON_TO_HERO_ICON_MAP: Record<AppIcons, IconType> = {
-  appSocial: 'akasha',
-  appCenter: 'SquaresPlusIcon',
-  appModeration: 'UsersIcon',
-  bookmark: 'BookmarkIcon',
-  chatBubble: 'ChatBubbleOvalLeftEllipsisIcon',
-  explore: 'SparklesIcon',
-  message: 'EnvelopeIcon',
-  notifications: 'BellIcon',
-  search: 'MagnifyingGlassIcon',
-  settingsAlt: 'Cog8ToothIcon',
-};
+  | 'widget';
