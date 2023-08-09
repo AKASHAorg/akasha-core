ba8b3f9d6f5197e131894b16d0706bbe695d0519
Yen <quynhyen.vothi@akasha.world>
2023-08-09 09:22:12 +0200

    chore(): add border, and optional button to empty card

--- ui/design-system-components/src/components/DefaultEmptyCard/index.tsx
+++ ui/design-system-components/src/components/DefaultEmptyCard/index.tsx
@@ -1,23 +1,36 @@
 import React from 'react';
-import { tw } from '@twind/core';
 import BasicCardBox from '@akashaorg/design-system-core/lib/components/BasicCardBox';
+import Button from '@akashaorg/design-system-core/lib/components/Button';
+import Card from '@akashaorg/design-system-core/lib/components/Card';
+import Stack from '@akashaorg/design-system-core/lib/components/Stack';
 import Text from '@akashaorg/design-system-core/lib/components/Text';
 
 export type DefaultEmptyCardProps = {
   infoText: string;
+  buttonLabel?: string;
+  buttonClickHandler?: () => void;
 };
 
-const DefaultEmptyCard: React.FC<DefaultEmptyCardProps> = ({ infoText }) => {
+const DefaultEmptyCard: React.FC<DefaultEmptyCardProps> = ({
+  infoText,
+  buttonLabel,
+  buttonClickHandler,
+}) => {
   return (
-    <div className={tw('flex(& col) justify-center align-center mt-6 mb-32')}>
+    <Card elevation="1" radius={20} padding={16}>
       <BasicCardBox
-        customStyle="bg(grey8 dark:grey5) w-[180px] h-[180px] m-auto my-4"
+        customStyle="bg(grey8 dark:grey5) w-[140px] h-[85px] shrink-0 m-auto my-4"
         round="rounded-xl"
       />
       <Text variant="h6" align="center">
         {infoText}
       </Text>
-    </div>
+      <Stack justify="end" fullWidth spacing="gap-y-4" customStyle="pt-2">
+        {buttonLabel && (
+          <Button variant="primary" label={buttonLabel} onClick={buttonClickHandler} />
+        )}
+      </Stack>
+    </Card>
   );
 };
 
