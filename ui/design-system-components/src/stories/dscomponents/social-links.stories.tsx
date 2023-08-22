d1e3e918a25ae8b92fadc152fdc82bbbf941f7f9
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2023-08-22 13:42:35 +0100

    Feat: Merge DS stories (#2023)
    * chore(ds): move ds components stories
    
    * chore(ds): move ds core stories
    
    * feat(ds): update scripts and remove redundant files

---
+++ ui/design-system-components/src/stories/dscomponents/social-links.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { SocialLinksProps, SocialLinks } from '../../components/EditProfile/SocialLinks';
+
+const meta: Meta<SocialLinksProps> = {
+  title: 'DSComponents/Profile/Edit/SocialLinks',
+  component: SocialLinks,
+};
+
+export default meta;
+type Story = StoryObj<SocialLinksProps>;
+
+export const BaseSocialLinks: Story = {
+  render: () => (
+    <SocialLinks
+      title="External URLs"
+      addNewButtonLabel="Add new"
+      description="You can add your personal websites or social links to be shared on your profile"
+      socialLinks={[]}
+      cancelButton={{ label: 'Cancel', handleClick: () => ({}) }}
+      saveButton={{
+        label: 'Save',
+        handleClick: formValues => {
+          console.log(formValues);
+        },
+      }}
+      onDelete={() => ({})}
+    />
+  ),
+};
---
+++ ui/design-system-components/src/stories/dscomponents/trending-widget-loading-card.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { tw } from '@twind/core';
+import type { Meta, StoryObj } from '@storybook/react';
+
+import TrendingWidgetLoadingCard from '../../components/TrendingWidgetLoadingCard';
+
+const meta: Meta = {
+  title: 'DSComponents/Cards/TrendingWidgetLoadingCard',
+  component: TrendingWidgetLoadingCard,
+};
+
+export default meta;
+type Story = StoryObj;
+
+export const BaseCard: Story = {
+  render: () => (
+    <div className={tw('w-[25%]')}>
+      <TrendingWidgetLoadingCard />
+    </div>
+  ),
+};
