b2a3d86df9f74a2222b9a4d344653f2dfdf79e91
SeverS <sever.abi@gmail.com>
2023-08-31 14:47:32 +0300

    chore(): fix imports

--- ui/apps/akasha-verse/src/extensions/install-app.tsx
+++ ui/apps/akasha-verse/src/extensions/install-app.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import singleSpaReact from 'single-spa-react';
-
 import getSDK from '@akashaorg/awf-sdk';
 import InstallApp from '@akashaorg/design-system-components/lib/components/InstallApp';
 import ErrorLoader from '@akashaorg/design-system-core/lib/components/ErrorLoader';
-import { EventDataTypes, EventTypes, RootExtensionProps, UIEventData } from '@akashaorg/typings/ui';
+import { EventDataTypes, EventTypes, RootExtensionProps } from '@akashaorg/typings/ui';
 import { APP_EVENTS } from '@akashaorg/typings/sdk';
 import { I18nextProvider, useTranslation } from 'react-i18next';
 import { withProviders } from '@akashaorg/ui-awf-hooks';
-import { filterEvent } from '@akashaorg/ui-app-loader/lib/events';
 
 const IntegrationInstallModal: React.FC<RootExtensionProps> = props => {
   const { extensionData, uiEvents, singleSpa } = props;
@@ -40,10 +38,13 @@ const IntegrationInstallModal: React.FC<RootExtensionProps> = props => {
         }
       },
     });
-    const sub = uiEvents.pipe(filterEvent(EventTypes.RegisterIntegration)).subscribe({
+    // EventTypes.RegisterIntegration
+    const sub = uiEvents.subscribe({
       next: (eventData: { event: EventTypes; data?: EventDataTypes }) => {
-        if (eventData.data.name === integrationName) {
-          setModalState(3);
+        if (eventData.event && eventData.event === EventTypes.RegisterIntegration) {
+          if (eventData.data.name === integrationName) {
+            setModalState(3);
+          }
         }
       },
     });
