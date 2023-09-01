c4c6ef2085b2dea6acf42e3b2bc16211c6991c08
Didd Tuni <didd.tuni@akasha.world>
2023-09-01 11:30:55 +0300

    fix: change missed placeholder image

--- ui/design-system-components/src/components/ProfileEngagements/Entry/EmptyEntry/index.tsx
+++ ui/design-system-components/src/components/ProfileEngagements/Entry/EmptyEntry/index.tsx
@@ -17,7 +17,13 @@ const EmptyEntry: React.FC<EmptyEntryProps> = ({
   viewerIsOwner = true,
 }) => {
   if (!viewerIsOwner && type === 'following')
-    return <InfoCard titleLabel={`@${userName} is not following anyone yet!`} />;
+    return (
+      <InfoCard
+        titleLabel={`@${userName} is not following anyone yet!`}
+        publicImgPath="/images"
+        assetName="longbeam-notfound.webp"
+      />
+    );
 
   let props = {
     titleLabel: titleLabel ? titleLabel : 'Looks like there are no followers!',
