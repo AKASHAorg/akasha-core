7d72558180c7bc340d2d52b605e4b859ae1bb8b0
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2023-09-01 11:40:18 +0100

    feat(messaging): use hook for root components

--- ui/apps/messaging/src/components/App.tsx
+++ ui/apps/messaging/src/components/App.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
-import { RootComponentProps } from '@akashaorg/typings/ui';
 import { I18nextProvider } from 'react-i18next';
+
+import { useRootComponentProps } from '@akashaorg/ui-awf-hooks';
+
 import AppRoutes from './app-routes';
 
-const MessagingApp: React.FC<RootComponentProps> = props => {
+const MessagingApp = () => {
+  const { getTranslationPlugin } = useRootComponentProps();
+
   return (
-    <I18nextProvider i18n={props.plugins['@akashaorg/app-translation']?.translation?.i18n}>
-      <AppRoutes {...props} />
+    <I18nextProvider i18n={getTranslationPlugin().i18n}>
+      <AppRoutes />
     </I18nextProvider>
   );
 };
--- ui/apps/messaging/src/components/app-routes.tsx
+++ ui/apps/messaging/src/components/app-routes.tsx
@@ -1,74 +1,79 @@
+import React, { useCallback, useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import * as React from 'react';
-import routes, { CHAT, MESSAGING, SETTINGS } from '../routes';
-import { RootComponentProps } from '@akashaorg/typings/ui';
+
+import { useGetMyProfileQuery } from '@akashaorg/ui-awf-hooks/lib/generated/hooks-new';
+import { useRootComponentProps } from '@akashaorg/ui-awf-hooks';
+
+import Helmet from '@akashaorg/design-system-core/lib/components/Helmet';
+
+import ChatPage from './chat-page';
 import InboxPage from './inbox/inbox-page';
 import SettingsPage from './settings-page';
-import ChatPage from './chat-page';
-import Helmet from '@akashaorg/design-system-core/lib/components/Helmet';
-import { useGetMyProfileQuery } from '@akashaorg/ui-awf-hooks/lib/generated/hooks-new';
-import { getHubUser, getMessages } from '../api/message';
-import { db } from '../db/messages-db';
 
-const AppRoutes: React.FC<RootComponentProps> = props => {
+import routes, { CHAT, MESSAGING, SETTINGS } from '../routes';
+
+const AppRoutes = () => {
   const profileDataReq = useGetMyProfileQuery(null, {
     select: resp => {
       return resp.viewer?.akashaProfile;
     },
   });
   const loggedProfileData = profileDataReq.data;
-  const loggedUserId = React.useMemo(() => loggedProfileData?.did?.id, [loggedProfileData]);
 
-  // const getHubUserCallback = React.useCallback(getHubUser, [loggedUserId]);
+  // const getHubUserCallback = useCallback(getHubUser, [loggedUserId]);
 
-  const [fetchingMessages, setFetchingMessages] = React.useState(false);
+  const [fetchingMessages, setFetchingMessages] = useState(false);
 
-  const fetchMessagesCallback = React.useCallback(async () => {
+  const { baseRouteName } = useRootComponentProps();
+
+  const fetchMessagesCallback = useCallback(async () => {
     setFetchingMessages(true);
     // get all messages from textile inbox, currently filterring by user pubkey is not supported
-    const messagesData = await getMessages();
-    const allMessages = messagesData.data
-      ?.map(res => {
-        if (res.body.content) {
-          const chatPartnerId = res.from === loggedUserId ? res.to : res.from;
-          return {
-            content: res.body.content?.slateContent,
-            ethAddress: res.body.content?.author,
-            timestamp: res.createdAt,
-            name: null,
-            from: res.from,
-            to: res.to,
-            read: res.read,
-            id: res.id,
-            loggedUserId: loggedUserId,
-            chatPartnerId,
-          };
-        }
-        return null;
-      })
-      .filter(Boolean);
+    // const messagesData = await getMessages();
+
+    // const allMessages = messagesData.data
+    //   ?.map(res => {
+    //     if (res.body.content) {
+    //       const chatPartnerId = res.from === loggedUserId ? res.to : res.from;
+    //       return {
+    //         content: res.body.content?.slateContent,
+    //         ethAddress: res.body.content?.author,
+    //         timestamp: res.createdAt,
+    //         name: null,
+    //         from: res.from,
+    //         to: res.to,
+    //         read: res.read,
+    //         id: res.id,
+    //         loggedUserId: loggedUserId,
+    //         chatPartnerId,
+    //       };
+    //     }
+    //     return null;
+    //   })
+    //   .filter(Boolean);
+
     // add new messages, key second param is not required since we have id as inbound index
     // db.messages.bulkPut(allMessages);
     setFetchingMessages(false);
-  }, [loggedUserId]);
+  }, []);
 
-  React.useEffect(() => {
+  useEffect(() => {
     fetchMessagesCallback();
   }, [fetchMessagesCallback]);
 
   return (
-    <Router basename={props.baseRouteName}>
+    <Router basename={baseRouteName}>
       <Helmet>
         <title>Message App | AKASHA World</title>
       </Helmet>
       <Routes>
         <Route
           path={`${routes[MESSAGING]}`}
-          element={<InboxPage {...props} loggedProfileData={loggedProfileData} />}
+          element={<InboxPage loggedProfileData={loggedProfileData} />}
         ></Route>
         <Route
           path={`${routes[SETTINGS]}`}
-          element={<SettingsPage {...props} loggedProfileData={loggedProfileData} />}
+          element={<SettingsPage loggedProfileData={loggedProfileData} />}
         ></Route>
         <Route
           path={`${routes[CHAT]}/:pubKey`}
