baa639231a74bb83145365a8a98378a936cd8f20
Marius Darila <marius.darila@akasha.world>
2023-09-05 13:46:03 +0200

    refactor(gql): rebuild following fragment

--- sdk/src/sw.ts
+++ sdk/src/sw.ts
@@ -19,7 +19,7 @@ precacheAndRoute(self.__WB_MANIFEST);
 // pageCache({ networkTimeoutSeconds: 6 });
 staticResourceCache({cacheName: 'static-v1.0'});
 imageCache({ maxEntries: 300 });
-// offlineFallback({ pageFallback: '/img/offline-page.png', imageFallback: '/img/offline-img.png' });
+offlineFallback({ pageFallback: '/img/offline-page.png', imageFallback: '/img/offline-img.png' });
 // setCacheNameDetails({
 //   prefix: 'akasha',
 //   suffix: 'v0.1.0',
