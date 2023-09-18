2450e3ffdbdeb4b070df41aa22b4270a63babcb4
Didd Tuni <didd.tuni@akasha.world>
2023-09-18 08:51:12 +0300

    chore: add a comment to a new hook

--- ui/hooks/src/use-logged-in.ts
+++ ui/hooks/src/use-logged-in.ts
@@ -1,6 +1,13 @@
 import { useMemo } from 'react';
 import { useGetLogin } from './use-login.new';
 
+/**
+ * Hook for retrieving a flag to check if a user is logged in or not and the profile id of a user
+ * @example useLoggedIn hook
+ * ```typescript
+ * const { isLoggedIn, loggedInProfileId } = useLoggedIn();
+ * ```
+ */
 export function useLoggedIn() {
   const loginQuery = useGetLogin();
   const data = useMemo(() => {
