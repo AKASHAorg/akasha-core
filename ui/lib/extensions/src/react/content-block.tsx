91bdfc7147a806906aa1ce96dea920f609e1d51a
SeverS <sever.abi@gmail.com>
2023-09-26 18:06:13 +0300

    chore(): resolve blockIds in Extension component

--- ui/lib/extensions/src/react/content-block.tsx
+++ ui/lib/extensions/src/react/content-block.tsx
@@ -1,43 +1,74 @@
 import * as React from 'react';
-import { useRootComponentProps } from '@akashaorg/ui-awf-hooks';
+import { hasOwn, useRootComponentProps } from '@akashaorg/ui-awf-hooks';
 import Parcel from 'single-spa-react/parcel';
-import { ContentBlockExtensionInterface, ContentBlockModes } from '@akashaorg/typings/lib/ui';
+import {
+  ContentBlock,
+  ContentBlockExtensionInterface,
+  ContentBlockModes,
+} from '@akashaorg/typings/lib/ui';
+import { useGetContentBlockByIdQuery } from '@akashaorg/ui-awf-hooks/lib/generated/hooks-new';
+
+export type BlockExtensionInterface = {
+  createBlock?: () => Promise<{
+    response: { blockID: string; error?: string };
+    blockInfo: Omit<ContentBlock, 'loadingFn'> & { mode: ContentBlockModes };
+  }>;
+};
 
 export type ContentBlockEditProps = {
   mode: ContentBlockModes.EDIT;
   propertyType: string;
   appName: string;
-  blockRef?: React.RefObject<any>;
+  blockRef?: React.RefObject<BlockExtensionInterface>;
 };
 export type ContentBlockReadOnlyProps = {
   mode: ContentBlockModes.READONLY;
-  blockRef?: React.RefObject<any>;
-};
-export type ContentBlockInteractiveProps = {
-  mode: ContentBlockModes.INTERACTIVE;
-  blockRef?: React.RefObject<any>;
+  blockId: string;
+  blockRef?: React.RefObject<BlockExtensionInterface>;
 };
 
-export type ContentBlockExtensionProps =
-  | ContentBlockEditProps
-  | ContentBlockReadOnlyProps
-  | ContentBlockInteractiveProps;
+// export type ContentBlockInteractiveProps = {
+//   mode: ContentBlockModes.INTERACTIVE;
+//   blockRef?: React.RefObject<BlockExtensionInterface>;
+// };
+
+export type ContentBlockExtensionProps = ContentBlockEditProps | ContentBlockReadOnlyProps;
 
 export const ContentBlockExtension = (
   props: Extract<ContentBlockExtensionProps, { mode: ContentBlockModes }>,
 ) => {
   const { getExtensionsPlugin, getContext } = useRootComponentProps();
   const contentBlockStoreRef = React.useRef(getExtensionsPlugin().contentBlockStore);
+  const blockId = React.useMemo(() => {
+    if (hasOwn(props, 'blockId')) {
+      return blockId;
+    }
+  }, [props]);
+
+  const blockInfoQuery = useGetContentBlockByIdQuery(
+    { id: blockId },
+    {
+      enabled: props.mode === ContentBlockModes.EDIT && blockId,
+      select: data => {
+        if (hasOwn(data.node, 'id')) {
+          return data.node;
+        }
+      },
+    },
+  );
 
   const matchingBlock: ContentBlockExtensionInterface & { appName: string } = React.useMemo(() => {
     switch (props.mode) {
       case ContentBlockModes.EDIT:
         return contentBlockStoreRef.current.getMatchingBlock(props.appName, props.propertyType);
+      case ContentBlockModes.READONLY:
+        console.log(blockInfoQuery.data, '<< block info');
+        return contentBlockStoreRef.current.getMatchingBlock();
       default:
         break;
     }
     return [];
-  }, [props]);
+  }, [props, blockInfoQuery]);
 
   return (
     <div>
