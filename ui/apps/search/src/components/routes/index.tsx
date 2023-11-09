38006cf8f6a65ad572f387e5700d543ce287022d
themonster2015 <quynhyen.vothi@akasha.world>
2023-11-09 15:28:42 +0700

    chore(): use useLoggedIn hook

--- ui/apps/search/src/components/routes/index.tsx
+++ ui/apps/search/src/components/routes/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
-import { useRootComponentProps } from '@akashaorg/ui-awf-hooks';
-import { useGetMyProfileQuery } from '@akashaorg/ui-awf-hooks/lib/generated/hooks-new';
+import { useRootComponentProps, useLoggedIn } from '@akashaorg/ui-awf-hooks';
 import SearchPage from './search-page';
 import OnboardingPage from './onboarding-page';
 import SettingsPage from './settings-page';
@@ -12,12 +11,8 @@ import routes, { ONBOARDING, RESULTS, SETTINGS } from '../../routes';
 const AppRoutes: React.FC<unknown> = () => {
   const { baseRouteName, navigateToModal } = useRootComponentProps();
 
-  const profileDataReq = useGetMyProfileQuery(null, {
-    select: resp => {
-      return resp.viewer?.akashaProfile;
-    },
-  });
-  const loggedProfileData = profileDataReq.data;
+  const { isLoggedIn, loggedInProfileId } = useLoggedIn();
+
   const showLoginModal = () => {
     navigateToModal({ name: 'login' });
   };
@@ -31,30 +26,23 @@ const AppRoutes: React.FC<unknown> = () => {
             <Route
               path=":searchKeyword"
               element={
-                <SearchPage showLoginModal={showLoginModal} loggedProfileData={loggedProfileData} />
+                <SearchPage showLoginModal={showLoginModal} loggedInProfileId={loggedInProfileId} />
               }
             />
             <Route
               path=""
               element={
-                <SearchPage showLoginModal={showLoginModal} loggedProfileData={loggedProfileData} />
+                <SearchPage showLoginModal={showLoginModal} loggedInProfileId={loggedInProfileId} />
               }
             />
           </Route>
           <Route
             path={routes[SETTINGS]}
-            element={
-              <SettingsPage showLoginModal={showLoginModal} loggedProfileData={loggedProfileData} />
-            }
+            element={<SettingsPage showLoginModal={showLoginModal} isLoggedIn={isLoggedIn} />}
           />
           <Route
             path={routes[ONBOARDING]}
-            element={
-              <OnboardingPage
-                showLoginModal={showLoginModal}
-                loggedProfileData={loggedProfileData}
-              />
-            }
+            element={<OnboardingPage showLoginModal={showLoginModal} isLoggedIn={isLoggedIn} />}
           />
         </Routes>
       </Stack>
