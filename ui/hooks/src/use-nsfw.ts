fc5555c5faa47007314023145bfe15defceb7328
themonster2015 <quynhyen.vothi@akasha.world>
2024-01-26 15:16:36 +0700

    chore(): add nsfw hook

---
+++ ui/hooks/src/use-nsfw.ts
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+const NsfwChoice = 'Show-Nsfw';
+/**
+ * Hook to get and set user's choice for showing/hiding nsfw content
+ * @example useNsfwToggling hook
+ * ```typescript
+ * const {showNsfw, toggleShowNsfw} = useNsfwToggling();
+ * ```
+ */
+export function useNsfwToggling() {
+  const currentNsfwSelection = JSON.parse(window.localStorage.getItem(NsfwChoice));
+
+  const [showNsfw, setShowNsfw] = useState<boolean>(
+    currentNsfwSelection === null ? true : currentNsfwSelection,
+  );
+
+  const toggleShowNsfw = showNsfw => {
+    setShowNsfw(showNsfw);
+    window.localStorage.setItem(NsfwChoice, JSON.stringify(showNsfw));
+  };
+
+  return { showNsfw, toggleShowNsfw };
+}
