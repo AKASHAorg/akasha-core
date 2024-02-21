9cacbe98939188e394edf1976d1cc3de8769edf6
themonster2015 <quynhyen.vothi@akasha.world>
2024-02-21 15:28:56 +0700

    chore(): fix errorboundary error persisting in different routes in the same app

--- ui/apps/akasha/src/components/app.tsx
+++ ui/apps/akasha/src/components/app.tsx
@@ -3,14 +3,17 @@ import { I18nextProvider } from 'react-i18next';
 import { useRootComponentProps } from '@akashaorg/ui-awf-hooks';
 import Spinner from '@akashaorg/design-system-core/lib/components/Spinner';
 import AppRoutes from './app-routes';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const SocialApp: React.FC<unknown> = () => {
-  const { getTranslationPlugin } = useRootComponentProps();
+  const { getTranslationPlugin, baseRouteName } = useRootComponentProps();
 
   return (
     <React.Suspense fallback={<Spinner />}>
       <I18nextProvider i18n={getTranslationPlugin().i18n}>
-        <AppRoutes />
+        <Router basename={baseRouteName}>
+          <AppRoutes />
+        </Router>
       </I18nextProvider>
     </React.Suspense>
   );
