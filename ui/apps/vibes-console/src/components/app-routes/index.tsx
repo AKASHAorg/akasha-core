d1c4117b05a96a1870efb4b99cd72993f608df4a
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2024-02-29 09:38:59 +0100

    refactor(vc): implement tanstack router

---
+++ ui/apps/vibes-console/src/components/app-routes/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Outlet, Route, Router, rootRouteWithContext } from '@tanstack/react-router';
+import ErrorComponent from './error-component';
+import { Applications, Dashboard, Settings } from '../../pages';
+
+const rootRoute = rootRouteWithContext()({
+  component: Outlet,
+});
+
+const applicationsRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: '/applications',
+  component: () => {
+    return <Applications />;
+  },
+});
+
+const dashboardRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: '/dashboard',
+  component: () => {
+    return <Dashboard />;
+  },
+});
+
+const settingsRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: '/settings',
+  component: () => {
+    return <Settings />;
+  },
+});
+
+const routeTree = rootRoute.addChildren([applicationsRoute, dashboardRoute, settingsRoute]);
+
+// @todo: update to use type from typings package
+export const createRouter = ({ baseRouteName }: { baseRouteName: string }) =>
+  new Router({
+    routeTree,
+    basepath: baseRouteName,
+    defaultErrorComponent: ({ error }) => (
+      <ErrorComponent error={(error as unknown as Error).message} />
+    ),
+  });
--- ui/apps/vibes-console/src/components/app.tsx
+++ ui/apps/vibes-console/src/components/app.tsx
@@ -2,15 +2,26 @@ import React from 'react';
 import { I18nextProvider } from 'react-i18next';
 import { useRootComponentProps } from '@akashaorg/ui-awf-hooks';
 import Spinner from '@akashaorg/design-system-core/lib/components/Spinner';
-import AppRoutes from './app-routes';
+import { createRouter } from './app-routes';
+import { RouterProvider } from '@tanstack/react-router';
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: ReturnType<typeof createRouter>;
+  }
+}
 
 const Application: React.FC<unknown> = () => {
-  const { getTranslationPlugin } = useRootComponentProps();
+  const { baseRouteName, getTranslationPlugin } = useRootComponentProps();
 
   return (
     <React.Suspense fallback={<Spinner />}>
       <I18nextProvider i18n={getTranslationPlugin().i18n}>
-        <AppRoutes />
+        <RouterProvider
+          router={createRouter({
+            baseRouteName,
+          })}
+        />
       </I18nextProvider>
     </React.Suspense>
   );
