2e7d07d63f073d4fb30bc88e3b9ce8985a8e79b9
Vali Cotea <quininez@users.noreply.github.com>
2024-03-04 19:48:11 +0200

    Add tanstack router to apps (#2195)
    * feat(): add tanstack router to extensions, settings, legal apps
    
    * chore(): fix test
    
    * chore(): add routes as children to master route

--- ui/apps/extensions/src/components/App.tsx
+++ ui/apps/extensions/src/components/App.tsx
@@ -2,16 +2,34 @@ import React from 'react';
 import { I18nextProvider } from 'react-i18next';
 import { useRootComponentProps } from '@akashaorg/ui-awf-hooks';
 import Spinner from '@akashaorg/design-system-core/lib/components/Spinner';
-import AppRoutes from './app-routes';
+import { RouterProvider } from '@tanstack/react-router';
+import { router } from './app-routes/index';
+import { useApolloClient } from '@apollo/client';
+import { Helmet, helmetData } from '@akashaorg/design-system-core/lib/utils';
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: ReturnType<typeof router>;
+  }
+}
 
 const App: React.FC<unknown> = () => {
-  const { getTranslationPlugin } = useRootComponentProps();
+  const { getTranslationPlugin, baseRouteName } = useRootComponentProps();
+  const apolloClient = useApolloClient();
 
   return (
     <React.StrictMode>
       <React.Suspense fallback={<Spinner />}>
         <I18nextProvider i18n={getTranslationPlugin().i18n}>
-          <AppRoutes />
+          <Helmet helmetData={helmetData}>
+            <title>Extensions | AKASHA World</title>
+          </Helmet>
+          <RouterProvider
+            router={router({
+              baseRouteName,
+              apolloClient,
+            })}
+          />
         </I18nextProvider>
       </React.Suspense>
     </React.StrictMode>
--- ui/apps/settings-app/src/components/app.tsx
+++ ui/apps/settings-app/src/components/app.tsx
@@ -1,18 +1,33 @@
 import * as React from 'react';
 import { I18nextProvider } from 'react-i18next';
 import { useRootComponentProps } from '@akashaorg/ui-awf-hooks';
-
 import Spinner from '@akashaorg/design-system-core/lib/components/Spinner';
-import AppRoutes from './app-routes';
+
+import { RouterProvider } from '@tanstack/react-router';
+import { router } from './app-routes/index';
+import { Helmet, helmetData } from '@akashaorg/design-system-core/lib/utils';
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: ReturnType<typeof router>;
+  }
+}
 
 const Application: React.FC<unknown> = () => {
-  const { getTranslationPlugin } = useRootComponentProps();
+  const { getTranslationPlugin, baseRouteName } = useRootComponentProps();
 
   return (
     <React.StrictMode>
       <React.Suspense fallback={<Spinner />}>
         <I18nextProvider i18n={getTranslationPlugin().i18n}>
-          <AppRoutes />
+          <Helmet helmetData={helmetData}>
+            <title>Settings | AKASHA World</title>
+          </Helmet>
+          <RouterProvider
+            router={router({
+              baseRouteName,
+            })}
+          />
         </I18nextProvider>
       </React.Suspense>
     </React.StrictMode>
