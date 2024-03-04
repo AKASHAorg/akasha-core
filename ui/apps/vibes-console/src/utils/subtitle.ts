1f05fad597d462763b1de10156ad945a4c4fdbbb
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2024-03-04 07:54:04 +0100

    chore(vc): update utils

---
+++ ui/apps/vibes-console/src/utils/subtitle.ts
@@ -0,0 +1,39 @@
+export const BMIntroSubtitles = [
+  {
+    label:
+      'Becoming a moderator means that you will be responsible to protect the community from harmful contents that violate our',
+  },
+  {
+    label: 'Code of Conduct',
+    link: '/code-of-conduct',
+  },
+  {
+    label: 'Make sure that you have fully understood',
+  },
+  {
+    label: 'our values',
+    link: '/values',
+  },
+  {
+    label: 'and our code of conduct before applying!',
+  },
+];
+
+export const BMConfirmationSubtitles = [
+  {
+    label:
+      'Your application will be reviewed by the admin and you will receive feedback shortly. To check your application status, you can click',
+  },
+  {
+    label: 'here',
+    link: 'CHECK_APPLICATION_STATUS',
+  },
+  {
+    label: 'or by visiting the',
+  },
+  {
+    label: 'moderation overview',
+    link: 'HOME',
+  },
+  { label: 'page' },
+];
