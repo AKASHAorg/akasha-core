121f68e5cb9e08088aa19749e4080de6fef26530
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2024-03-04 13:12:32 +0100

    refactor(ds): update vibes cards - update story

--- ui/design-system-components/src/components/VibesIntroCard/index.tsx
+++ ui/design-system-components/src/components/VibesIntroCard/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { tw } from '@twind/core';
 import Anchor from '@akashaorg/design-system-core/lib/components/Anchor';
 import Card from '@akashaorg/design-system-core/lib/components/Card';
 import Stack from '@akashaorg/design-system-core/lib/components/Stack';
@@ -13,64 +12,33 @@ export type OverviewCTA = {
 };
 
 export type VibesIntroCardProps = {
-  assetName?: string;
-  assetExtension?: string;
   titleLabel: string;
-  introLabel: string;
   subtitleLabel: string;
-  publicImgPath?: string;
   overviewCTAArr: OverviewCTA[];
 };
 
 const VibesIntroCard: React.FC<VibesIntroCardProps> = props => {
-  const {
-    assetName = 'vibe-overview',
-    titleLabel,
-    introLabel,
-    subtitleLabel,
-    assetExtension = 'webp',
-    publicImgPath = '/images',
-    overviewCTAArr,
-  } = props;
+  const { titleLabel, subtitleLabel, overviewCTAArr } = props;
 
   return (
     <Card padding={16}>
-      <Stack spacing="gap-4" customStyle="grid grid-cols-1">
-        <Text variant="h5" align="center">
-          {titleLabel}
-        </Text>
-
-        <Stack customStyle="w-40 h-40 my-2 mx-auto">
-          <img
-            className={tw('object-contain')}
-            aria-label={assetName}
-            src={`${publicImgPath}/${assetName}.${assetExtension}`}
-            alt={assetName}
-            data-testid={`${assetName}-image`}
-          />
-        </Stack>
-
-        {introLabel && (
-          <Text weight="bold" align="center">
-            {introLabel}
-          </Text>
-        )}
+      <Stack spacing="gap-4">
+        <Text variant="h5">{titleLabel}</Text>
 
         {subtitleLabel && (
-          <Text variant="subtitle2" align="center">
-            {subtitleLabel}
+          <Text variant="body2" weight="light" color={{ light: 'grey5', dark: 'grey6' }}>
+            {subtitleLabel}.
           </Text>
         )}
 
         {overviewCTAArr && overviewCTAArr.length > 0 && (
-          <Stack justify="between" customStyle="md:px-20" spacing="gap-y-4">
+          <Stack justify="between" spacing="gap-y-4">
             {overviewCTAArr.map(({ url, label, handler }, idx) => (
               <Stack key={label + idx}>
                 {handler && typeof handler === 'function' ? (
                   <Button plain={true} onClick={handler}>
                     <Text
                       weight="bold"
-                      align="center"
                       variant="button-md"
                       color={{ light: 'secondaryLight', dark: 'secondaryDark' }}
                     >
@@ -78,11 +46,7 @@ const VibesIntroCard: React.FC<VibesIntroCardProps> = props => {
                     </Text>
                   </Button>
                 ) : (
-                  <Anchor
-                    href={url}
-                    dataTestId={`${label}-link`}
-                    customStyle="text-sm text-center font-bold"
-                  >
+                  <Anchor href={url} dataTestId={`${label}-link`} customStyle="text-sm font-bold">
                     {label}
                   </Anchor>
                 )}
