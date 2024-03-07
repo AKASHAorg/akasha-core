7cb7b5d514a200a0832d97627024ccfdfaa5051f
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2024-03-07 16:02:59 +0100

    refactor(ds): update table component - update story and test

--- ui/design-system-core/src/components/Table/index.tsx
+++ ui/design-system-core/src/components/Table/index.tsx
@@ -1,63 +1,19 @@
 import React from 'react';
 import { apply, tw } from '@twind/core';
-
 import Stack from '../Stack';
 import Text from '../Text';
-import Icon from '../Icon';
-import { ChevronRightIcon } from '../Icon/hero-icons-outline';
 
-type TDataValues = string[];
+type TDataValues = { value: React.ReactNode[]; clickHandler?: () => void };
 
 export type TableProps = {
   tableTitle?: string;
-  theadValues?: string[];
+  theadValues?: React.ReactNode[];
   rows: TDataValues[];
-  hasIcons?: boolean;
-  clickableRows?: boolean;
   customTdStyle?: string;
-  onRowClick?: (contentId: string) => void;
 };
 
 const Table: React.FC<TableProps> = props => {
-  const {
-    tableTitle,
-    theadValues,
-    rows,
-    hasIcons = false,
-    clickableRows = false,
-    customTdStyle = '',
-    onRowClick,
-  } = props;
-
-  const handleRowClick = (contentId: string) => () => {
-    if (clickableRows && typeof onRowClick === 'function') {
-      onRowClick(contentId);
-    }
-  };
-
-  const generateTdValue = (idx: number, value: string) => {
-    if (hasIcons && idx === 2) {
-      return (
-        <div className={tw('flex space-x-1.5 items-center')}>
-          <div
-            className={tw(
-              `w-2 h-2 rounded-full ${
-                ['Kept', 'Accepted'].includes(value)
-                  ? 'bg-success'
-                  : 'bg(errorLight dark:errorDark)'
-              }`,
-            )}
-          />
-          <Text variant="body2">{value}</Text>
-        </div>
-      );
-    }
-
-    if (hasIcons && idx === 3) {
-      return <Icon icon={<ChevronRightIcon />} accentColor={true} />;
-    }
-    return <Text variant="body2">{value}</Text>;
-  };
+  const { tableTitle, theadValues, rows, customTdStyle = '' } = props;
 
   const baseTdStyle = apply(`py-4 px-5 ${customTdStyle}`);
 
@@ -80,30 +36,27 @@ const Table: React.FC<TableProps> = props => {
           <thead>
             <tr>
               {theadValues.map((value, idx) => (
-                <th key={value + idx} className={tw(baseTdStyle)}>
-                  <Text>{value}</Text>
+                <th key={idx} className={tw(baseTdStyle)}>
+                  {value}
                 </th>
               ))}
             </tr>
           </thead>
         )}
         <tbody>
-          {rows.map((valueArr, idx) => (
+          {rows.map((row, idx) => (
             <tr
               key={idx}
               className={tw(
                 `${
                   !theadValues && idx === 0 ? 'border-none' : 'border(t-1 grey8 dark:grey5)'
-                } cursor-${clickableRows ? 'pointer' : 'default'}`,
+                } cursor-${typeof row.clickHandler == 'function' ? 'pointer' : 'default'}`,
               )}
-              onClick={handleRowClick(valueArr[3])}
+              onClick={row.clickHandler}
             >
-              {valueArr.map((value, idx) => (
-                <td
-                  key={value + idx}
-                  className={`${baseTdStyle} ${hasIcons && idx === 3 ? 'flex justify-end' : ''}`}
-                >
-                  {generateTdValue(idx, value)}
+              {row.value.map((value, idx) => (
+                <td key={idx} className={baseTdStyle}>
+                  {value}
                 </td>
               ))}
             </tr>
