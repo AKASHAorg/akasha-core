dbb5301c68fb6bb02972a8c407c3f4b3c48b3e3d
Vali Cotea <quinine35@gmail.com>
2024-03-08 16:55:31 +0200

    fix(): tests

--- ui/apps/akasha/src/components/__tests__/global-antenna-page.spec.tsx
+++ ui/apps/akasha/src/components/__tests__/global-antenna-page.spec.tsx
@@ -38,7 +38,9 @@ describe('< GlobalAntennaPage /> component', () => {
     await act(async () => {
       renderWithAllProviders(
         <BaseComponent
-          authenticatedProfile={genUser('pkh:eip155:5:0xc47a483494db8fe455ba29a53a7f75349dfc02ff')}
+          authenticatedProfile={genUser(
+            'did:pkh:eip155:5:0xc47a483494db8fe455ba29a53a7f75349dfc02ff',
+          )}
         />,
         {},
       );
