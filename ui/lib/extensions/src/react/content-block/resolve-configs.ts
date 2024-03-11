e33fae0cf67fca077ca030208e1927e67a832ef3
Didd Tuni <didd.tuni@akasha.world>
2024-03-11 08:21:53 +0300

    feat(): cache block config

--- ui/lib/extensions/src/react/content-block/resolve-configs.ts
+++ ui/lib/extensions/src/react/content-block/resolve-configs.ts
@@ -1,6 +1,7 @@
 import { ContentBlockModes } from '@akashaorg/typings/lib/ui';
 import { MatchingBlock } from './common.types';
 import { ILogger } from '@akashaorg/typings/lib/sdk/log';
+import { hasOwn } from '@akashaorg/ui-awf-hooks';
 
 interface IResolveConfigs {
   matchingBlocks: MatchingBlock[];
@@ -8,14 +9,24 @@ interface IResolveConfigs {
   logger: ILogger;
 }
 
+const blocksConfigCache = new Map();
+
 export const resolveConfigs = async ({ matchingBlocks, mode, logger }: IResolveConfigs) => {
   const newBlocks = [];
   for (const block of matchingBlocks) {
     try {
-      const config = await block.blockInfo.loadingFn({
-        blockInfo: { ...block.blockInfo, mode },
-        blockData: block.blockData,
-      })();
+      let id = null;
+      if (block?.blockData && hasOwn(block.blockData, 'id')) {
+        id = block.blockData.id;
+      }
+      if (!blocksConfigCache.has(id)) {
+        const config = await block.blockInfo.loadingFn({
+          blockInfo: { ...block.blockInfo, mode },
+          blockData: block.blockData,
+        })();
+        blocksConfigCache.set(id, config);
+      }
+      const config = blocksConfigCache.get(id);
       newBlocks.push({ ...block, config });
     } catch (err) {
       logger.error('content block resolveConfigs function', err);
