8a4f2f948398c905aeeec471d36948cad4a4cb7b
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2024-03-15 09:30:33 +0100

    chore(ds): update stories

---
+++ ui/design-system-components/src/stories/dscore/snackbar.stories.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { CheckCircleIcon } from '@akashaorg/design-system-core/lib/components/Icon/hero-icons-outline';
+import Snackbar, { SnackbarProps } from '@akashaorg/design-system-core/lib/components/Snackbar';
+import { NotificationTypes } from '@akashaorg/typings/lib/ui';
+
+const meta: Meta<SnackbarProps> = {
+  title: 'DSCore/Bars/Snackbar',
+  component: Snackbar,
+  tags: ['autodocs'],
+  argTypes: {
+    title: { control: 'text' },
+    type: {
+      control: 'select',
+      options: [
+        NotificationTypes.Alert,
+        NotificationTypes.Caution,
+        NotificationTypes.Error,
+        NotificationTypes.Info,
+        NotificationTypes.Success,
+      ],
+    },
+    icon: { control: 'object' },
+    description: { control: 'text' },
+    actionButtonLabel: { control: 'text' },
+    customStyle: { control: 'text' },
+    handleButtonClick: { action: 'button clicked' },
+    handleDismiss: { action: 'dismissed' },
+  },
+};
+
+type Story = StoryObj<SnackbarProps>;
+
+const baseArgs: Story = {
+  args: {
+    title: 'Snackbar',
+    description: 'Some important information will appear here',
+  },
+};
+
+export const Default: Story = { args: { ...baseArgs.args } };
+
+export const AlertSnackbar: Story = { args: { ...baseArgs.args, type: NotificationTypes.Alert } };
+
+export const CautionSnackbar: Story = {
+  args: { ...baseArgs.args, type: NotificationTypes.Caution },
+};
+
+export const ErrorSnackbar: Story = { args: { ...baseArgs.args, type: NotificationTypes.Error } };
+
+export const InfoSnackbar: Story = { args: { ...baseArgs.args, type: NotificationTypes.Info } };
+
+export const SuccessSnackbar: Story = {
+  args: { ...baseArgs.args, type: NotificationTypes.Success },
+};
+
+export const SnackbarWithActionButtonLabel: Story = {
+  args: { ...baseArgs.args, actionButtonLabel: 'OK' },
+};
+
+export const SnackbarWithSpecificIcon: Story = {
+  args: { ...baseArgs.args, icon: <CheckCircleIcon /> },
+};
+
+export default meta;
---
+++ ui/design-system-components/src/stories/dscore/spinner.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Spinner, { SpinnerProps } from '@akashaorg/design-system-core/lib/components/Spinner';
+
+const meta: Meta<SpinnerProps> = {
+  title: 'DSCore/Spinners/Spinner',
+  component: Spinner,
+  tags: ['autodocs'],
+  argTypes: {
+    size: { control: 'text', options: ['sm', 'md', 'lg', 'xl', 'xxl'] },
+    loadingLabel: { control: 'text' },
+    partialSpinner: { control: 'boolean' },
+  },
+};
+
+type Story = StoryObj<SpinnerProps>;
+
+const baseArgs: Story = {
+  args: {
+    size: 'sm',
+  },
+};
+
+export const Default: Story = { args: { ...baseArgs.args } };
+
+export const SpinnerWithLoadingLabel: Story = {
+  args: { ...baseArgs.args, loadingLabel: 'loading...' },
+};
+
+export const SpinnerWithColor: Story = { args: { ...baseArgs.args, color: 'blue' } };
+
+export const PartialSpinner: Story = { args: { ...baseArgs.args, partialSpinner: true } };
+
+export default meta;
