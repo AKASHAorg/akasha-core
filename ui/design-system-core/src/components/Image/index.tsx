9cc0f8c4749237026dc80c45a2c5d54b368b6d3c
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2024-03-17 05:44:22 +0100

    chore(ds): update components

--- ui/design-system-core/src/components/Image/index.tsx
+++ ui/design-system-core/src/components/Image/index.tsx
@@ -1,17 +1,14 @@
 import React, { PropsWithChildren } from 'react';
 import { apply, tw } from '@twind/core';
 
-export type ImageProps = {
-  customStyle?: string;
-  dataTestId?: string;
-};
+export type ImageProps = PropsWithChildren<
+  React.DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement> & {
+    customStyle?: string;
+    dataTestId?: string;
+  }
+>;
 
-const Image: React.FC<
-  PropsWithChildren<
-    ImageProps &
-      React.DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>
-  >
-> = props => {
+const Image: React.FC<ImageProps> = props => {
   const { src, customStyle = '', dataTestId, ...rest } = props;
 
   const className = apply`object-contain ${customStyle}`;
