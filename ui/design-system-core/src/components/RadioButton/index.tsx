94aeeb05e9b36976e9eca256c0b38018ab06b322
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2024-04-16 19:03:58 +0100

    refactor(ds): update radiobutton component

--- ui/design-system-core/src/components/RadioButton/index.tsx
+++ ui/design-system-core/src/components/RadioButton/index.tsx
@@ -16,7 +16,7 @@ export type RadioButtonProps = {
   handleChange: (e: React.ChangeEvent) => void;
 };
 
-const baseStyles = 'flex items-center space-x-2 mb-2';
+const baseStyles = 'flex items-center mb-2';
 
 const baseButtonStyles =
   'flex items-center justify-center mr-4 ml-0 p-0 rounded-full shrink-0 after:content-[] after:absolute';
@@ -25,8 +25,8 @@ export const RadioButton: React.FC<RadioButtonProps> = props => {
   const {
     handleChange,
     id,
-    label = 'Radio button',
-    value = 'Radio button',
+    label,
+    value,
     isSelected = false,
     error = false,
     size = 'small',
