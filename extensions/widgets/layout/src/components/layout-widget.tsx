9007cc9bad96fc003f5ba914c3c9c4fba8be302a
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2024-05-09 11:21:22 +0100

    chore(layout): adjust styles

--- extensions/widgets/layout/src/components/layout-widget.tsx
+++ extensions/widgets/layout/src/components/layout-widget.tsx
@@ -234,7 +234,7 @@ const Layout: React.FC<unknown> = () => {
             </Stack>
           </Stack>
 
-          <Stack align="center" justify="between" customStyle="sticky top-0 h-screen">
+          <Stack align="center" justify="between" customStyle="sticky top-0 h-screen relative">
             <Stack customStyle={`grid grid-auto-rows pt-4 ${showWidgets ? '' : 'hidden'}`}>
               <Widget
                 name={layoutConfig.widgetSlotId}
@@ -246,7 +246,7 @@ const Layout: React.FC<unknown> = () => {
               />
             </Stack>
 
-            <Stack customStyle="fixed bottom-2 mx-2 lg:(static my-4)">
+            <Stack customStyle="fixed bottom-2 mx-2 lg:(w-[21rem] mb-2)">
               <Widget name={layoutConfig.cookieWidgetSlotId} />
             </Stack>
           </Stack>
