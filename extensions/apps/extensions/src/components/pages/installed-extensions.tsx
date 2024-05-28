df8611248352206b8bd1e66cd58905540c523496
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2024-05-28 09:51:18 +0100

    feat(extensions): update component and page

--- extensions/apps/extensions/src/components/pages/installed-extensions.tsx
+++ extensions/apps/extensions/src/components/pages/installed-extensions.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from '@tanstack/react-router';
 import { useRootComponentProps } from '@akashaorg/ui-awf-hooks';
 import Button from '@akashaorg/design-system-core/lib/components/Button';
 import {
@@ -9,21 +10,30 @@ import {
 } from '@akashaorg/design-system-core/lib/components/Icon/akasha-icons';
 import { BellIcon, Cog8ToothIcon, MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { InstalledExtensions } from '../installed-extensions';
+import routes, { EXTENSIONS } from '../../routes';
 
 export const InstalledExtensionsPage: React.FC<unknown> = () => {
+  const navigate = useNavigate();
   const { getRoutingPlugin } = useRootComponentProps();
   const { t } = useTranslation('app-extensions');
 
   const handleAppClick = (appName: string) => {
-    console.log(appName);
     getRoutingPlugin().navigateTo({
       appName,
+    });
+  };
+
+  const handleClickDiscover = () => {
+    navigate({
+      to: routes[EXTENSIONS],
     });
   };
 
   const description = t(
     'Play with your friends in AKASHA World and enjoy a couple of puzzle games or drawing games or any kind of game!',
   );
+
+  const installedExtensions = [];
 
   const defaultExtensions = [
     {
@@ -65,19 +75,24 @@ export const InstalledExtensionsPage: React.FC<unknown> = () => {
     },
   ];
 
+  const addAction = ext => ({
+    ...ext,
+    action: <Button variant="secondary" label={t('Open')} onClick={() => handleAppClick(ext.id)} />,
+  });
+
   return (
     <InstalledExtensions
       titleLabel={t('Installed Extensions')}
-      defaultExtensions={defaultExtensions.map(ext => ({
-        ...ext,
-        action: (
-          <Button variant="secondary" label={t('Open')} onClick={() => handleAppClick(ext.id)} />
-        ),
-      }))}
+      installedExtensions={installedExtensions.map(addAction)}
+      defaultExtensions={defaultExtensions.map(addAction)}
       sections={[
         {
-          title: '',
-          description: '',
+          assetName: 'longbeam-notfound',
+          title: t('No extensions installed yet!'),
+          discoverLabel: t('Discover'),
+          description: t('cool extensions and install them'),
+          description2: t('to customize your world'),
+          onClickDiscover: handleClickDiscover,
         },
         {
           title: t('Default Extensions'),
