64449445319ba59f44b9d8bf03a99df8d5a53e14
Vali Cotea <quinine35@gmail.com>
2024-06-06 12:40:29 +0300

    chore()

--- extensions/apps/antenna/src/extensions/image-block/image-readonly-block.tsx
+++ extensions/apps/antenna/src/extensions/image-block/image-readonly-block.tsx
@@ -15,7 +15,7 @@ export const ImageReadonlyBlock = (
   return (
     <Stack align={content.align || 'start'} spacing="gap-1">
       <ImageBlockGallery images={transformedImages} />
-      {content.caption && <Text breakWord>{content.caption}</Text>}
+      {content.caption && <Text customStyle="break-all">{content.caption}</Text>}
     </Stack>
   );
 };
