139c2a5c2b2894b74f90b2194f719785d244b877
Didd Tuni <didd.tuni@akasha.world>
2024-06-10 12:58:00 +0300

    chore():

--- libs/design-system-core/jest.setup.js
+++ libs/design-system-core/jest.setup.js
@@ -1,7 +1,6 @@
 import { install } from '@twind/core';
 import twindConfig from './src/twind/twind.config.js';
 import ResizeObserver from 'resize-observer-polyfill';
-import '@testing-library/jest-dom';
 
 global.ResizeObserver = ResizeObserver;
 
