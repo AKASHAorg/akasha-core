b2eae4d222f2df5ef31a4c92346584b62de47abc
Didd Tuni <didd.tuni@akasha.world>
2024-06-13 18:41:15 +0300

    chore(): move mock file

--- extensions/apps/antenna/src/components/__mocks__/index.ts
+++ extensions/apps/antenna/src/components/__mocks__/index.ts
@@ -1,4 +1,5 @@
 export * from './get-beam-page-mocks';
 export * from './get-reflect-editor-mocks';
 export * from './get-reflection-page-mocks';
+export * from './get-reflection-feed-mocks';
 export * from './constants';
