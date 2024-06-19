e54fd3e9d29cd2c630a6086860f634516a38fe2b
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2024-06-19 11:51:29 +0100

    feat(extensions): conditionally add subroute

--- extensions/apps/extensions/src/index.tsx
+++ extensions/apps/extensions/src/index.tsx
@@ -7,7 +7,7 @@ import {
   RootComponentProps,
   MenuItemType,
 } from '@akashaorg/typings/lib/ui';
-import routes, { EXTENSIONS, HOME, INSTALLED } from './routes';
+import routes, { EXTENSIONS, HOME, INSTALLED, MY_EXTENSIONS } from './routes';
 import { ContentBlockStore } from './plugins/content-block-store';
 import { ExtensionStore } from './plugins/extension-store';
 import { WidgetStore } from './plugins/widget-store';
@@ -18,47 +18,63 @@ import { Akasha } from '@akashaorg/design-system-core/lib/components/Icon/akasha
 /**
  * All the plugins must export an object like this:
  */
-export const register: (opts: IntegrationRegistrationOptions) => IAppConfig = opts => ({
-  loadingFn: () => import('./components'),
-  mountsIn: opts.layoutConfig?.applicationSlotId,
-  logo: { type: LogoTypeSource.ICON, solidIcon: true, value: <Akasha /> },
-  i18nNamespace: ['app-extensions'],
-  routes: {
-    ...routes,
-  },
-  title: 'AKASHA Core Extensions',
-  menuItems: {
-    label: 'Extensions',
-    type: MenuItemType.App,
+export const register: (opts: IntegrationRegistrationOptions) => IAppConfig = opts => {
+  const isDevModeEnabled = window.localStorage.getItem('DevMode') === 'true';
+  const baseSubRoutes = [
+    {
+      label: HOME,
+      index: 0,
+      route: routes[HOME],
+      type: MenuItemType.Internal,
+    },
+    {
+      label: EXTENSIONS,
+      index: 1,
+      route: routes[EXTENSIONS],
+      type: MenuItemType.Internal,
+    },
+    {
+      label: INSTALLED,
+      index: 2,
+      route: routes[INSTALLED],
+      type: MenuItemType.Internal,
+    },
+  ];
+  const subRoutes = isDevModeEnabled
+    ? [
+        ...baseSubRoutes,
+        {
+          label: MY_EXTENSIONS,
+          index: 3,
+          route: routes[MY_EXTENSIONS],
+          type: MenuItemType.Internal,
+        },
+      ]
+    : baseSubRoutes;
+
+  return {
+    loadingFn: () => import('./components'),
+    mountsIn: opts.layoutConfig?.applicationSlotId,
     logo: { type: LogoTypeSource.ICON, solidIcon: true, value: <Akasha /> },
-    area: [MenuItemAreaType.AppArea],
-    subRoutes: [
-      {
-        label: HOME,
-        index: 0,
-        route: routes[HOME],
-        type: MenuItemType.Internal,
-      },
-      {
-        label: EXTENSIONS,
-        index: 1,
-        route: routes[EXTENSIONS],
-        type: MenuItemType.Internal,
-      },
-      {
-        label: INSTALLED,
-        index: 2,
-        route: routes[INSTALLED],
-        type: MenuItemType.Internal,
-      },
-    ],
-  },
-  extends: (matcher, loader) => {
-    matcher({
-      'install-app': loader(() => import('./extensions/install-app')),
-    });
-  },
-});
+    i18nNamespace: ['app-extensions'],
+    routes: {
+      ...routes,
+    },
+    title: 'AKASHA Core Extensions',
+    menuItems: {
+      label: 'Extensions',
+      type: MenuItemType.App,
+      logo: { type: LogoTypeSource.ICON, solidIcon: true, value: <Akasha /> },
+      area: [MenuItemAreaType.AppArea],
+      subRoutes: subRoutes,
+    },
+    extends: (matcher, loader) => {
+      matcher({
+        'install-app': loader(() => import('./extensions/install-app')),
+      });
+    },
+  };
+};
 
 export const getPlugin = (
   props: RootComponentProps & {
