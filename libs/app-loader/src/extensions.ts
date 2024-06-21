406c5ae757efda63f4eb1efa9417ddfad9c76b7d
Marius Darila <3396463+kenshyx@users.noreply.github.com>
2024-06-21 11:14:04 +0200

    feat(registry): load remote source (#2323)
    * feat(registry): load remote source
    
    * chore(appLoader): use URL for path join
    
    * chore(): add fallback mainFile path

--- libs/app-loader/src/extensions.ts
+++ libs/app-loader/src/extensions.ts
@@ -1,8 +1,83 @@
 import { WorldConfig } from '@akashaorg/typings/lib/ui';
 import getSDK from '@akashaorg/awf-sdk';
+import { SortOrder } from '@akashaorg/typings/lib/sdk/graphql-types-new';
+import { ILogger } from '@akashaorg/typings/lib/sdk/log';
+
+/**
+ * Retrieves the latest version of a published app from the Akasha Registry.
+ *
+ * @param appName - The name of the app to retrieve.
+ * @param logger - An optional logger instance to use for logging.
+ * @returns The latest version of the app, including its source, version, ID, creation date, and manifest data.
+ * @throws Error if the DID_PUBLISHER_PUBLIC_KEY environment variable is not defined.
+ */
+const getPublishedAppLatestVersion = async (appName: string, logger?: ILogger) => {
+  const sdk = getSDK();
+  //this will change to INDEXING_DID once we have the passive indexing feature
+  const did = process.env?.DID_PUBLISHER_PUBLIC_KEY;
+  const log = logger || console;
+  if (!did) {
+    throw new Error('DID_PUBLISHER_PUBLIC_KEY is not defined');
+  }
+  const app = await sdk.services.gql.client.GetAppsByPublisherDID(
+    {
+      id: did,
+      first: 1,
+      filters: { where: { name: { equalTo: appName } } },
+      sorting: { createdAt: SortOrder.Desc },
+    },
+    { context: { source: sdk.services.gql.contextSources.default } },
+  );
+  if (!('akashaAppList' in app.node)) {
+    log.warn(`An error occurred while fetching app ${appName}`);
+    return;
+  }
+  const appNode = app?.node?.akashaAppList?.edges.length
+    ? app?.node?.akashaAppList?.edges[0].node
+    : undefined;
+
+  if (!appNode.id) {
+    log.warn(`No app found for publisher ${did} and name ${appName}`);
+    return;
+  }
+
+  if (!appNode.releasesCount || !appNode.releases.edges.length) {
+    log.warn(`No published versions found for app ${appName}`);
+    return;
+  }
+  const { source, version, id, createdAt } = appNode.releases.edges[0].node;
+
+  // @Todo: update the manifest config
+  return {
+    sources: sdk.services.common.ipfs.multiAddrToUri([source]),
+    id,
+    createdAt,
+    name: appName,
+    enabled: true,
+    author: did,
+    integrationID: id,
+    integrationType: appNode.applicationType,
+    manifestData: {
+      version,
+      description: appNode.description,
+      displayName: appNode.displayName,
+      keywords: appNode.keywords,
+      license: appNode.license,
+      contributors: appNode.contributors?.map(contributor => contributor.akashaProfile.did.id),
+      mainFile: 'index.js',
+    },
+  };
+};
 
 export const getRemoteLatestExtensionInfos = async (extensions: { name: string }[]) => {
-  return Promise.resolve([]);
+  const pkgInfos = [];
+  for (const extension of extensions) {
+    const app = await getPublishedAppLatestVersion(extension.name);
+    if (app) {
+      pkgInfos.push(app);
+    }
+  }
+  return Promise.resolve(pkgInfos);
 };
 
 export const getExtensionsData = async (extNames: string[], worldConfig: WorldConfig) => {
