8472c49e98f33be5f8f538cffba8ca595a41976f
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2024-06-26 12:33:01 +0100

    fix(ds): stop ev propagation on list item handlers
    Signed-off-by: Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>

--- libs/design-system-core/src/components/List/list-element.tsx
+++ libs/design-system-core/src/components/List/list-element.tsx
@@ -9,15 +9,14 @@ export type ListItemProps = { customStyle?: string } & ListItem;
 
 const ListElement: React.FC<ListItemProps> = props => {
   const { label, icon, color, disabled, customStyle = '', onClick, ...rest } = props;
+
+  const handleButtonClick = (e: React.SyntheticEvent) => {
+    e.stopPropagation();
+    if (onClick) onClick(label);
+  };
+
   return (
-    <Button
-      customStyle={customStyle}
-      onClick={() => {
-        if (onClick) onClick(label);
-      }}
-      disabled={disabled}
-      plain
-    >
+    <Button customStyle={customStyle} onClick={handleButtonClick} disabled={disabled} plain>
       <Stack direction="row" align="center" spacing="gap-x-1" customStyle="py-2 px-4">
         {icon && <Icon icon={icon} color={color} size="sm" />}
         <Text variant="body1" color={color} {...rest}>
