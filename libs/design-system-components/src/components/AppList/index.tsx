34675b3cea18f3dd3a4a787d38d8edd685b7c598
Vali Cotea <quininez@users.noreply.github.com>
2024-07-17 10:16:15 +0300

    My extensions Infinite list (#2342)
    * feat(): my extensions page
    
    * feat(): my extensions page improvements
    
    * chore(): typings, other fixes
    
    * chore(): fix test
    
    * chore(): adjust typings
    
    * feat(): add infinte scroll to applist
    
    * my extensions infinite list
    
    * chore()
    
    * chore()
    
    * chore(): remove unsused utils methods

--- libs/design-system-components/src/components/AppList/index.tsx
+++ libs/design-system-components/src/components/AppList/index.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react';
-
+import InfiniteScroll from '../InfiniteScroll';
 import Divider from '@akashaorg/design-system-core/lib/components/Divider';
 import Icon from '@akashaorg/design-system-core/lib/components/Icon';
 import Stack from '@akashaorg/design-system-core/lib/components/Stack';
@@ -27,14 +27,17 @@ export type App = {
 export type AppListProps = {
   apps: App[];
   showAppTypeIndicator?: boolean;
+  onLoadMore: () => Promise<unknown>;
 };
+
+const ENTRY_HEIGHT = 92;
 
 /**
  * Component that renders a list of apps
  * @param apps - array of extensions
  * @param onAppSelected - handler for clicking on an app from the list
  */
-const AppList: React.FC<AppListProps> = ({ apps, showAppTypeIndicator }) => {
+const AppList: React.FC<AppListProps> = ({ apps, showAppTypeIndicator, onLoadMore }) => {
   const getIconByAppType = (applicationType: AkashaAppApplicationType) => {
     switch (applicationType) {
       case AkashaAppApplicationType.App:
@@ -47,48 +50,56 @@ const AppList: React.FC<AppListProps> = ({ apps, showAppTypeIndicator }) => {
   };
 
   return (
-    <Stack spacing="gap-y-4">
-      {apps?.map((app, index, array) => (
-        <Stack key={app.name} spacing="gap-y-4">
-          <Stack direction="row" justify="between" align="center" spacing="gap-x-8">
-            <Stack direction="row" spacing="gap-x-3">
-              <AppAvatar appType={app.applicationType} avatar={app.logoImage} />
-              <Stack direction="column" justify="between">
-                <Stack direction="row" spacing="gap-2">
-                  <Text variant="button-sm">{app.name}</Text>
-                  {showAppTypeIndicator && (
-                    <Stack
-                      customStyle="w-[18px] h-[18px] rounded-full"
-                      background={{ light: 'secondaryLight', dark: 'secondaryDark' }}
-                      justify="center"
-                      align="center"
-                    >
-                      <Icon
-                        color={{ light: 'secondaryLight', dark: 'white' }}
-                        size={'xs'}
-                        solid
-                        icon={getIconByAppType(app.applicationType)}
-                      />
-                    </Stack>
-                  )}
+    <InfiniteScroll
+      totalElements={apps.length}
+      itemHeight={ENTRY_HEIGHT}
+      overScan={1}
+      onLoadMore={onLoadMore}
+    >
+      {({ index, itemIndex }) => {
+        const app = apps[itemIndex];
+        return (
+          <Stack spacing="gap-y-4">
+            <Stack direction="row" justify="between" align="center" spacing="gap-x-8">
+              <Stack direction="row" spacing="gap-x-3">
+                <AppAvatar appType={app.applicationType} avatar={app.logoImage} />
+                <Stack direction="column" justify="between">
+                  <Stack direction="row" spacing="gap-2">
+                    <Text variant="button-sm">{app.name}</Text>
+                    {showAppTypeIndicator && (
+                      <Stack
+                        customStyle="w-[18px] h-[18px] rounded-full"
+                        background={{ light: 'secondaryLight', dark: 'secondaryDark' }}
+                        justify="center"
+                        align="center"
+                      >
+                        <Icon
+                          color={{ light: 'secondaryLight', dark: 'white' }}
+                          size={'xs'}
+                          solid
+                          icon={getIconByAppType(app.applicationType)}
+                        />
+                      </Stack>
+                    )}
+                  </Stack>
+                  <Text
+                    variant="footnotes2"
+                    weight="normal"
+                    color={{ light: 'grey4', dark: 'grey7' }}
+                    lineClamp={2}
+                  >
+                    {app.description || app.displayName}
+                  </Text>
                 </Stack>
-                <Text
-                  variant="footnotes2"
-                  weight="normal"
-                  color={{ light: 'grey4', dark: 'grey7' }}
-                  lineClamp={2}
-                >
-                  {app.description || app.displayName}
-                </Text>
               </Stack>
-            </Stack>
 
-            {app.action}
+              {app.action}
+            </Stack>
+            {index < apps.length - 1 && <Divider />}
           </Stack>
-          {index < array.length - 1 && <Divider />}
-        </Stack>
-      ))}
-    </Stack>
+        );
+      }}
+    </InfiniteScroll>
   );
 };
 
