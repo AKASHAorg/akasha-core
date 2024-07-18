95c9f496b20ede0072f0ef3fe3de826cc161b9b5
Didd <didd.tuni@akasha.world>
2024-07-18 17:28:24 +0300

    chore(): change the logic of checking host matches a given domain

--- libs/design-system-components/src/utils/get-icon-from-link.ts
+++ libs/design-system-components/src/utils/get-icon-from-link.ts
@@ -1,12 +1,17 @@
 import { IconType } from '@akashaorg/typings/lib/ui';
-import isUrl from 'is-url';
 
 export const getIconFromLink = (href: string): IconType | null => {
-  if (!isUrl(href)) return null;
   const host = new URL(href).host;
-  if (host.endsWith('github.com')) return 'Github';
-  if (host.endsWith('twitter.com') || host.endsWith('x.com')) return 'Twitter';
-  if (host.endsWith('discordapp.com')) return 'Discord';
-  if (host.endsWith('t.me') || host.endsWith('telegram.me')) return 'Telegram';
+  if (checkHostMatchesDomain(host, 'github.com')) return 'Github';
+  if (checkHostMatchesDomain(host, 'twitter.com') || checkHostMatchesDomain(host, 'x.com'))
+    return 'Twitter';
+  if (checkHostMatchesDomain(host, 'discordapp.com')) return 'Discord';
+  if (checkHostMatchesDomain(host, 't.me') || checkHostMatchesDomain(host, 'telegram.me'))
+    return 'Telegram';
   return 'LinkIcon';
 };
+
+function checkHostMatchesDomain(host: string, domain: string) {
+  const regex = new RegExp(`^((https?://)?(www.)?)${domain}(?:\\/.*)?`);
+  return regex.test(host);
+}
