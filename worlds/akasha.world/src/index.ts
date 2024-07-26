e0d688b5f03b0c9f55938c94c99479482940213c
Marius Darila <marius.darila@akasha.world>
2024-07-26 15:33:38 +0200

    chore(): without dynamic import

--- worlds/akasha.world/src/index.ts
+++ worlds/akasha.world/src/index.ts
@@ -3,6 +3,7 @@ import '@akashaorg/design-system-core/src/twind/globals.css';
 
 import { WorldConfig } from '@akashaorg/typings/lib/ui';
 import { AkashaApp, AkashaAppApplicationType } from '@akashaorg/typings/lib/sdk/graphql-types-new';
+import rOverrides from './registry-overrides';
 
 console.time('AppLoader:firstMount');
 
@@ -47,10 +48,10 @@ declare const __LOAD_LOCAL_SOURCES__: boolean;
     },
   ];
 
-  // if (__DEV__ || __LOAD_LOCAL_SOURCES__) {
-  //   const overwrites = await import('./registry-overrides');
-  //   registryOverrides = overwrites.default;
-  // }
+  if (__DEV__ || __LOAD_LOCAL_SOURCES__) {
+    //   const overwrites = await import('./registry-overrides');
+    registryOverrides = rOverrides;
+  }
 
   const loaderConfig: WorldConfig = {
     title: 'AKASHA World',
