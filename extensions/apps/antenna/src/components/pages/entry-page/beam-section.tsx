7bc18fdafc10dff75cec39515516cf1cbff8abb0
Didd <didd.tuni@akasha.world>
2024-07-29 11:36:31 +0300

    fix(): fill editor space  when a beam is removed

--- extensions/apps/antenna/src/components/pages/entry-page/beam-section.tsx
+++ extensions/apps/antenna/src/components/pages/entry-page/beam-section.tsx
@@ -4,6 +4,7 @@ import Divider from '@akashaorg/design-system-core/lib/components/Divider';
 import Stack from '@akashaorg/design-system-core/lib/components/Stack';
 import BeamCard from '@akashaorg/ui-lib-feed/lib/components/cards/beam-card';
 import ReflectEditor from '../../reflect-editor';
+import Card from '@akashaorg/design-system-core/lib/components/Card';
 import routes, { REFLECT } from '../../../routes';
 import { useTranslation } from 'react-i18next';
 import { BeamData } from '@akashaorg/typings/lib/ui';
@@ -25,10 +26,15 @@ const BeamSection: React.FC<BeamSectionProps> = props => {
   const [isReflecting, setIsReflecting] = useState(
     routerState.location.pathname.endsWith(routes[REFLECT]),
   );
+  const activeBeam = beamData?.active;
 
   return (
-    <Stack dataTestId="beam-section" spacing="gap-y-2" customStyle="grow mb-2">
-      <Stack customStyle="grow">
+    <Stack
+      dataTestId="beam-section"
+      spacing="gap-y-2"
+      customStyle={`grow ${activeBeam ? 'mb-2' : ''}`}
+    >
+      <Card customStyle="grow" type="plain">
         <BeamCard
           beamData={beamData}
           noWrapperCard={true}
@@ -47,32 +53,34 @@ const BeamSection: React.FC<BeamSectionProps> = props => {
             setIsReflecting(!isReflecting);
           }}
         />
-        <Divider />
-      </Stack>
-      <Stack padding="px-2" customStyle="mt-auto">
-        {!isLoggedIn && (
-          <EditorPlaceholder
-            onClick={() =>
-              showLoginModal(
-                'Member Only Feature',
-                'You need to connect first to be able to use this feature.',
-              )
-            }
-            profileId={null}
-            actionLabel={t('Reflect')}
-            placeholderLabel={t('Share your thoughts')}
-            transformSource={transformSource}
-          />
-        )}
-        {isLoggedIn && beamData?.active && (
-          <ReflectEditor
-            beamId={beamId}
-            reflectToId={beamId}
-            showEditor={isReflecting}
-            setShowEditor={setIsReflecting}
-          />
-        )}
-      </Stack>
+        {activeBeam && <Divider />}
+      </Card>
+      {activeBeam && (
+        <Stack padding="px-2" customStyle="mt-auto">
+          {!isLoggedIn && (
+            <EditorPlaceholder
+              onClick={() =>
+                showLoginModal(
+                  'Member Only Feature',
+                  'You need to connect first to be able to use this feature.',
+                )
+              }
+              profileId={null}
+              actionLabel={t('Reflect')}
+              placeholderLabel={t('Share your thoughts')}
+              transformSource={transformSource}
+            />
+          )}
+          {isLoggedIn && (
+            <ReflectEditor
+              beamId={beamId}
+              reflectToId={beamId}
+              showEditor={isReflecting}
+              setShowEditor={setIsReflecting}
+            />
+          )}
+        </Stack>
+      )}
     </Stack>
   );
 };
