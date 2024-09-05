d18818bad6e4fcb0a92ec07dc009fa341f965d61
SeverS <sever.abi@gmail.com>
2024-09-05 14:39:15 +0300

    chore(): fix dropdown on beam card

--- libs/design-system-core/src/utils/useCloseActions.ts
+++ libs/design-system-core/src/utils/useCloseActions.ts
@@ -1,11 +1,24 @@
 import React, { useRef } from 'react';
 import { useClickAway, useKey } from 'react-use';
 
-export function useCloseActions(onClose: () => void, wrapperRef?: React.MutableRefObject<unknown>) {
+export function useCloseActions(
+  onClose: () => void,
+  wrapperRef?: React.MutableRefObject<unknown>,
+  // if set to false, it does not trigger the callback
+  // when clicking away. Useful when you handle clickAway manually but still want to trigger on key press
+  triggerOnClickAway = true,
+) {
   const ref = useRef(null);
   const contentRef = wrapperRef ? wrapperRef : ref;
+
+  const onClickAwayClose = () => {
+    if (triggerOnClickAway) {
+      onClose();
+    }
+  };
+
   useKey('Esc', onClose);
   useKey('Escape', onClose);
-  useClickAway(contentRef, onClose); //Specifying event should be avoided as it has unpredictable results
+  useClickAway(contentRef, onClickAwayClose);
   return contentRef;
 }
