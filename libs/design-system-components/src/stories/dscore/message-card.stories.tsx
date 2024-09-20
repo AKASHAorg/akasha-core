48b6d27f1ef410a665455dce7d0738a1d5ecdd0e
Didd Tuni <35398733+didd@users.noreply.github.com>
2024-09-20 11:25:31 +0300

    chore(): mention only one user per block (#2410)

--- libs/design-system-components/src/stories/dscore/message-card.stories.tsx
+++ libs/design-system-components/src/stories/dscore/message-card.stories.tsx
@@ -1,7 +1,10 @@
+import React from 'react';
+import Icon from '@akashaorg/design-system-core/lib/components/Icon';
 import type { Meta, StoryObj } from '@storybook/react';
 import MessageCard, {
   MessageCardProps,
 } from '@akashaorg/design-system-core/lib/components/MessageCard';
+import { ExclamationTriangleIcon } from '@akashaorg/design-system-core/lib/components/Icon/hero-icons-outline';
 
 MessageCard.displayName = 'MessageCard';
 
@@ -9,22 +12,32 @@ const meta: Meta<MessageCardProps> = {
   title: 'DSCore/Cards/MessageCard',
   component: MessageCard,
   argTypes: {
-    title: { control: 'text' },
     message: { control: 'text' },
-    titleIcon: { control: 'object' },
-    titleVariant: { control: 'text' },
-    elevation: { control: 'text' },
-    background: { control: 'text' },
-    borderColor: { control: 'text' },
+    icon: { control: 'object' },
+    background: { control: 'object' },
     customStyle: { control: 'text' },
-    onClose: { action: 'card closed' },
   },
 };
 
 type Story = StoryObj<MessageCardProps>;
 
 export const Default: Story = {
-  args: { title: 'Title', elevation: '1', message: 'A sample message...' },
+  args: {
+    message: 'A sample message...',
+  },
+};
+
+export const MessageCardWithIcon: Story = {
+  args: {
+    icon: (
+      <Icon
+        icon={<ExclamationTriangleIcon />}
+        color={{ light: 'warningLight', dark: 'warningLight' }}
+      />
+    ),
+    message: 'A sample message...',
+    background: { light: 'warningDark/30', dark: 'warningDark/30' },
+  },
 };
 
 export default meta;
