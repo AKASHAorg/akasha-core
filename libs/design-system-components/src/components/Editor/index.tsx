def8f05adcb8769fe23d32396793b25206b5e098
Didd <didd.tuni@akasha.world>
2024-09-22 22:04:48 +0300

    chore(): refactor editor's link feature

--- libs/design-system-components/src/components/Editor/index.tsx
+++ libs/design-system-components/src/components/Editor/index.tsx
@@ -1,4 +1,11 @@
-import React, { useState, useRef, useCallback, useEffect, useLayoutEffect } from 'react';
+import React, {
+  useState,
+  useRef,
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  KeyboardEvent,
+} from 'react';
 import {
   createEditor,
   Editor,
@@ -168,7 +175,7 @@ const EditorBox: React.FC<EditorBoxProps> = props => {
   const { insertData, insertText } = editor;
 
   const handleInsertLink = (text: string) => {
-    CustomEditor.insertLink(editor, { url: text.trim() });
+    CustomEditor.insertLink(editor, text.trim());
   };
 
   editor.insertText = text => {
@@ -404,7 +411,26 @@ const EditorBox: React.FC<EditorBoxProps> = props => {
   };
 
   const onKeyDown = useCallback(
-    event => {
+    (event: KeyboardEvent<HTMLDivElement>) => {
+      const { selection } = editor;
+      /* Default left/right behavior is unit:'character'.
+       ** This fails to distinguish between two cursor positions, such as
+       ** <inline>foo<cursor/></inline> vs <inline>foo</inline><cursor/>.
+       ** Here we modify the behavior to unit:'offset'.
+       ** This lets the user step into and out of the inline without stepping over characters.
+       */
+      if (selection && Range.isCollapsed(selection)) {
+        if (event.key === 'ArrowLeft') {
+          event.preventDefault();
+          Transforms.move(editor, { unit: 'offset', reverse: true });
+          return;
+        }
+        if (event.key === 'ArrowRight') {
+          event.preventDefault();
+          Transforms.move(editor, { unit: 'offset' });
+          return;
+        }
+      }
       /**
        * key handler for the mention popover
        * inserts the mention on tab, enter or space keypress
