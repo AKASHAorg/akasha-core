f68a5a69e3bd45a5fa74f0e8090212608fad42cc
Sever Abibula <5903809+SeverS@users.noreply.github.com>
2024-10-18 22:21:48 +0300

    add description, license and collaborators sub-pages (#2445)
    * chore(): add descr, lic and collab sub-pages
    
    * chore(): fix default pill text color

--- extensions/apps/extensions/src/components/pages/info-page/sub-pages/app-description-page.tsx
+++ extensions/apps/extensions/src/components/pages/info-page/sub-pages/app-description-page.tsx
@@ -1,39 +1,43 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import Text from '@akashaorg/design-system-core/lib/components/Text';
 import Card from '@akashaorg/design-system-core/lib/components/Card';
 import Stack from '@akashaorg/design-system-core/lib/components/Stack';
 import Divider from '@akashaorg/design-system-core/lib/components/Divider';
-import ExtensionHeader from '@akashaorg/design-system-components/lib/components/ExtensionHeader';
+import InfoSubRoutePageHeader from '../InfoSubroutePageHeader';
+import { useTranslation } from 'react-i18next';
+import {
+  AkashaAppApplicationType,
+  AppImageSource,
+} from '@akashaorg/typings/lib/sdk/graphql-types-new';
 
 type AppDescriptionPageProps = {
   appId: string;
+  description?: string;
+  extensionLogo?: AppImageSource;
+  extensionName?: string;
+  extensionDisplayName?: string;
+  extensionType?: AkashaAppApplicationType;
 };
 
-export const AppDescriptionPage: React.FC<AppDescriptionPageProps> = ({ appId }) => {
-  const { t } = useTranslation('app-extensions');
-  // @TODO get app description from the hook when available
-
-  const mockDescriptionData =
-    'This is a brief description of the extension, highlighting its main features and functionality. Explaining what it does and how it benefits the user in a couple of concise sentences.';
+export const AppDescriptionPage = (props: AppDescriptionPageProps) => {
+  const { description, extensionLogo, extensionDisplayName, extensionName, extensionType } = props;
+  const { t } = useTranslation();
 
   return (
     <>
       <Card padding="p-4">
         <Stack spacing="gap-y-4">
-          <ExtensionHeader
-            appName={'Extension Name'}
-            packageName="Package name"
+          <InfoSubRoutePageHeader
             pageTitle={t('Description')}
+            appName={extensionDisplayName}
+            packageName={extensionName}
+            appLogo={extensionLogo}
+            appType={extensionType}
           />
           <Divider />
-          {!!mockDescriptionData && (
-            <Stack spacing="gap-y-4">
-              <Text variant="body2">
-                {t('{{description}}', { description: mockDescriptionData })}
-              </Text>
-            </Stack>
-          )}
+          <Stack spacing="gap-y-4">
+            <Text variant="body2">{description}</Text>
+          </Stack>
         </Stack>
       </Card>
     </>
