69af2ca7d9771b3aa344987f704dce7718af1547
Didd Tuni <35398733+didd@users.noreply.github.com>
2025-02-04 21:21:08 +0300

    chore(): adjust ui package components (#2540)

--- libs/ui/src/akasha-components/image.tsx
+++ libs/ui/src/akasha-components/image.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import * as React from 'react';
 import { Loader2 } from 'lucide-react';
 
@@ -105,5 +103,6 @@ const Image = React.forwardRef<HTMLImageElement, ImageProps>(
     );
   },
 );
+Image.displayName = 'Image';
 
 export { ImageRoot, ImageFallback, Image };
