b313d8f130c34f232a1b260ab77f6b046c133037
AndinAvdiu <andin.avdiu@akasha.world>
2025-02-07 14:25:34 +0100

    Fix comment

--- extensions/apps/settings/src/components/pages/notifications-option.tsx
+++ extensions/apps/settings/src/components/pages/notifications-option.tsx
@@ -41,7 +41,7 @@ const NotificationsOption: React.FC = () => {
     success: {
       title: t('In-app notifications enabled'),
       description: t(
-        // The current implementation of 'Initialize' method of PushProtocolAPI does not subscribe the user to all the channels by default
+        // The current implementation of 'Initialize' method of PushProtocolAPI does not subscribe the user to all the channel options by default
         'Notifications for all default apps are disabled. Manage them in preferences.',
       ),
     },
