a8704dbbc5787e1f8304b30a04ff49ec4b6f02e4
Didd <didd.tuni@akasha.world>
2025-02-10 13:03:27 +0300

    chore(): update button component

--- libs/ui/src/akasha-components/button.tsx
+++ libs/ui/src/akasha-components/button.tsx
@@ -4,8 +4,10 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { Loader2 } from 'lucide-react';
 
 import { cn } from '@/library/utils';
+import { typographyVariants } from '@/akasha-components/typography';
+
 const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 cursor-pointer',
   {
     variants: {
       variant: {
@@ -31,6 +33,11 @@ const buttonVariants = cva(
   },
 );
 
+const fontSizeMap = {
+  default: 'sm',
+  sm: 'xs',
+} as const;
+
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
@@ -39,13 +46,29 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, loading, asChild = false, disabled, children, ...props }, ref) => {
+  (
+    {
+      className,
+      variant,
+      size = 'default',
+      loading,
+      asChild = false,
+      disabled,
+      children,
+      ...props
+    },
+    ref,
+  ) => {
     const Comp = asChild ? Slot : 'button';
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(
+          size && size !== 'icon' && typographyVariants({ variant: fontSizeMap[size] }),
+          'font-bold',
+          buttonVariants({ variant, size, className }),
+        )}
         ref={ref}
-        disabled={loading ? true : disabled}
+        disabled={loading || disabled}
         {...props}
       >
         {loading ? <Loader2 className="animate-spin" /> : children}
