09f4ad04bc19396246f07c0612e6dbcf84308f7f
Sever Abibula <5903809+SeverS@users.noreply.github.com>
2025-02-11 20:06:29 +0200

    Devkit HMR adjustments (#2537)
    * chore(): adjust importmaps, delete sources on unmount
    
    * chore(): adjust csp, guard against missing bare specifier in systemjs
    
    * chore(): adjust csp
    
    ---------
    
    Co-authored-by: Marius Darila <3396463+kenshyx@users.noreply.github.com>

--- libs/app-loader/src/loading-functions/react.ts
+++ libs/app-loader/src/loading-functions/react.ts
@@ -6,7 +6,7 @@ import { LoadingFunctionOptions } from './index';
 
 export const getLifecycles = (
   rootComponent: IAppConfig['rootComponent'],
-  { onRenderError, onModuleError }: LoadingFunctionOptions,
+  { deleteSourcesOnUnmount, onRenderError, onModuleError }: LoadingFunctionOptions,
 ) => {
   const lifecycles = singleSpaReact({
     React,
@@ -41,7 +41,12 @@ export const getLifecycles = (
   return {
     bootstrap: lifecycles.bootstrap,
     mount: lifecycles.mount,
-    unmount: lifecycles.unmount,
+    unmount: (props: IRootComponentProps) => {
+      if (deleteSourcesOnUnmount && System.has(props.name)) {
+        System.delete(System.resolve(props.name));
+      }
+      return lifecycles.unmount(props);
+    },
     update: lifecycles.update,
   };
 };
