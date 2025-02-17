36cb1c6db45df3105f815b272272ff718cfa4fad
Didd <didd.tuni@akasha.world>
2025-02-17 14:22:44 +0300

    chore(): apply border radius customization

--- twindBaseConfig.js
+++ twindBaseConfig.js
@@ -92,6 +92,7 @@ export default {
       },
       borderRadius: {
         large: '20px',
+        xl: `calc(var(--radius) + 4px)`,
         lg: `var(--radius)`,
         md: `calc(var(--radius) - 2px)`,
         sm: 'calc(var(--radius) - 4px)',
