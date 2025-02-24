3fdaabfb63e1b1340a4aadac1835c0bbae7c7779
Klemen Kunstek <klemen.kunstek@akasha.world>
2025-02-24 11:36:04 +0100

    chore(): update prop type for Stack component

--- libs/ui/src/akasha-components/stack.tsx
+++ libs/ui/src/akasha-components/stack.tsx
@@ -40,7 +40,7 @@ const stackVariants = cva('flex divide-accent', {
 export interface StackProps
   extends React.HTMLAttributes<HTMLDivElement>,
     VariantProps<typeof stackVariants> {
-  spacing?: 0.5 | 1 | 1.5 | 2 | 2.5 | 3 | 3.5 | 4 | 5 | 6 | 7 | 8;
+  spacing?: 0.5 | 1 | 1.5 | 2 | 2.5 | 3 | 3.5 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 }
 
 const Stack = React.forwardRef<HTMLDivElement, StackProps>(
