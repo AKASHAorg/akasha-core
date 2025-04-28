8758a18731aed03e1c5aa2d2983d1deb24737c1b
Klemen Kunstek <klemen.kunstek@akasha.world>
2025-04-28 18:14:17 +0200

    chore(): update and replace Switch component

--- libs/ui/src/components/switch.tsx
+++ libs/ui/src/components/switch.tsx
@@ -17,7 +17,7 @@ const Switch = React.forwardRef<
   >
     <SwitchPrimitives.Thumb
       className={cn(
-        'pointer-events-none block h-5 w-5 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-[0.6rem] data-[state=unchecked]:translate-x-0',
+        'pointer-events-none block h-5 w-5 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-[100%] data-[state=unchecked]:translate-x-0',
       )}
     />
   </SwitchPrimitives.Root>
