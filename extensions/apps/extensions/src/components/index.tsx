1472c9881a2ef3c1c2c58d27fb4608d0898b585f
Josemaria Nriagu <josemaria.nriagu@akasha.world>
2025-05-12 08:51:22 +0100

    refactor: use helmet directly
    Signed-off-by: Josemaria Nriagu <josemaria.nriagu@akasha.world>

--- extensions/apps/extensions/src/components/index.tsx
+++ extensions/apps/extensions/src/components/index.tsx
@@ -5,7 +5,7 @@ import { Loader2 } from 'lucide-react';
 import { RouterProvider } from '@tanstack/react-router';
 import { router } from './app-routes/index';
 import { useApolloClient } from '@apollo/client';
-import { Helmet, helmetData } from '@akashaorg/design-system-core/lib/utils';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 declare module '@tanstack/react-router' {
   interface Register {
@@ -21,19 +21,21 @@ const App: React.FC<unknown> = () => {
   return (
     <React.StrictMode>
       <React.Suspense fallback={<Loader2 className="h-8 w-8 animate-spin text-primary" />}>
-        <I18nextProvider i18n={getTranslationPlugin().i18n}>
-          <Helmet helmetData={helmetData}>
-            <title>Extensions | {worldConfig.title}</title>
-          </Helmet>
-          <RouterProvider
-            router={router({
-              baseRouteName,
-              apolloClient,
-              decodeAppName,
-              plugins,
-            })}
-          />
-        </I18nextProvider>
+        <HelmetProvider>
+          <I18nextProvider i18n={getTranslationPlugin().i18n}>
+            <Helmet>
+              <title>Extensions | {worldConfig.title}</title>
+            </Helmet>
+            <RouterProvider
+              router={router({
+                baseRouteName,
+                apolloClient,
+                decodeAppName,
+                plugins,
+              })}
+            />
+          </I18nextProvider>
+        </HelmetProvider>
       </React.Suspense>
     </React.StrictMode>
   );
--- extensions/apps/world-builder/src/components/index.tsx
+++ extensions/apps/world-builder/src/components/index.tsx
@@ -3,7 +3,7 @@ import { I18nextProvider } from 'react-i18next';
 import { RouterProvider } from '@tanstack/react-router';
 import { useRootComponentProps, withProviders } from '@akashaorg/ui-core-hooks';
 import { Loader2 } from 'lucide-react';
-import { Helmet, helmetData } from '@akashaorg/design-system-core/lib/utils';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { router } from './app-routes';
 
 declare module '@tanstack/react-router' {
@@ -17,16 +17,18 @@ const App: React.FC<unknown> = () => {
 
   return (
     <React.Suspense fallback={<Loader2 className="h-8 w-8 animate-spin text-primary" />}>
-      <I18nextProvider i18n={getTranslationPlugin().i18n}>
-        <Helmet helmetData={helmetData}>
-          <title>World Builder | {worldConfig.title}</title>
-        </Helmet>
-        <RouterProvider
-          router={router({
-            baseRouteName,
-          })}
-        />
-      </I18nextProvider>
+      <HelmetProvider>
+        <I18nextProvider i18n={getTranslationPlugin().i18n}>
+          <Helmet>
+            <title>World Builder | {worldConfig.title}</title>
+          </Helmet>
+          <RouterProvider
+            router={router({
+              baseRouteName,
+            })}
+          />
+        </I18nextProvider>
+      </HelmetProvider>
     </React.Suspense>
   );
 };
