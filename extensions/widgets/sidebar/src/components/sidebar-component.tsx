09d651c682ac0364e55f83e695a648d40e78d82d
Klemen Kunstek <klemen.kunstek@akasha.world>
2025-05-14 15:56:15 +0200

    chore(): reimplement sidebar with shadcn sidebar

--- extensions/widgets/sidebar/src/components/sidebar-component.tsx
+++ extensions/widgets/sidebar/src/components/sidebar-component.tsx
@@ -1,13 +1,13 @@
 import React, { Suspense, useEffect, useMemo, useState, useSyncExternalStore } from 'react';
 import { useTranslation } from 'react-i18next';
-import { EventTypes, MenuItemAreaType, IMenuItem } from '@akashaorg/typings/lib/ui';
+import { EventTypes, MenuItemAreaType, IMenuItem, UIEventData } from '@akashaorg/typings/lib/ui';
 import {
+  filterEvents,
   useAccordion,
   useAkashaStore,
   useDismissedCard,
   useRootComponentProps,
 } from '@akashaorg/ui-core-hooks';
-import { startMobileSidebarHidingBreakpoint } from '@akashaorg/design-system-core/lib/utils/breakpoints';
 import Link from '@akashaorg/design-system-core/lib/components/Link';
 import Button from '@akashaorg/design-system-core/lib/components/Button';
 import { Card } from '@akashaorg/ui/lib/akasha-components/card';
@@ -24,6 +24,7 @@ import ListSidebarApps from './list-sidebar-apps';
 import SidebarCTACard from './cta-card';
 import SidebarHeader from './sidebar-header';
 import FallbackHeader from './fallback-header';
+import * as Shadcn from '@akashaorg/ui/lib/components/sidebar';
 
 const SidebarComponent: React.FC<unknown> = () => {
   const {
@@ -39,9 +40,6 @@ const SidebarComponent: React.FC<unknown> = () => {
   } = useAkashaStore();
 
   const { t } = useTranslation('ui-widget-sidebar');
-  const [isMobile, setIsMobile] = useState(
-    window.matchMedia(startMobileSidebarHidingBreakpoint).matches,
-  );
   const routing = getCorePlugins().routing;
   const routeData = useSyncExternalStore(routing.subscribe, routing.getSnapshot);
   const [activeOption, setActiveOption] = useState<IMenuItem | null>(null);
@@ -50,19 +48,48 @@ const SidebarComponent: React.FC<unknown> = () => {
 
   const { activeAccordionId, setActiveAccordionId, handleAccordionClick } = useAccordion();
   const [dismissed, dismissCard] = useDismissedCard('@akashaorg/ui-widget-sidebar_cta-card');
+  const { setOpen, openMobile, setOpenMobile, toggleSidebar, isMobile } = Shadcn.useSidebar();
 
   useEffect(() => {
-    const mql = window.matchMedia(startMobileSidebarHidingBreakpoint);
-
-    const resize = () => {
-      setIsMobile(mql.matches);
-    };
-
-    window.addEventListener('resize', resize);
+    const eventsSub = uiEvents
+      .pipe(filterEvents([EventTypes.ShowSidebar, EventTypes.HideSidebar]))
+      .subscribe({
+        next: (eventInfo: UIEventData) => {
+          switch (eventInfo.event) {
+            case EventTypes.ShowSidebar:
+              if (isMobile) {
+                setOpenMobile(true);
+              } else {
+                setOpen(true);
+              }
+              break;
+            case EventTypes.HideSidebar:
+              if (isMobile) {
+                setOpenMobile(false);
+              } else {
+                setOpen(false);
+              }
+              break;
+            default:
+              break;
+          }
+        },
+      });
     return () => {
-      window.removeEventListener('resize', resize);
+      if (eventsSub) {
+        eventsSub.unsubscribe();
+      }
     };
-  }, []);
+  }, [openMobile, setOpenMobile, toggleSidebar, uiEvents, isMobile, setOpen]);
+
+  // make sure that topBar button state is synced with mobile sidebar
+  useEffect(() => {
+    if (isMobile && !openMobile) {
+      uiEvents.next({
+        event: EventTypes.HideSidebar,
+      });
+    }
+  }, [isMobile, openMobile, uiEvents]);
 
   // sort according to worldConfig index
   const worldApps = useMemo(() => {
@@ -188,73 +215,82 @@ const SidebarComponent: React.FC<unknown> = () => {
       }}
       logger={logger}
     >
-      <Card className="p-0 rounded-r-2xl xl:rounded-2xl w-[19.5rem] max-w-[19.5rem] max-h-screen xl:max-h-[calc(100vh-20px) h-full xl:h-fit">
-        <Suspense
-          fallback={<FallbackHeader authenticatedDID={authenticatedDID} isLoggedIn={isLoggedIn} />}
-        >
-          <SidebarHeader
-            authenticatedDID={authenticatedDID}
-            connectLabel={t('Connect')}
-            cancelLabel={t('Cancel')}
-            isLoggedIn={isLoggedIn}
-            logoutClickHandler={handleLogoutClick}
-            loginClickHandler={handleLoginClick}
-            isAuthenticating={isAuthenticating}
-            handleProfileAvatarClick={handleProfileAvatarClick}
-          />
-        </Suspense>
-        {/*
+      <Shadcn.Sidebar>
+        <Card className="p-0 rounded-r-2xl xl:rounded-2xl max-h-screen xl:max-h-[calc(100vh-20px) h-full xl:h-fit">
+          <Shadcn.SidebarHeader>
+            <Suspense
+              fallback={
+                <FallbackHeader authenticatedDID={authenticatedDID} isLoggedIn={isLoggedIn} />
+              }
+            >
+              <SidebarHeader
+                authenticatedDID={authenticatedDID}
+                connectLabel={t('Connect')}
+                cancelLabel={t('Cancel')}
+                isLoggedIn={isLoggedIn}
+                logoutClickHandler={handleLogoutClick}
+                loginClickHandler={handleLoginClick}
+                isAuthenticating={isAuthenticating}
+                handleProfileAvatarClick={handleProfileAvatarClick}
+              />
+            </Suspense>
+          </Shadcn.SidebarHeader>
+          <Shadcn.SidebarContent>
+            {/*
           this container will grow up to a max height of 68vh, 32vh currently accounts for the height of other sections and paddings. Adjust accordingly, if necessary.
         */}
-        <Stack direction="column" className="overflow-auto">
-          {/* container for world apps */}
-          {worldApps?.length > 0 && (
-            <ListSidebarApps
-              list={worldApps}
-              activeAccordionId={activeAccordionId}
-              activeOption={activeOption}
-              setActiveAccordionId={setActiveAccordionId}
-              handleAccordionClick={handleAccordionClick}
-              onOptionClick={handleOptionClick}
-              onClickMenuItem={handleAppIconClick}
-            />
-          )}
-          {/* container for user-installed apps */}
-          {userInstalledApps?.length > 0 && (
-            <ListSidebarApps
-              list={userInstalledApps}
-              activeAccordionId={activeAccordionId}
-              activeOption={activeOption}
-              setActiveAccordionId={setActiveAccordionId}
-              handleAccordionClick={handleAccordionClick}
-              hasBorderTop={true}
-              onOptionClick={handleOptionClick}
-              onClickMenuItem={handleAppIconClick}
-            />
-          )}
-        </Stack>
-        {!dismissed && (
-          <SidebarCTACard onClickCTAButton={handleClickExplore} onDismissCard={dismissCard} />
-        )}
-        {modSocialLinks?.length > 0 && (
-          <Stack className="px-8 py-4 border-t-1 border-grey9 dark:border-grey3">
-            <Text variant="footnotes2">{t('Get in touch')}</Text>
-            <Stack direction="row" spacing={4} className="w-fit h-fit mt-6">
-              {modSocialLinks?.map((socialLink, idx) => (
-                <Link key={idx} to={socialLink.link} target="_blank">
-                  <Button
-                    icon={socialLink.icon}
-                    solidIcon={idx > 0}
-                    variant="primary"
-                    greyBg={true}
-                    iconOnly={true}
-                  />
-                </Link>
-              ))}
+            <Stack direction="column" className="overflow-auto">
+              {/* container for world apps */}
+              {worldApps?.length > 0 && (
+                <ListSidebarApps
+                  list={worldApps}
+                  activeAccordionId={activeAccordionId}
+                  activeOption={activeOption}
+                  setActiveAccordionId={setActiveAccordionId}
+                  handleAccordionClick={handleAccordionClick}
+                  onOptionClick={handleOptionClick}
+                  onClickMenuItem={handleAppIconClick}
+                />
+              )}
+              {/* container for user-installed apps */}
+              {userInstalledApps?.length > 0 && (
+                <ListSidebarApps
+                  list={userInstalledApps}
+                  activeAccordionId={activeAccordionId}
+                  activeOption={activeOption}
+                  setActiveAccordionId={setActiveAccordionId}
+                  handleAccordionClick={handleAccordionClick}
+                  hasBorderTop={true}
+                  onOptionClick={handleOptionClick}
+                  onClickMenuItem={handleAppIconClick}
+                />
+              )}
             </Stack>
-          </Stack>
-        )}
-      </Card>
+            {!dismissed && (
+              <SidebarCTACard onClickCTAButton={handleClickExplore} onDismissCard={dismissCard} />
+            )}
+            {modSocialLinks?.length > 0 && (
+              <Stack className="px-8 py-4 border-t-1 border-grey9 dark:border-grey3">
+                <Text variant="footnotes2">{t('Get in touch')}</Text>
+                <Stack direction="row" spacing={4} className="w-fit h-fit mt-6">
+                  {modSocialLinks?.map((socialLink, idx) => (
+                    <Link key={idx} to={socialLink.link} target="_blank">
+                      <Button
+                        icon={socialLink.icon}
+                        solidIcon={idx > 0}
+                        variant="primary"
+                        greyBg={true}
+                        iconOnly={true}
+                      />
+                    </Link>
+                  ))}
+                </Stack>
+              </Stack>
+            )}
+          </Shadcn.SidebarContent>
+          <Shadcn.SidebarFooter />
+        </Card>
+      </Shadcn.Sidebar>
     </ErrorBoundary>
   );
 };
