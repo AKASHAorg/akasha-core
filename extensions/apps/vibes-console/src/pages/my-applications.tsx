8ff77f1142dcf7132527daba07b8394755d9756e
Sever Abibula <5903809+SeverS@users.noreply.github.com>
2025-05-16 16:45:22 +0300

    chore(): remove table (#2640)

--- extensions/apps/vibes-console/src/pages/my-applications.tsx
+++ extensions/apps/vibes-console/src/pages/my-applications.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { useNavigate } from '@tanstack/react-router';
 import { Card } from '@akashaorg/ui/lib/akasha-components/card';
 import PaginatedTable from '@akashaorg/design-system-components/lib/components/PaginatedTable';
 import Stack from '@akashaorg/design-system-core/lib/components/Stack';
 import { Typography } from '@akashaorg/ui/lib/akasha-components/typography';
-import { renderChevron, renderDate, renderStatus } from '../utils';
-import routes, { MY_APPLICATION_DETAIL } from '../routes';
 import { NoItemFound } from '../components/no-item-found';
 
 export const MyApplications: React.FC<unknown> = () => {
-  const navigate = useNavigate();
   const { t } = useTranslation('vibes-console');
-  const handleRowClick = (applicationId: string) => {
-    navigate({
-      to: routes[MY_APPLICATION_DETAIL],
-      params: {
-        applicationId,
-      },
-    });
-  };
+
   const loggedUserApplications = [];
 
   if (!loggedUserApplications.length) {
@@ -29,15 +18,11 @@ export const MyApplications: React.FC<unknown> = () => {
       </Card>
     );
   }
-  const loggedUserApplicationsRows = loggedUserApplications.map(({ id, resolvedDate, status }) => ({
-    value: [renderDate(resolvedDate), renderStatus(status), renderChevron()],
-    clickHandler: () => handleRowClick(id),
-  }));
+
   return (
     <Stack spacing="gap-y-4">
       <Typography variant="h5">{t('Your Applications')}</Typography>
       <PaginatedTable
-        rows={loggedUserApplicationsRows}
         pageCount={1}
         currentPage={1}
         onClickPage={() => {
